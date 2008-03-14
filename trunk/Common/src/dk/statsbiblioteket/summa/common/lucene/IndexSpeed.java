f1bfaa1ab8d432713c8f75c266627c36d43b6d6d
toke <toke@6c0f5c6f-88b2-4050-bb98-6ed3ea504abe>
2008-03-14 11:46:26 +0000

    Changed version from Id: to Rev:

--- trunk/Common/src/dk/statsbiblioteket/summa/common/lucene/IndexSpeed.java
+++ trunk/Common/src/dk/statsbiblioteket/summa/common/lucene/IndexSpeed.java
@@ -54,7 +54,8 @@ import org.apache.lucene.search.Query;
         author = "te")
 public class IndexSpeed {
     private static final Log log = LogFactory.getLog(IndexSpeed.class);
-    private static final String version = "$Id:$ $Version:$";
+    private static final String version =
+            "$Rev:$";
 
     private static int maxdocs = Integer.MAX_VALUE-1;
     private static int ramBuffer = 8;
