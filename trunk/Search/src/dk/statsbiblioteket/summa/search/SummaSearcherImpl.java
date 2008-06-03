e649fe16f3e4b91af630031b55fb5d29cbc949fa
toke <toke@6c0f5c6f-88b2-4050-bb98-6ed3ea504abe>
2008-06-03 14:58:38 +0000

    Work on the SummaSearcherImpl control of SearchNodes

--- trunk/Search/src/dk/statsbiblioteket/summa/search/SummaSearcherImpl.java
+++ trunk/Search/src/dk/statsbiblioteket/summa/search/SummaSearcherImpl.java
@@ -8,8 +8,8 @@ import dk.statsbiblioteket.util.qa.QAInfo;
 import dk.statsbiblioteket.summa.common.configuration.Configurable;
 import dk.statsbiblioteket.summa.common.configuration.Configuration;
 
-import java.util.List;
-import java.util.Arrays;
+import java.util.concurrent.Semaphore;
+import java.rmi.RemoteException;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -30,12 +30,52 @@ public abstract class SummaSearcherImpl implements SummaSearcher, Configurable {
             DEFAULT_NUMBER_OF_CONCURRENT_SEARCHES;
     private int searchQueueMaxSize = DEFAULT_SEARCH_QUEUE_MAX_SIZE;
 
+    private Semaphore searchQueueSemaphore;
+    private Semaphore searchActiveSemaphore;
+
     /**
      * Extracts basic settings from the configuration.
      * @param conf the configuration for the searcher.
      */
     public SummaSearcherImpl(Configuration conf) {
+        searchQueueMaxSize =
+                conf.getInt(CONF_SEARCH_QUEUE_MAX_SIZE, searchQueueMaxSize);
+        searchQueueSemaphore = new Semaphore(searchQueueMaxSize, true);
+        maxNumberOfConcurrentSearches =
+                conf.getInt(CONF_NUMBER_OF_CONCURRENT_SEARCHES,
+                            maxNumberOfConcurrentSearches);
+        searchActiveSemaphore =
+                new Semaphore(maxNumberOfConcurrentSearches, true);
     }
 
-    // TODO: Pool of requests (futures)
+    public String fullSearch(String filter, String query, long startIndex,
+                             long maxRecords, String sortKey,
+                             boolean reverseSort, String[] resultFields,
+                             String[] fallbacks) throws RemoteException {
+        if (searchQueueSemaphore.availablePermits() == 0) {
+            throw new RemoteException(
+                    "Could not perform search as the queue of requests exceed "
+                    + searchQueueMaxSize);
+        }
+        try {
+            searchQueueSemaphore.acquire();
+        } catch (InterruptedException e) {
+            throw new RemoteException(
+                    "Interrupted while waiting for search queue access", e);
+        }
+        try {
+            try {
+                searchActiveSemaphore.acquire();
+                // TODO: Find the one with the least load, which is ready
+                return null;
+            } catch (InterruptedException e) {
+                throw new RemoteException("Interrupted while waiting for active"
+                                          + " search queue access", e);
+            } finally {
+                searchActiveSemaphore.release();
+            }
+        } finally {
+            searchQueueSemaphore.release();
+        }
+    }
 }
