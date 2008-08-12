1f8dac4a3b3189cb04ffb8aafce22a8cf10a041b
toke <toke@6c0f5c6f-88b2-4050-bb98-6ed3ea504abe>
2008-08-12 15:01:40 +0000

    A bit of work on getting SearchTest to run

--- trunk/Control/src/dk/statsbiblioteket/summa/control/service/SearchService.java
+++ trunk/Control/src/dk/statsbiblioteket/summa/control/service/SearchService.java
@@ -26,8 +26,9 @@ import java.rmi.RemoteException;
 import java.io.IOException;
 
 import dk.statsbiblioteket.summa.search.SummaSearcher;
-import dk.statsbiblioteket.summa.search.LuceneSearcher;
-import dk.statsbiblioteket.summa.search.document.DocumentResponse;
+import dk.statsbiblioteket.summa.search.SummaSearcherImpl;
+import dk.statsbiblioteket.summa.search.ResponseCollection;
+import dk.statsbiblioteket.summa.search.Request;
 import dk.statsbiblioteket.summa.common.configuration.Configuration;
 import dk.statsbiblioteket.summa.common.Logging;
 import dk.statsbiblioteket.summa.control.api.Status;
@@ -37,7 +38,7 @@ import org.apache.commons.logging.LogFactory;
 
 /**
  * Wrapper for a {@link SummaSearcher}, which will normally translate to a
- * {@link LuceneSearcher}.
+ * {@link SummaSearcherImpl}.
  */
 @QAInfo(level = QAInfo.Level.NORMAL,
         state = QAInfo.State.IN_DEVELOPMENT,
@@ -49,12 +50,12 @@ public class SearchService extends ServiceBase implements SummaSearcher,
     /**
      * The class to instantiate and use for searching.
      * </p><p>
-     * This is optional. Default is the {@link LuceneSearcher} class.
+     * This is optional. Default is the {@link SummaSearcherImpl} class.
      */
     public static final String CONF_SEARCHER_CLASS =
             "summa.search.searcher-class";
     public static final Class<? extends SummaSearcher> DEFAULT_SEARCHER_CLASS =
-            LuceneSearcher.class;
+            SummaSearcherImpl.class;
 
     private Configuration conf;
     private SummaSearcher searcher;
@@ -149,76 +150,9 @@ public class SearchService extends ServiceBase implements SummaSearcher,
     /* Simple pass-through to the underlying searcher */
 
 
-    /**
-     * Implementation of the {@link SummaSearcher} interface. See the JavaDocs
-     * for {@link SummaSearcher#fullSearch} for details on usage.
-     * @param filter      a query that narrows the search. A filter does not
-     *                    affect scores.<br />
-     *                    This parameter is optional. Default is null.
-     * @param query       a query as entered by a user. This is expanded to
-     *                    the underlying index query-system, normally with
-     *                    the use of
-     *           {@link dk.statsbiblioteket.summa.common.index.IndexDescriptor}.
-     * @param startIndex  the starting index for the result, counting from 0.
-     *                    If the result is to be merged with the result from
-     *                    other searchers, this needs to be 0 in order to
-     *                    ensure proper merging.
-     * @param maxRecords  the maximum number of records to return.<br />
-     *                    this parameter is mandatory and is rounded down to
-     *                    the value specified in properties, using the key
-     *                    {link #CONF_MAX_NUMBER_OF_RECORDS}.
-     * @param sortKey     specifies how to sort. If this is null or
-     *                    {@link #SORT_ON_SCORE}, sorting will be done on the
-     *                    scores for the hits. If a field-name is specified,
-     *                    sorting will be done on that field.<br />
-     *                    This parameter is optional.
-     *                    Default is {@link #SORT_ON_SCORE}.
-     *                    Specifying null is the same as specifying
-     *                    {@link #SORT_ON_SCORE}.
-     * @param reverseSort if true, the sort is performed in reverse order.
-     * @param resultFields the fields to extract content from.
-     *                    This parameter is optional. Default is specified
-     *                    in the conf. at {@link #CONF_RESULT_FIELDS}.
-     * @param fallbacks   if the value of a given field cannot be extracted,
-     *                    the corresponding value from fallbacks is returned.
-     *                    Note that the length of fallbacks and fields must
-     *                    be the same.
-     * @return the result of a search, suitable for merging or XML construction.
-     * @throws RemoteException if there was an exception during search.
-     */
-    public DocumentResponse fullSearch(String filter, String query, long startIndex,
-                             long maxRecords, String sortKey,
-                             boolean reverseSort, String[] resultFields,
-                             String[] fallbacks) throws RemoteException {
-        return searcher.fullSearch(filter, query, startIndex, maxRecords,
-                                   sortKey, reverseSort,
-                                   resultFields, fallbacks);
+    public ResponseCollection search(Request request) throws RemoteException {
+        return searcher.search(request);
     }
-
-    /**
-     * Simple shortcut for fullSearch. Equivalent to {@code
-     * fullSearch(null, query, startIndex, maxRecords, null, false, null, null);
-     * }
-     * @param query       a query as entered by a user. This is expanded to
-     *                    the underlying index query-system, normally with
-     *                    the use of
-     *           {@link dk.statsbiblioteket.summa.common.index.IndexDescriptor}.
-     * @param startIndex  the starting index for the result, counting from 0.
-     *                    If the result is to be merged with the result from
-     *                    other searchers, this needs to be 0 in order to
-     *                    ensure proper merging.
-     * @param maxRecords  the maximum number of records to return.<br />
-     *                    this parameter is mandatory and is rounded down to
-     *                    the value specified in properties, using the key
-     *                    {link #CONF_MAX_NUMBER_OF_RECORDS}.
-     * @return the search-result in XML, as specified in {@link #fullSearch}.
-     * @throws RemoteException if there was an exception during search.
-     */
-    public String simpleSearch(String query, long startIndex,
-                               long maxRecords) throws RemoteException {
-        return searcher.simpleSearch(query, startIndex, maxRecords);
-    }
-
 
     /**
      * Alias for {@link #stop}.
@@ -230,135 +164,4 @@ public class SearchService extends ServiceBase implements SummaSearcher,
 
     /* Delegation of SummaSearcher methods */
 
-    public void clearStatistics() throws RemoteException {
-        checkSearcher();
-        searcher.clearStatistics();
-    }
-    public double getAverageResponseTime() throws RemoteException {
-        checkSearcher();
-        return searcher.getAverageResponseTime();
-    }
-    public int getCurrentSearches() throws RemoteException {
-        checkSearcher();
-        return searcher.getCurrentSearches();
-    }
-    public String[] getFallbackValues() throws RemoteException {
-        checkSearcher();
-        return searcher.getFallbackValues();
-    }
-    public String getIndexLocation() throws RemoteException {
-        checkSearcher();
-        return searcher.getIndexLocation();
-    }
-    public String getLastQuery() throws RemoteException {
-        checkSearcher();
-        return searcher.getLastQuery();
-    }
-    public long getLastResponseTime() throws RemoteException {
-        checkSearcher();
-        return searcher.getLastResponseTime();
-    }
-    public int getMaxConcurrentSearches() throws RemoteException {
-        checkSearcher();
-        return searcher.getMaxConcurrentSearches();
-    }
-    public long getMaxRecords() throws RemoteException {
-        checkSearcher();
-        return searcher.getMaxRecords();
-    }
-    public long getQueryCount() throws RemoteException {
-        checkSearcher();
-        return searcher.getQueryCount();
-    }
-    public int getQueueLength() throws RemoteException {
-        checkSearcher();
-        return searcher.getQueueLength();
-    }
-    public String[] getResultFields() throws RemoteException {
-        checkSearcher();
-        return searcher.getResultFields();
-    }
-    public int getSearcherAvailabilityTimeout() throws RemoteException {
-        checkSearcher();
-        return searcher.getSearcherAvailabilityTimeout();
-    }
-    public int getSearchers() throws RemoteException {
-        checkSearcher();
-        return searcher.getSearchers();
-    }
-    public int getSearchQueueMaxSize() throws RemoteException {
-        checkSearcher();
-        return searcher.getSearchQueueMaxSize();
-    }
-    public String getSortKey() throws RemoteException {
-        checkSearcher();
-        return searcher.getSortKey();
-    }
-    public long getTotalResponseTime() throws RemoteException {
-        checkSearcher();
-        return searcher.getTotalResponseTime();
-    }
-    public String getWarmupData() throws RemoteException {
-        checkSearcher();
-        return searcher.getWarmupData();
-    }
-    public int getWarmupMaxTime() throws RemoteException {
-        checkSearcher();
-        return searcher.getWarmupMaxTime();
-    }
-    public long performWarmup() throws RemoteException {
-        checkSearcher();
-        return searcher.performWarmup();
-    }
-    public void reloadIndex() throws RemoteException {
-        checkSearcher();
-        searcher.reloadIndex();
-    }
-    public void setFallbackValues(
-            String[] fallbackValues) throws RemoteException {
-        checkSearcher();
-        searcher.setFallbackValues(fallbackValues);
-    }
-    public void setMaxConcurrentSearches(int maxSearches) throws
-                                                          RemoteException {
-        checkSearcher();
-        searcher.setMaxConcurrentSearches(maxSearches);
-    }
-    public void setMaxRecords(long maxRecords) throws RemoteException {
-        checkSearcher();
-        searcher.setMaxRecords(maxRecords);
-    }
-    public void setResultFields(String[] fieldNames) throws RemoteException {
-        checkSearcher();
-        searcher.setResultFields(fieldNames);
-    }
-    public void setSearcherAvailabilityTimeout(int ms) throws RemoteException {
-        checkSearcher();
-        searcher.setSearcherAvailabilityTimeout(ms);
-    }
-    public void setSearchers(int searchers) throws RemoteException {
-        checkSearcher();
-        searcher.setSearchers(searchers);
-    }
-    public void setSearchQueueMaxSize(int maxSize) throws RemoteException {
-        checkSearcher();
-        searcher.setSearchQueueMaxSize(maxSize);
-    }
-    public void setSortKey(String sortKey) throws RemoteException {
-        checkSearcher();
-        searcher.setSortKey(sortKey);
-    }
-    public void setWarmupData(String location) throws RemoteException {
-        checkSearcher();
-        searcher.setWarmupData(location);
-    }
-    public void setWarmupMaxTime(int maxTime) throws RemoteException {
-        checkSearcher();
-        searcher.setWarmupMaxTime(maxTime);
-    }
-    private void checkSearcher() throws RemoteException {
-        if (searcher == null) {
-            throw new RemoteException("SearchService not started");
-        }
-    }
 }
