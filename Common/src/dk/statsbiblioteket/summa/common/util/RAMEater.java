dc9531b0b3e70cfda7c27dc1b78acd4ae363af66
Toke Eskildsen <te@ekot.dk>
2008-08-21 09:13:58 +0000

    Upgraded RAMEater to resident capabilities, so allocated RAM does not swap

--- Common/src/dk/statsbiblioteket/summa/common/util/RAMEater.java
+++ Common/src/dk/statsbiblioteket/summa/common/util/RAMEater.java
@@ -24,6 +24,7 @@ package dk.statsbiblioteket.summa.common.util;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 import dk.statsbiblioteket.util.qa.QAInfo;
 
@@ -35,6 +36,8 @@ import dk.statsbiblioteket.util.qa.QAInfo;
         state = QAInfo.State.QA_NEEDED,
         author = "te")
 public class RAMEater {
+    private static final long SLEEP_TIME = 20;
+    public static final long RANDOM_UPDATES = 100;
 
     /**
      * Allocated memory in chunks of 1 MB and fills it with garbage. This goes
@@ -43,11 +46,40 @@ public class RAMEater {
      * @param args all arguments are ignored.
      */
     public static void main(String[] args) {
+        boolean resident = false;
+        int maxMB = Integer.MAX_VALUE;
+        int pos = 0;
+        while (pos < args.length) {
+            if ("-h".equals(args[pos])) {
+                System.out.println("Usage: RAMEater [-r] [-m MB]");
+                System.out.println(
+                        "-r: resident. Do not exit and perform periodical, "
+                        + "random updates to memory to keep it from being "
+                        + "swapped\n"
+                        + "-m: max MB to allocate");
+                return;
+            }
+            if ("-r".equals(args[pos])) {
+                resident = true;
+                pos++;
+                continue;
+            }
+            if ("-m".equals(args[pos])) {
+                pos++;
+                maxMB = Integer.parseInt(args[pos]);
+                pos++;
+            }
+        }
+        if (maxMB == Integer.MAX_VALUE) {
+            System.out.println("Allocating RAM until OutOfMemory");
+        } else {
+            System.out.println("Allocating a maximum of " + maxMB + " MB");
+        }
         int BLOCK_SIZE = 1024*1024;
         List<byte[]> bytes = new ArrayList<byte[]>(1000);
         try {
             //noinspection InfiniteLoopStatement
-            while (true) {
+            for (int mb = 0 ; mb < maxMB ; mb++) {
                 byte[] newBytes = new byte[BLOCK_SIZE];
                 for (int i = 0 ; i < BLOCK_SIZE ;i++) {
                     // We don't care about the content, but we put in something
@@ -60,6 +92,30 @@ public class RAMEater {
             System.out.println("Allocated "
                                + (long)bytes.size() * BLOCK_SIZE / 1024 / 1024
                                + "MB before OutOfMemoryError was thrown");
+        }
+        if (!resident) {
+            return;
+        }
+        System.out.println(String.format("Allocated %d MB",
+                                         (long)bytes.size() * BLOCK_SIZE 
+                                         / 1048576));
+        System.out.println(String.format(
+                "Entering random updates mode: Updating %d random bytes each "
+                + "%d ms", RANDOM_UPDATES, SLEEP_TIME));
+        Random random = new Random();
+        //noinspection InfiniteLoopStatement
+        while (true) {
+            try {
+                Thread.sleep(SLEEP_TIME);
+            } catch (InterruptedException e) {
+                throw new IllegalStateException("Interrupted while sleeping in"
+                                                + " resident-mode", e);
+            }
+            for (int i = 0 ; i < RANDOM_UPDATES ; i++) {
+                bytes.get(random.nextInt(bytes.size()))
+                        [random.nextInt(BLOCK_SIZE)] =
+                        (byte)(random.nextInt(255) & 0xFF);
+            }
         }
     }
 }
