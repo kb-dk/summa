2e21b44456872c0a3feff5a55d11d1b7674b6bbc
toke <toke@6c0f5c6f-88b2-4050-bb98-6ed3ea504abe>
2008-09-10 09:58:01 +0000

    Made the FacetMapTest more solid

--- trunk/FacetBrowser/test/dk/statsbiblioteket/summa/facetbrowser/core/FacetMapTest.java
+++ trunk/FacetBrowser/test/dk/statsbiblioteket/summa/facetbrowser/core/FacetMapTest.java
@@ -23,26 +23,35 @@
 package dk.statsbiblioteket.summa.facetbrowser.core;
 
 import dk.statsbiblioteket.summa.common.configuration.Configuration;
+import dk.statsbiblioteket.summa.common.configuration.ConfigurationStorage;
 import dk.statsbiblioteket.summa.common.configuration.storage.MemoryStorage;
 import dk.statsbiblioteket.summa.facetbrowser.Structure;
+import dk.statsbiblioteket.summa.facetbrowser.FacetStructure;
 import dk.statsbiblioteket.summa.facetbrowser.core.map.CoreMap;
 import dk.statsbiblioteket.summa.facetbrowser.core.map.CoreMapBitStuffed;
 import dk.statsbiblioteket.summa.facetbrowser.core.tags.Facet;
 import dk.statsbiblioteket.summa.facetbrowser.core.tags.TagHandler;
+import dk.statsbiblioteket.summa.facetbrowser.core.tags.TagHandlerImpl;
 import dk.statsbiblioteket.util.qa.QAInfo;
+import dk.statsbiblioteket.util.Files;
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
 import java.io.StringWriter;
+import java.io.File;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Collections;
+import java.util.ArrayList;
+import java.util.Random;
 
 /**
  * FacetMap Tester.
  */
+@SuppressWarnings({"DuplicateStringLiteralInspection"})
 @QAInfo(level = QAInfo.Level.NORMAL,
         state = QAInfo.State.IN_DEVELOPMENT,
         author = "te")
@@ -59,28 +68,31 @@ public class FacetMapTest extends TestCase {
         super.setUp();
 
         MemoryStorage memStore = new MemoryStorage();
-        memStore.put(Structure.CONF_FACETS, "A, B, C");
+        List<ConfigurationStorage> subs =
+                memStore.createSubStorages(Structure.CONF_FACETS, 3);
+        int counter = 0;
+        for (String facetName: new String[]{"A", "B", "C"}) {
+            subs.get(counter++).put(FacetStructure.CONF_FACET_NAME, facetName);
+        }
         Configuration config = new Configuration(memStore);
         Structure structure = new Structure(config);
+        assertEquals("The Structure should contain 3 Facets",
+                     3, structure.getFacets().size());
         core = new CoreMapBitStuffed(config, structure);
-        Facet[] pools = new Facet[structure.getFacetNames().size()];
-        int position = 0;
-        //noinspection UnusedDeclaration
-        for (String facetName: structure.getFacetNames()) {
-            //noinspection DuplicateStringLiteralInspection
-            // TODO: Update this
-//            pools[position++] = new Facet(
-//                    new File(System.getProperty("java.io.tmpdir")),
-//                    "facettest" + position, null, null, true, true);
-        }
-//        handler = new TagHandlerImpl(structure, pools);
+        handler = new TagHandlerImpl(config, structure, false);
         map = new FacetMap(structure, core, handler, false);
+        map.open(TMP_DIR);
     }
-
+    private File TMP_DIR = new File(System.getProperty("java.io.tmpdir"),
+                                    "testTagHandler");
     public void tearDown() throws Exception {
         super.tearDown();
         //noinspection AssignmentToNull
-        map = null;
+        map.close();
+        handler.close();
+        if (TMP_DIR.exists()) {
+            Files.delete(TMP_DIR);
+        }
     }
 
     public Map<String, List<String>> arraysToMap(String[] facets,
@@ -106,8 +118,9 @@ public class FacetMapTest extends TestCase {
                                          new String[][]{{"BZoo", "BBar"}}));
         assertEquals("Adding another should give 2", 2, map.getDocCount());
         map.removeDocument(0);
-        assertEquals("Removing a document should decrease the count", 
-                     1, map.getDocCount());
+        assertEquals("Removing a document should not decrease the count with no"
+                     + " shift (default)",
+                     2, map.getDocCount());
     }
 
     protected String dump(String[] values) {
@@ -125,6 +138,7 @@ public class FacetMapTest extends TestCase {
 
     public void assertHasTags(String message, int docID, String facet,
                               String[] expectedTags) throws Exception {
+        Arrays.sort(expectedTags);
         int facetID = handler.getFacetID(facet);
         if (facetID == -1) {
             fail(message + ". Facet \"" + facet + "\" was not present");
@@ -167,7 +181,7 @@ public class FacetMapTest extends TestCase {
                       new String[]{"AFoo", "ABar", "AZoo", "ABii"});
         assertHasTags("Facet 0/A should be unchanged", 0, "A", 
                       new String[]{"AFoo", "ABar", "AZoo", "ABoo", "AKazam"});
-        map.removeDocument(0);
+/*        map.removeDocument(0);
         assertHasTags("Facet 1/A should now be 0/A", 0, "A",
                       new String[]{"AFoo", "ABar", "AZoo", "ABii"});
         try {
@@ -175,7 +189,115 @@ public class FacetMapTest extends TestCase {
             fail("Document 1 should no longer exist");
         } catch(Exception e) {
             // Expected
+        }*/
+    }
+
+    public void testInsert() throws Exception {
+        map.addToDocument(0, arraysToMap(
+                new String[]{"A"},
+                new String[][]{{"AFoo", "ABar"}}));
+        assertHasTags("Doc 0 Facet A should be added", 0, "A",
+                      new String[]{"AFoo", "ABar"});
+        map.addToDocument(1, arraysToMap(
+                new String[]{"A"},
+                new String[][]{{"AFoo", "AAbe"}}));
+        assertHasTags("Doc 1 Facet A should be added", 1, "A",
+                      new String[]{"AFoo", "AAbe"});
+        assertHasTags("Doc 0 Facet A should be unchanged", 0, "A",
+                      new String[]{"AFoo", "ABar"});
+    }
+
+    public void testInsertOrder() throws Exception {
+        int RUNS = 100;
+        String[][][] INSERTS = new String[][][] {
+                {{"1A"}, {"1"}},
+                {{"1B"}, {"3"}},
+                {{"3B"}, {"5"}},
+                {{"0A"}, {"2"}},
+                {{"0A"}, {"3"}},
+                {{"0A"}, {"2"}},
+                {{"0B"}, {"4"}}
+        };
+        for (int i = 0 ; i < RUNS ; i++) {
+            handler.clearTags();
+            core.clear();
+            permute(INSERTS);
+            insertTags(INSERTS);
+            assertHasTags("Doc 0 Facet A should be right", 0, "A",
+                          new String[]{"2", "3"});
+            assertHasTags("Doc 0 Facet B should be right", 0, "B",
+                          new String[]{"4"});
+            assertHasTags("Doc 1 Facet A should be right", 1, "A",
+                          new String[]{"1"});
+            assertHasTags("Doc 1 Facet b should be right", 1, "B",
+                          new String[]{"3"});
+            assertHasTags("Doc 3 Facet A should be right", 3, "B",
+                          new String[]{"5"});
         }
+    }
+
+    private void permute(String[][][] inserts) {
+        Random random = new Random();
+        for (int i = 0 ; i < inserts.length ; i++) {
+            String[][] t = inserts[i];
+            int pos = random.nextInt(inserts.length);
+            inserts[i] = inserts[pos];
+            inserts[pos] = t;
+        }
+    }
+
+    public void testInsertMultiple() throws Exception {
+        String[][][] INSERTS = new String[][][] {
+                {{"0A"}, {"2"}},
+                {{"0A"}, {"3"}},
+                {{"0A"}, {"2"}},
+                {{"0B"}, {"4"}},
+                {{"1A"}, {"1"}},
+                {{"1B"}, {"3"}},
+                {{"3B"}, {"5"}}
+        };
+        insertTags(INSERTS);
+        assertHasTags("Doc 0 Facet A should be right", 0, "A",
+                      new String[]{"1", "2", "3"});
+        assertHasTags("Doc 0 Facet B should be right", 0, "B",
+                      new String[]{"4"});
+        assertHasTags("Doc 1 Facet A should be right", 1, "A",
+                      new String[]{"1"});
+        assertHasTags("Doc 1 Facet b should be right", 1, "B",
+                      new String[]{"3"});
+        assertHasTags("Doc 3 Facet A should be right", 3, "B",
+                      new String[]{"5"});
+    }
+
+    private void insertTags(String[][][] INSERTS) {
+        for (String[][] insert: INSERTS) {
+            String pos = insert[0][0];
+            int docID = Integer.parseInt(pos.substring(0, 1));
+            String facetName = pos.substring(1, 2);
+            String[] tags = insert[1];
+            map.addToDocument(docID, arraysToMap(
+                    new String[]{facetName},
+                    new String[][]{tags}));
+        }
+    }
+
+    public void testPersistence() throws Exception {
+        testInsert();
+        map.store();
+        map.addToDocument(0, arraysToMap(
+                new String[]{"B"},
+                new String[][]{{"AFoo"}}));
+        assertHasTags("Doc 0 Facet B should created", 0, "B",
+                      new String[]{"AFoo"});
+        map.open(TMP_DIR);
+        assertHasTags("Doc 0 Facet A should be fine after persistence", 0, "A",
+                      new String[]{"AFoo", "ABar"});
+        assertHasTags("Doc 1 Facet A should be fine after persistence", 1, "A",
+                      new String[]{"AFoo", "AAbe"});
+        int facetID = handler.getFacetID("B");
+        int[] tagIDs = core.get(0, facetID);
+        assertEquals("Doc 0 Facet B should have no tags after open", 
+                     0, tagIDs.length);
     }
 
     public static Test suite() {
