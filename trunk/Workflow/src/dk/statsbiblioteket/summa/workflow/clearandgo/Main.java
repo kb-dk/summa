cf878321384b9b99cf85c621b156034e70bdb838
mke <mke@6c0f5c6f-88b2-4050-bb98-6ed3ea504abe>
2008-10-21 09:43:39 +0000

    Add Workflow module. It compiles a summa-workflow.zip with a summa-workflow-clearandgo.jar. The Workflow module is not included in the global build target yet.
    Fix typo in summa.ilib.control.api in build.properties

---
+++ trunk/Workflow/src/dk/statsbiblioteket/summa/workflow/clearandgo/Main.java
@@ -0,0 +1,12 @@
+package dk.statsbiblioteket.summa.workflow.clearandgo;
+
+/**
+ * Main class for the <i>Clear and Go</i> workflow.
+ */
+public class Main {
+
+    public static void main (String[] args) {
+        
+    }
+
+}
