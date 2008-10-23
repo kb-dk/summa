46c9c705d40b838b9868d85c8de60c8975abe9d2
Mikkel Kamstrup Erlandsen (defunct, work profile) <mkamstrup@users.noreply.github.com>
2008-10-23 13:34:14 +0000

    Include Workflow module in global build
    Fix compilation of summa-workflow.jar
    
    Rewrite Main to be able to cope with both indirect workflow loading (via the summa.configuration system property) or via a command line arg naming a url, resource, rmi target, or file

--- Workflow/src/dk/statsbiblioteket/summa/workflow/Main.java
+++ Workflow/src/dk/statsbiblioteket/summa/workflow/Main.java
@@ -1,17 +1,51 @@
 package dk.statsbiblioteket.summa.workflow;
 
 import dk.statsbiblioteket.summa.common.configuration.Configuration;
+import dk.statsbiblioteket.summa.common.configuration.storage.XStorage;
+
+import java.io.IOException;
 
 /**
  * Main class for running workflows. It will instantiate a
- * {@link WorkflowManager} based on a configuration obtained from
- * {@link Configuration#getSystemConfiguration(boolean)}
- * with {@code allowUnset=true}
+ * {@link WorkflowManager} based on a configuration obtained from:
+ * <ul>
+ *   <li>The system property {@link Configuration#CONF_CONFIGURATION_PROPERTY}, or</li>
+ *   <li>The first command line argument (only one argument allowed). A workflow
+ *       defined this way may be specified as described in
+ *       {@link Configuration#load}</li>
+ * </ul>
+ * If a workflow is specified both ways the program will exit with an error.
  */
 public class Main {
 
     public static void main (String[] args) {
-        Configuration conf = Configuration.getSystemConfiguration(true);
+        if (args.length > 1) {
+            System.err.println("Too many arguments. Only one or zero arguments "
+                               + "allowed. Bailing out");
+            System.exit(1);
+        }
+
+        Configuration conf = null;
+
+        if (args.length == 1) {
+            if (System.getProperty(Configuration.CONF_CONFIGURATION_PROPERTY) != null) {
+                System.err.println("Both system property "
+                                   + Configuration.CONF_CONFIGURATION_PROPERTY
+                                   + " and command line argument present. "
+                                   + "Bailing out");
+                System.exit(3);
+            }
+
+            try {
+                conf = Configuration.load(args[0]);
+            } catch (Exception e) {
+                System.err.println("Error reading workflow '" + args[0] + "': "
+                                   +e.getMessage() +". Bailing out");
+                System.exit(2);
+            }
+        } else {
+            conf  = Configuration.getSystemConfiguration();
+        }
 
         WorkflowStep core = new WorkflowManager(conf);
         core.run();
