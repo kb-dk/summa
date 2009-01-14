4734e57e28f9fe4e950791910e3486babac7d7d0
Toke Eskildsen <te@ekot.dk>
2009-01-14 13:39:18 +0000

    Slightly cleaner configuration-handling in RMISearcherProxy

--- Search/src/dk/statsbiblioteket/summa/search/rmi/RMISearcherProxy.java
+++ Search/src/dk/statsbiblioteket/summa/search/rmi/RMISearcherProxy.java
@@ -37,12 +37,14 @@ public class RMISearcherProxy extends UnicastRemoteObject
      */
     public static final String CONF_REGISTRY_PORT =
                                              "summa.searcher.rmi.registry.port";
+    public static final int DEFAULT_REGISTRY_PORT = 28000;
 
     /**
      * Configuration property defining the name under which the searcher should
      * run. Default is 'summa-searcher'.
      */
     public static final String CONF_SERVICE_NAME = "summa.searcher.rmi.name";
+    public static final String DEFAULT_SERVICE_NAME = "summa-searcher";
 
     public static final Class<? extends SummaSearcher> DEFAULT_BACKEND =
                                                         SummaSearcherImpl.class;
@@ -108,8 +110,8 @@ public class RMISearcherProxy extends UnicastRemoteObject
         backend = SummaSearcherFactory.createSearcher (backendConf);
         log.trace ("Created searcher: " + backend.getClass().getName());
 
-        serviceName = conf.getString (CONF_SERVICE_NAME, "summa-searcher");
-        registryPort = conf.getInt(CONF_REGISTRY_PORT, 28000);
+        serviceName = conf.getString (CONF_SERVICE_NAME, DEFAULT_SERVICE_NAME);
+        registryPort = conf.getInt(CONF_REGISTRY_PORT, DEFAULT_REGISTRY_PORT);
         
         RemoteHelper.exportRemoteInterface (this, registryPort, serviceName);
 
