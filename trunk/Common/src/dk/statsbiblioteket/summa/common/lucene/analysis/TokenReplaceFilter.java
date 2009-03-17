3714e2ba032dd438ba433d688ed16090a68b6fc2
mke <mke@6c0f5c6f-88b2-4050-bb98-6ed3ea504abe>
2009-03-17 13:52:32 +0000

    Implement a TokenReplaceFilter with unit tests. This is going to deprecate TokenMasker sooner or later

---
+++ trunk/Common/src/dk/statsbiblioteket/summa/common/lucene/analysis/TokenReplaceFilter.java
@@ -0,0 +1,65 @@
+package dk.statsbiblioteket.summa.common.lucene.analysis;
+
+import org.apache.lucene.analysis.TokenFilter;
+import org.apache.lucene.analysis.TokenStream;
+import org.apache.lucene.analysis.Token;
+
+import java.util.Map;
+import java.util.HashMap;
+import java.io.IOException;
+
+/**
+ *  A {@code TokenFilter} replacing only entire tokens, not substrings
+ * inside tokens.
+ */
+public class TokenReplaceFilter extends TokenFilter {
+
+    public static final String DEFAULT_REPLACE_RULES =
+            "'c++' > 'cplusplus';"
+            + "'c#' > 'csharp';"
+            + " 'c* algebra' > 'cstaralgebra';"
+            + " 'c*-algebra > 'cstaralgebra'";
+
+    protected Map<String,String> tokenMap;
+
+    /**
+     * Construct a token stream filtering the given input.
+     */
+    public TokenReplaceFilter(TokenStream input,
+                              Map<String,String> tokenMap) {
+        super(input);
+        this.tokenMap = tokenMap;
+    }
+
+    /**
+     * Create a token replacer with the {@link #DEFAULT_REPLACE_RULES}
+     * @param input the token stream to read tokens from
+     */
+    public TokenReplaceFilter(TokenStream input) {
+        this(input, DEFAULT_REPLACE_RULES);
+    }
+
+    public TokenReplaceFilter(TokenStream input, String rules) {
+        this(input, RuleParser.parse(rules));
+    }
+
+    @Override
+    public Token next(Token tok) throws IOException {
+        tok = input.next(tok);
+
+        if (tok == null) {
+            return null;
+        }
+
+        String val = tokenMap.get(tok.term());
+
+        if (val != null) {
+            tok.setTermBuffer(val);
+        }
+
+        return tok;
+    }
+
+
+
+}
