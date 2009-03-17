b74020597cd3514abf35f936e1eb9e262ae3251a
Mikkel Kamstrup Erlandsen (defunct, work profile) <mkamstrup@users.noreply.github.com>
2009-03-17 13:52:32 +0000

    Implement a TokenReplaceFilter with unit tests. This is going to deprecate TokenMasker sooner or later

---
+++ Common/test/dk/statsbiblioteket/summa/common/lucene/analysis/TokenReplaceFilterTest.java
@@ -0,0 +1,60 @@
+package dk.statsbiblioteket.summa.common.lucene.analysis;
+
+import junit.framework.TestCase;
+import org.apache.lucene.analysis.TokenStream;
+import org.apache.lucene.analysis.Token;
+import org.apache.lucene.analysis.WhitespaceTokenizer;
+import org.apache.lucene.analysis.Tokenizer;
+
+import java.io.StringReader;
+
+/**
+ * Unit tests for {@link TokenReplaceFilter}
+ */
+public class TokenReplaceFilterTest extends TestCase {
+
+    TokenReplaceFilter filter;
+    TokenStream stream;
+    Token tok;
+
+    static void assertTokens(TokenStream tokenizer, String... tokens)
+                                                               throws Exception{
+        Token tok = new Token();
+        int count = 0;
+
+        while ((tok = tokenizer.next(tok)) != null) {
+            if (count >= tokens.length) {
+                fail("Too many tokens from tokenizer, found " + (count+1)
+                     + ". Expected " + tokens.length + ".");
+            }
+
+            assertEquals("Mismatch in token number " + (count + 1) + ":",
+                         tokens[count], tok.term());
+            count++;
+        }
+
+        assertEquals("To few tokens from tokenizer, found " + count
+                     + ". Expected " + tokens.length + ".",
+                     tokens.length, count);
+    }
+
+    static TokenStream getStream(String text) {
+        return new WhitespaceTokenizer(new StringReader(text));
+    }
+
+    public void testDummy() throws Exception {
+        stream = getStream("foo");
+        assertTokens(stream, "foo");
+    }
+
+    public void testSingleTokenNoReplace() throws Exception {
+        filter = new TokenReplaceFilter(getStream("foo"));
+        assertTokens(filter, "foo");
+    }
+
+    public void testSingleTokenCPlusPlusReplace() throws Exception {
+        filter = new TokenReplaceFilter(getStream("c++"));
+        assertTokens(filter, "cplusplus");
+    }
+
+}
