c0017070d0f472358a8492925136afd6d20754b5
Mikkel Kamstrup Erlandsen (defunct, work profile) <mkamstrup@users.noreply.github.com>
2009-03-18 09:56:06 +0000

    Make StorageRunner.main() block forever after constructing the storage. Also use commons-logging instead of stdout.

--- Storage/src/dk/statsbiblioteket/summa/storage/tools/StorageRunner.java
+++ Storage/src/dk/statsbiblioteket/summa/storage/tools/StorageRunner.java
@@ -4,9 +4,11 @@ import dk.statsbiblioteket.summa.storage.api.Storage;
 import dk.statsbiblioteket.summa.storage.api.StorageFactory;
 import dk.statsbiblioteket.summa.common.configuration.Configuration;
 import dk.statsbiblioteket.util.qa.QAInfo;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 
 /**
- * A small debug utility to launch a Storage instance
+ * A small utility to launch a Storage instance
  */
 @QAInfo(level = QAInfo.Level.NORMAL,
         state = QAInfo.State.QA_NEEDED,
@@ -14,10 +16,20 @@ import dk.statsbiblioteket.util.qa.QAInfo;
 public class StorageRunner {
 
     public static void main (String[] args) throws Exception {
+        Log log = LogFactory.getLog(StorageRunner.class);
         Configuration conf = Configuration.getSystemConfiguration(true);        
 
-        System.out.println ("Creating Storage instance");
+        log.info("Creating storage instance");
         Storage storage = StorageFactory.createStorage(conf);
+
+        log.info("Storage is running");
+
+        // Block indefinitely (non-busy)
+        while(true) {
+            synchronized (conf) {
+                conf.wait();
+            }
+        }
     }
 
 }
