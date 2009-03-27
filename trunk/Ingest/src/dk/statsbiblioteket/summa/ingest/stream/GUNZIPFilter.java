43aaad540d311c74f365dc38eee03b6b81ced2aa
toke <toke@6c0f5c6f-88b2-4050-bb98-6ed3ea504abe>
2009-03-27 10:40:35 +0000

    Refactoring: Changed GUNZIPFilter to extend ObjectFilterImpl Enhancement: Avoided cloning of Payloads in GUNZIPFilter - just wrap the Stream in the existing Payload instead

--- trunk/Ingest/src/dk/statsbiblioteket/summa/ingest/stream/GUNZIPFilter.java
+++ trunk/Ingest/src/dk/statsbiblioteket/summa/ingest/stream/GUNZIPFilter.java
@@ -5,13 +5,13 @@
 package dk.statsbiblioteket.summa.ingest.stream;
 
 import java.io.IOException;
-import java.util.NoSuchElementException;
 import java.util.zip.GZIPInputStream;
 
 import dk.statsbiblioteket.summa.common.configuration.Configuration;
-import dk.statsbiblioteket.summa.common.filter.Filter;
 import dk.statsbiblioteket.summa.common.filter.Payload;
-import dk.statsbiblioteket.summa.common.filter.object.ObjectFilter;
+import dk.statsbiblioteket.summa.common.filter.object.ObjectFilterImpl;
+import dk.statsbiblioteket.summa.common.filter.object.PayloadException;
+import dk.statsbiblioteket.summa.common.Logging;
 import dk.statsbiblioteket.util.qa.QAInfo;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -23,91 +23,33 @@ import org.apache.commons.logging.LogFactory;
         state = QAInfo.State.IN_DEVELOPMENT,
         author = "te",
         comment="Needs basic unit testing")
-public class GUNZIPFilter implements ObjectFilter {
+public class GUNZIPFilter extends ObjectFilterImpl {
     private static Log log = LogFactory.getLog(GUNZIPFilter.class);
-
-    private ObjectFilter source;
-    private Payload lastPayload = null;
 
     /**
      * The GUNZIPFilter does not use the provided configuration for anything.
      * @param configuration setup for GUNZIPFilter.
      */
     public GUNZIPFilter(Configuration configuration) {
+        super(configuration);
         log.debug("Filter created");
     }
 
-    public boolean hasNext() {
-        if (source == null) {
-            log.warn("hasNext() called with empty source");
-            return false;
+    @Override
+    protected boolean processPayload(Payload payload) throws PayloadException {
+        if (payload.getStream() == null) {
+            log.debug("No Stream present in " + payload);
+            throw new PayloadException("No Stream present", payload);
         }
-        return source.hasNext();
-    }
-
-    /**
-     * Requests the next payload from the source, clones it and replaces the
-     * stream with new GZIPInputStream(oldStream).
-     * @return the source payload, with stream wrapped in gunzip.
-     */
-    public synchronized Payload next() {
-        if (source == null) {
-            log.warn("next() called with empty source");
-            throw new NoSuchElementException("No source defined, so no payloads"
-                                             + " can be returned");
-        }
-        lastPayload = source.next().clone();
-        while (lastPayload.getStream() == null) {
-            log.error("No stream found in Payload '" + lastPayload
-                      + "'. Skipping to next payload");
-            lastPayload = source.next().clone();
-        }
+        log.debug("Wrapping Stream in " + payload + " in GZIPInputStream");
+        Logging.logProcess(getName(), "Wrapping Stream in GZIPInputStream",
+                           Logging.LogLevel.TRACE, payload);
         try {
-            log.trace("Wrapping stream in GZIPInputStream");
-            lastPayload.setStream(new GZIPInputStream(lastPayload.getStream()));
+            payload.setStream(new GZIPInputStream(payload.getStream()));
         } catch (IOException e) {
-            throw new RuntimeException("Unable to create GZIPInputstream", e);
+            throw new PayloadException("Unable to create GZIPInputstream",
+                                       e, payload);
         }
-        return lastPayload;
-    }
-
-    public void remove() {
-        log.warn("Remove not supported");
-    }
-
-    public void setSource(Filter filter) {
-        if (filter instanceof ObjectFilter) {
-            source = (ObjectFilter)filter;
-        } else {
-            throw new IllegalArgumentException("This filter only accepts "
-                                               + "ObjectFilter as source. "
-                                               + "Got " + filter.getClass());
-        }
-    }
-
-    /**
-     * Pumps the last delivered payload empty, then moved onto the next payload
-     * until there are no payloads left.
-     * @return true if pumping should continue.
-     * @throws IOException in case of a read error.
-     */
-    public boolean pump() throws IOException {
-        if (source == null) {
-            log.warn("pump() called with empty source");
-            return false;
-        }
-        return !(lastPayload == null || lastPayload.getStream().read() == -1)
-               || hasNext() && next() != null;
-    }
-
-    public void close(boolean success) {
-        if (source == null) {
-            log.warn("close() called with empty source");
-            return;
-        }
-        source.close(true);
+        return true;
     }
 }
-
-
-
