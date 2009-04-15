a13548911a43b680ae2b1428e6b5771c487216be
Toke Eskildsen <te@ekot.dk>
2009-04-15 08:26:49 +0000

    Fixed all sh-scripts to be callable from arbitrary paths

--- Samples/Summix/bin/summa-storage.sh
+++ Samples/Summix/bin/summa-storage.sh
@@ -154,9 +154,9 @@ if [ "$ENABLE_JMX" == "true" ]; then
     JMX="$JMX_PORT $JMX_SSL $JMX_PASS $JMX_ACCESS";
 fi;
 
-pushd lib > /dev/null
-CODEBASE="-Djava.rmi.server.codebase=$CODEBASE_BASEURL/$(ls summa-common-*.jar) $CODEBASE_BASEURL/$(ls summa-storage-api-*.jar)"
-popd
+#pushd lib > /dev/null
+#CODEBASE="-Djava.rmi.server.codebase=$CODEBASE_BASEURL/$(ls summa-common-*.jar) $CODEBASE_BASEURL/$(ls summa-storage-api-*.jar)"
+#popd
 
 COMMAND="$JVM_OPTS $SECURITY_POLICY $JMX -cp $CLASSPATH $MAINCLASS"
 
