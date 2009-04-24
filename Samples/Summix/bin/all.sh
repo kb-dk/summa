ecbcf3500d0463fa074ed8e76f4cbdc7d48d69e1
Mikkel Kamstrup Erlandsen (defunct, work profile) <mkamstrup@users.noreply.github.com>
2009-04-24 12:05:26 +0000

    Import 'all.sh' into Summix

---
+++ Samples/Summix/bin/all.sh
@@ -0,0 +1,67 @@
+#! /bin/bash
+
+#
+# Run all Summix scripts and set up a complete Summa
+#
+
+#
+# Command used for detaching a process from the parent shell
+#
+DETACH="setsid"
+STORAGE="bin/summa-storage.sh config/storage.xml"
+INGEST="bin/summa-filter.sh config/ingest.xml"
+INDEX="bin/summa-filter.sh config/indexer.xml"
+SEARCHER="bin/summa-searcher.sh config/searcher.xml"
+
+pushd "$(dirname $0)/.." > /dev/null
+
+#
+# $1: Name
+# $2: Command
+#
+function await() {
+    echo -n "[$(date)] Waiting for $1 ... "
+    if [ "$(eval pgrep -f \"$2\")" != "" ]; then
+        echo "Already running, BAD"
+        exit 1
+    fi
+    $2
+    if [ "$?" == "0" ]; then
+        echo "OK"
+    else
+        echo "FAILED"
+        exit 2
+    fi        
+}
+
+#
+# $1: Name
+# $2: Command
+#
+function detach() {
+    echo -n "[$(date)] Starting $1 ... "
+    if [ "$(eval pgrep -f \"$2\")" != "" ]; then
+        echo "Already up, OK"
+        return
+    fi
+    $DETACH $2 &
+    sleep 3
+    if [ "$(eval pgrep -f \"$2\")" != "" ]; then
+        echo "OK"
+    else
+        echo "FAILED"
+        exit 2
+    fi        
+}
+
+function all_good() {
+    echo "All good. Run 'bin/search-tool.sh search.document.query=bar' as a test query"
+}
+
+detach storage "$STORAGE"
+await ingest "$INGEST"
+await index "$INDEX"
+detach searcher "$SEARCHER"
+
+all_good
+
