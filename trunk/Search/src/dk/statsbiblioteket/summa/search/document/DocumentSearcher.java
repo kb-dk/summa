722dd3916c7273d5d1c5126549b92b65768eed6a
mke <mke@6c0f5c6f-88b2-4050-bb98-6ed3ea504abe>
2009-04-24 09:38:01 +0000

    Twiddle javadoc

--- trunk/Search/src/dk/statsbiblioteket/summa/search/document/DocumentSearcher.java
+++ trunk/Search/src/dk/statsbiblioteket/summa/search/document/DocumentSearcher.java
@@ -35,8 +35,7 @@ import java.rmi.RemoteException;
  * from a corpus of documents. The base example is a Lucene searcher.
  * </p><p>
  * All constants prefixed with SEARCH maps directly to from {@link Request} to
- * {@link #fullSearch( String, String, long, long, String, boolean, String[],
- * String[])}. The only mandatory value is {@link #SEARCH_QUERY}.
+ * {@link #fullSearch}. The only mandatory value is {@link #SEARCH_QUERY}.
  */
 @QAInfo(level = QAInfo.Level.NORMAL,
         state = QAInfo.State.IN_DEVELOPMENT,
