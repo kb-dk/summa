19f6e559144f109b475dc5fa246588974fdaccea
Mikkel Kamstrup Erlandsen (defunct, work profile) <mkamstrup@users.noreply.github.com>
2009-05-01 13:02:50 +0000

    In JStorage: If the looked up value is a closure then execute it and return the closure's return value
    Bugfix: Make Configuration more forgiving when parsing ints and longs. Namely fractnional numbers are now allowed, and will be rounded down to the nearest integer
    
    Sanitize messages from ConfigurationStorageTestCase

--- Common/test/dk/statsbiblioteket/summa/common/configuration/JStorageTest.java
+++ Common/test/dk/statsbiblioteket/summa/common/configuration/JStorageTest.java
@@ -10,4 +10,20 @@ public class JStorageTest extends ConfigurationStorageTestCase {
     public JStorageTest() {
         super(new JStorage());
     }
+
+    public void testLoadResource() throws Exception {
+        JStorage js = new JStorage("configuration.js");
+        Configuration conf = new Configuration(js);
+
+        assertFalse(conf.valueExists("summa.nonexisting"));
+
+        assertEquals(27, conf.getInt("summa.test.int"));
+        assertEquals(27, conf.getLong("summa.test.int"));
+
+        assertTrue(conf.getBoolean("summa.test.boolean"));
+
+        assertEquals(1, conf.getInt("summa.test.closure"));
+        assertEquals(2, conf.getInt("summa.test.closure"));
+        assertEquals(3, conf.getInt("summa.test.closure"));
+    }
 }
