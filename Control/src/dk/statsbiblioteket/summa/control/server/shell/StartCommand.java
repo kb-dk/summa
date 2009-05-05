c761a5a6c13c5637849a2916a63efeb33ce5e35f
Mikkel Kamstrup Erlandsen (defunct, work profile) <mkamstrup@users.noreply.github.com>
2009-05-05 12:38:37 +0000

    Bugfix: In the 'start' command of control-shell don't try to start a client if it is already running

--- Control/src/dk/statsbiblioteket/summa/control/server/shell/StartCommand.java
+++ Control/src/dk/statsbiblioteket/summa/control/server/shell/StartCommand.java
@@ -4,8 +4,7 @@ import dk.statsbiblioteket.summa.common.configuration.Configuration;
 import dk.statsbiblioteket.summa.common.rpc.RemoteHelper;
 import dk.statsbiblioteket.summa.common.shell.Command;
 import dk.statsbiblioteket.summa.common.shell.ShellContext;
-import dk.statsbiblioteket.summa.control.api.ControlConnection;
-import dk.statsbiblioteket.summa.control.api.ClientDeployer;
+import dk.statsbiblioteket.summa.control.api.*;
 import dk.statsbiblioteket.summa.control.api.feedback.rmi.RemoteConsoleFeedback;
 import dk.statsbiblioteket.summa.control.api.feedback.rmi.RemoteFeedback;
 import dk.statsbiblioteket.summa.control.server.ControlUtils;
@@ -13,6 +12,8 @@ import dk.statsbiblioteket.util.rpc.ConnectionContext;
 import dk.statsbiblioteket.util.rpc.ConnectionManager;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+
+import java.io.IOException;
 
 /**
  * Control shell command for starting a Client.
@@ -56,6 +57,11 @@ public class StartCommand extends Command {
         }
 
         String instanceId = args[0];
+
+        if (isClientRunning(instanceId)) {
+            ctx.info("Client '" + instanceId + "' is already running");
+            return;
+        }
 
         String transport = getOption("t") != null ? getOption("t") : "ssh";
         transport = ControlUtils.getDeployerClassName(transport);
@@ -109,6 +115,29 @@ public class StartCommand extends Command {
             }
         }
 
+    }
+
+    private boolean isClientRunning(String instanceId) {
+        ConnectionContext<ControlConnection> connCtx = cm.get(controlAddress);
+        ControlConnection control = connCtx.getConnection();
+        try {
+            ClientConnection client = control.getClient(instanceId);
+
+            if (client != null) {
+                log.debug("Client is already running");
+                return true;
+            }
+        } catch (InvalidClientStateException e){
+            return false;
+        } catch (IOException e) {
+            throw new InvalidClientStateException(instanceId,
+                                                  "Connection to '" + instanceId
+                                                  + "' is broken: "
+                                                  + e.getMessage(), e);
+        } finally {
+            connCtx.unref();
+        }
+        return false;
     }
 }
 
