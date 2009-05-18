07510b06831572b98c222c1add8185772d069901
Mikkel Kamstrup Erlandsen (defunct, work profile) <mkamstrup@users.noreply.github.com>
2009-05-18 07:07:54 +0000

    Feature: Allow fetching status of deployed services from the Control shell. Just pass clientId/serviceId to the status command in the Control shell

--- Control/src/dk/statsbiblioteket/summa/control/server/shell/StatusCommand.java
+++ Control/src/dk/statsbiblioteket/summa/control/server/shell/StatusCommand.java
@@ -5,6 +5,8 @@ import dk.statsbiblioteket.summa.common.shell.RemoteCommand;
 import dk.statsbiblioteket.summa.common.shell.ShellContext;
 import dk.statsbiblioteket.util.rpc.ConnectionManager;
 
+import java.io.IOException;
+
 /**
  *
  */
@@ -14,11 +16,12 @@ public class StatusCommand extends RemoteCommand<ControlConnection> {
 
     public StatusCommand(ConnectionManager<ControlConnection> connMgr,
                          String controlAddress) {
-        super("status", "Print the status of the control server, or query the " +
-              "status of deployed clients",
+        super("status", "Print the status of the control server, query the " +
+              "status of deployed clients, or query the status of deployed " +
+              "services",
               connMgr);
 
-        setUsage("status [client_id]...");
+        setUsage("status [clientId|clientId/serviceId]...");
 
         this.controlAddress = controlAddress;
     }
@@ -35,21 +38,56 @@ public class StatusCommand extends RemoteCommand<ControlConnection> {
             } else {
                 ctx.info ("Status of clients:");
                 for (String target : targets) {
-                    String status;
-                    try {
-                        ClientConnection client = control.getClient(target);
-                        status = "" + client.getStatus();
-                    } catch (InvalidClientStateException e) {
-                        status = e.getMessage();
-                    } catch (NoSuchClientException e) {
-                        status = "No such client";
+                    String[] parsedTarget = target.split("/");
+
+                    if (parsedTarget.length == 1) {
+                        printClientStatus(ctx, control, parsedTarget[0]);
+                    } else if (parsedTarget.length == 2){
+                        printServiceStatus(ctx, control,
+                                           parsedTarget[0], parsedTarget[1]);
+                    } else {
+                        ctx.warn("Malformed client or service id: " + target);
                     }
-                    ctx.info("\t" + target + ": " + status);
                 }
             }
         } finally {
             releaseConnection();
         }
+    }
+
+    private void printClientStatus(ShellContext ctx, ControlConnection control,
+                                   String clientId) throws IOException {
+        String status;
+        try {
+            ClientConnection client = control.getClient(clientId);
+            status = "" + client.getStatus();
+        } catch (InvalidClientStateException e) {
+            status = e.getMessage();
+        } catch (NoSuchClientException e) {
+            status = "No such client";
+        }
+        ctx.info("\t" + clientId + ": " + status);
+    }
+
+    private void printServiceStatus(ShellContext ctx, ControlConnection control,
+                                    String clientId, String serviceId)
+                                                            throws IOException {
+        String status;
+        try {
+            ClientConnection client = control.getClient(clientId);
+            Service service = client.getServiceConnection(serviceId);
+            status = service.getStatus().toString();
+        } catch (InvalidServiceStateException e){
+            status = e.getMessage();
+        } catch (NoSuchServiceException e) {
+            status = "No such service '" + serviceId + "'";
+        } catch (InvalidClientStateException e) {
+            status = "Client '" + clientId + "' not running";
+        } catch (NoSuchClientException e) {
+            status = "No such client '" + clientId + "'";
+        }
+        ctx.info("\t" + clientId + "/" + serviceId + ": " + status);
+
     }
 }
 
