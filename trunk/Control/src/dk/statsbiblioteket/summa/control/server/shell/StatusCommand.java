8ef0f6a9d1fcb98cfbe51faab4e6ba907763af93
mke <mke@6c0f5c6f-88b2-4050-bb98-6ed3ea504abe>
2009-05-19 12:17:22 +0000

    Add formatted output of the 'status' command on control shell
    Column name tweaks in status command

--- trunk/Control/src/dk/statsbiblioteket/summa/control/server/shell/StatusCommand.java
+++ trunk/Control/src/dk/statsbiblioteket/summa/control/server/shell/StatusCommand.java
@@ -3,6 +3,7 @@ package dk.statsbiblioteket.summa.control.server.shell;
 import dk.statsbiblioteket.summa.control.api.*;
 import dk.statsbiblioteket.summa.common.shell.RemoteCommand;
 import dk.statsbiblioteket.summa.common.shell.ShellContext;
+import dk.statsbiblioteket.summa.common.shell.Layout;
 import dk.statsbiblioteket.util.rpc.ConnectionManager;
 
 import java.io.IOException;
@@ -21,6 +22,11 @@ public class StatusCommand extends RemoteCommand<ControlConnection> {
               "services",
               connMgr);
 
+        installOption("f", "formatted", false, "Print strictly formatted output"
+                                               + " for machine parsing. Format "
+                                               + "is: "
+                                               + "'client/service | statusCode | message'");
+
         setUsage("status [clientId|clientId/serviceId]...");
 
         this.controlAddress = controlAddress;
@@ -28,65 +34,92 @@ public class StatusCommand extends RemoteCommand<ControlConnection> {
 
     public void invoke(ShellContext ctx) throws Exception {
         ControlConnection control = getConnection(controlAddress);
-
+        Layout layout = new Layout("Client/Service");
         String[] targets = getArguments();
+
+        if (hasOption("formatted")) {
+            layout.appendColumns("StatusCode", "Message");
+            layout.setPrintHeaders(false);
+            layout.setDelimiter(" | ");
+        } else {
+            layout.appendColumns("Message");
+        }
 
         try {
             if (targets.length == 0) {
                 Status status = control.getStatus();
                 ctx.info("Control status: " + status.toString());
             } else {
-                ctx.info ("Status of clients:");
                 for (String target : targets) {
                     String[] parsedTarget = target.split("/");
 
                     if (parsedTarget.length == 1) {
-                        printClientStatus(ctx, control, parsedTarget[0]);
+                        listClientStatus(layout, control, parsedTarget[0]);
                     } else if (parsedTarget.length == 2){
-                        printServiceStatus(ctx, control,
-                                           parsedTarget[0], parsedTarget[1]);
+                        listServiceStatus(layout, control,
+                                          parsedTarget[0], parsedTarget[1]);
                     } else {
                         ctx.warn("Malformed client or service id: " + target);
                     }
                 }
+                ctx.info(layout.toString());
             }
         } finally {
             releaseConnection();
         }
     }
 
-    private void printClientStatus(ShellContext ctx, ControlConnection control,
+    private void listClientStatus(Layout layout, ControlConnection control,
                                    String clientId) throws IOException {
-        String status;
+        String statusCode;
+        String msg;
         try {
             ClientConnection client = control.getClient(clientId);
-            status = "" + client.getStatus();
+            Status status =  client.getStatus();
+            statusCode = status.getCode().toString();
+            msg = status.toString();
         } catch (InvalidClientStateException e) {
-            status = e.getMessage();
+            statusCode = Status.CODE.not_instantiated.toString();
+            msg = "Not running";
         } catch (NoSuchClientException e) {
-            status = "No such client";
+            statusCode = Status.CODE.not_instantiated.toString();
+            msg = "No such client";
         }
-        ctx.info("\t" + clientId + ": " + status);
+
+        layout.appendRow("Client/Service", clientId,
+                         "StatusCode", statusCode,
+                         "Message", msg);
     }
 
-    private void printServiceStatus(ShellContext ctx, ControlConnection control,
+    private void listServiceStatus(Layout layout, ControlConnection control,
                                     String clientId, String serviceId)
                                                             throws IOException {
-        String status;
+        String statusCode;
+        String msg;
+
         try {
             ClientConnection client = control.getClient(clientId);
             Service service = client.getServiceConnection(serviceId);
-            status = service.getStatus().toString();
+            Status status = service.getStatus();
+            statusCode = status.getCode().toString();
+            msg = status.toString();
         } catch (InvalidServiceStateException e){
-            status = e.getMessage();
+            statusCode = Status.CODE.not_instantiated.toString();
+            msg = e.getMessage();
         } catch (NoSuchServiceException e) {
-            status = "No such service '" + serviceId + "'";
+            statusCode = Status.CODE.not_instantiated.toString();
+            msg = "No such service '" + serviceId + "'";
         } catch (InvalidClientStateException e) {
-            status = "Client '" + clientId + "' not running";
+            statusCode = Status.CODE.not_instantiated.toString();
+            msg = "Client not running";
         } catch (NoSuchClientException e) {
-            status = "No such client '" + clientId + "'";
+            statusCode = Status.CODE.not_instantiated.toString();
+            msg = "No such client";
         }
-        ctx.info("\t" + clientId + "/" + serviceId + ": " + status);
+
+        layout.appendRow("Client/Service", clientId + "/" + serviceId,
+                         "StatusCode", statusCode,
+                         "Message", msg);
 
     }
 }
