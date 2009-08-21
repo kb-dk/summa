abeedc7e4a42904519b0dda0dcc2c8f35b32ede1
mkamstrup <mkamstrup@6c0f5c6f-88b2-4050-bb98-6ed3ea504abe>
2009-08-21 09:32:38 +0000

    Idea project tweaking

--- projects/arctika/trunk/test/dk/statsbiblioteket/summa/arctika/TikaTest.java
+++ projects/arctika/trunk/test/dk/statsbiblioteket/summa/arctika/TikaTest.java
@@ -17,7 +17,7 @@
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
  */
-package dk.statsbiblioteket.summa.support.arc;
+package dk.statsbiblioteket.summa.arctika;
 
 import dk.statsbiblioteket.util.qa.QAInfo;
 import dk.statsbiblioteket.util.Files;
