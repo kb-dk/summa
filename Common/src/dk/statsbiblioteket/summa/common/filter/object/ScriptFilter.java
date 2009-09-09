78cef60910cefadd22f6f312b1f31319a8be3f04
Toke Eskildsen <te@ekot.dk>
2009-09-09 08:48:09 +0000

    Bugfix: The script filter did not resolve the script type properly for external scripts Code style: Reduced multi-line statements  to fewer lines

--- Common/src/dk/statsbiblioteket/summa/common/filter/object/ScriptFilter.java
+++ Common/src/dk/statsbiblioteket/summa/common/filter/object/ScriptFilter.java
@@ -182,51 +182,44 @@ public class ScriptFilter extends ObjectFilterImpl {
                 return conf.getString(CONF_SCRIPT_LANG);
             }
 
-            try {
-                URL url = new URL(conf.getString(CONF_SCRIPT_URL));
-                String urlString = url.toString();
+            URL url = Resolver.getURL(conf.getString(CONF_SCRIPT_URL));
+            String urlString = url.toString();
 
-                if (urlString.indexOf('.') == -1) {
-                    log.warn("Unable to detect script extension from: "
-                              + urlString + ". Using default: "
-                              + DEFAULT_SCRIPT_LANG);
-                    return DEFAULT_SCRIPT_LANG;
-                }
-
-                return urlString.substring(urlString.lastIndexOf('.'));
-
-            } catch (MalformedURLException e) {
-                throw new ConfigurationException("Malformed URL in "
-                                                 + CONF_SCRIPT_URL
-                                                 + ":" + e.getMessage(), e);
+            if (urlString.indexOf('.') == -1) {
+                log.warn("Unable to detect script extension from: "
+                         + urlString + ". Using default: "
+                         + DEFAULT_SCRIPT_LANG);
+                return DEFAULT_SCRIPT_LANG;
             }
+
+            return urlString.substring(urlString.lastIndexOf('.')+1);
         } else if (conf.valueExists(CONF_SCRIPT_INLINE)) {
             return conf.getString(CONF_SCRIPT_LANG, DEFAULT_SCRIPT_LANG);
         } else {
-            throw new ConfigurationException("No URL or inlined script defined."
-                                             + " Please set one of the "
-                                             + CONF_SCRIPT_URL + " or "
-                                             + CONF_SCRIPT_INLINE
-                                             + " properties for this filter");
+            throw new ConfigurationException(String.format(
+                    "No URL or inlined script defined. Please set one of the %s"
+                    + " or %s properties for this filter",
+                    CONF_SCRIPT_URL, CONF_SCRIPT_INLINE));
         }        
     }
 
     private static InputStream readScript(Configuration conf) {
         if (!conf.valueExists(CONF_SCRIPT_URL)
             && !conf.valueExists(CONF_SCRIPT_INLINE)) {
-            throw new ConfigurationException("No URL or inlined script defined." +
-                                             " Please set one of the "
-                                             + CONF_SCRIPT_URL + " or "
-                                             + CONF_SCRIPT_INLINE
-                                             + " properties for this filter");
+            throw new ConfigurationException(String.format(
+                    "No URL or inlined script defined. Please set one of the %s"
+                    + " or %s properties for this filter",
+                    CONF_SCRIPT_URL, CONF_SCRIPT_INLINE));
         }
 
         if (conf.valueExists(CONF_SCRIPT_URL)) {
             if (conf.valueExists(CONF_SCRIPT_INLINE)) {
-                log.error("Both an inlined script and a script URL are defined."
-                          + " Please use only one of " + CONF_SCRIPT_URL
-                          + " or " + CONF_SCRIPT_INLINE + ". Using script" +
-                          "from " + conf.getString(CONF_SCRIPT_URL));
+                log.error(String.format(
+                        "Both an inlined script and a script URL are defined."
+                        + " Please use only one of %s or %s. Using script from"
+                        + " %s",
+                        CONF_SCRIPT_URL, CONF_SCRIPT_INLINE,
+                        conf.getString(CONF_SCRIPT_URL)));
             }
 
             try {
@@ -236,22 +229,21 @@ public class ScriptFilter extends ObjectFilterImpl {
 
                 if (url == null) {
                     throw new ConfigurationException(
-                                             "Unable to locate script: "
-                                             + conf.getString(CONF_SCRIPT_URL));
+                            "Unable to locate script: "
+                            + conf.getString(CONF_SCRIPT_URL));
                 } else {
                     log.debug("Script resolved to: " + url);
                 }
 
                 return url.openStream();
             } catch (MalformedURLException e) {
-                throw new ConfigurationException("Malformed URL in "
-                                                 + CONF_SCRIPT_URL
-                                                 + ":" + e.getMessage(), e);
+                throw new ConfigurationException(String.format(
+                        "Malformed URL in %s: %s",
+                        CONF_SCRIPT_URL, e.getMessage()), e);
             } catch (IOException e) {
-                throw new ConfigurationException("Unable to read script data from"
-                                                 + " URL "
-                                                 + conf.getString(CONF_SCRIPT_URL)
-                                                 + ": " + e.getMessage(), e);
+                throw new ConfigurationException(String.format(
+                        "Unable to read script data from URL '%s': %s",
+                        conf.getString(CONF_SCRIPT_URL), e.getMessage()), e);
             }
         } else {
             assert conf.valueExists(CONF_SCRIPT_INLINE);
@@ -299,8 +291,8 @@ public class ScriptFilter extends ObjectFilterImpl {
             Boolean result = (Boolean) engine.get("allowPayload");
 
             if (result == null) {
-                throw new PayloadException("Script returned null. It must "
-                                           + "return a boolean");
+                throw new PayloadException(
+                        "Script returned null. It must return a boolean");
             }
 
             String message = (String) engine.get("feedbackMessage");
@@ -317,9 +309,9 @@ public class ScriptFilter extends ObjectFilterImpl {
             return result;
             
         } catch (ClassCastException e) {
-            throw new PayloadException("Script did not return a boolean, "
-                                       + "but a "
-                                       + engine.get("allowPayload"));
+            throw new PayloadException(
+                    "Script did not return a boolean, but a "
+                    + engine.get("allowPayload"));
         }
     }
 }
