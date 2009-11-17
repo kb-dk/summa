7d98c9fedd97e1d4e9996c2b6b139ca45497dd02
Toke Eskildsen <te@ekot.dk>
2009-11-17 10:28:50 +0000

    Testing: Added an optional delay for the PayloadFeederHelper to simulate processing time

--- Common/src/dk/statsbiblioteket/summa/common/unittest/PayloadFeederHelper.java
+++ Common/src/dk/statsbiblioteket/summa/common/unittest/PayloadFeederHelper.java
@@ -19,17 +19,16 @@
  */
 package dk.statsbiblioteket.summa.common.unittest;
 
-import dk.statsbiblioteket.util.qa.QAInfo;
-import dk.statsbiblioteket.summa.common.filter.object.ObjectFilter;
-import dk.statsbiblioteket.summa.common.filter.Payload;
 import dk.statsbiblioteket.summa.common.filter.Filter;
-
-import java.util.List;
-import java.util.ArrayList;
-import java.io.IOException;
-
+import dk.statsbiblioteket.summa.common.filter.Payload;
+import dk.statsbiblioteket.summa.common.filter.object.ObjectFilter;
+import dk.statsbiblioteket.util.qa.QAInfo;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Takes a list of Payloads in the constructor and delivers the Payloads when
@@ -42,12 +41,23 @@ public class PayloadFeederHelper implements ObjectFilter {
     private static Log log = LogFactory.getLog(PayloadFeederHelper.class);
 
     private List<Payload> payloads;
+    private int delay = 0;
+    private Long lastDeliveryTime = 0L;
     
     public PayloadFeederHelper(List<Payload> payloads) {
         //noinspection DuplicateStringLiteralInspection
         log.debug("Creating feeder with " + payloads.size() + " Payloads");
         this.payloads = new ArrayList<Payload>(payloads.size());
         this.payloads.addAll(payloads);
+    }
+
+    public PayloadFeederHelper(
+            List<Payload> payloads, int delayBetweenPayloads) {
+        //noinspection DuplicateStringLiteralInspection
+        log.debug("Creating feeder with " + payloads.size() + " Payloads");
+        this.payloads = new ArrayList<Payload>(payloads.size());
+        this.payloads.addAll(payloads);
+        delay = delayBetweenPayloads;
     }
 
     public boolean hasNext() {
@@ -67,10 +77,19 @@ public class PayloadFeederHelper implements ObjectFilter {
     }
 
     public Payload next() {
+        long sleepTime = lastDeliveryTime + delay - System.currentTimeMillis();
+        if (sleepTime > 0) {
+            try {
+                Thread.sleep(sleepTime);
+            } catch (InterruptedException e) {
+                log.debug("Interrupted while sleeping " + sleepTime + "ms");
+            }
+        }
         Payload payload = !hasNext() ? null : payloads.remove(0);
         if (log.isTraceEnabled()) {
             log.trace("Delivering " + payload);
         }
+        lastDeliveryTime = System.currentTimeMillis();
         return payload;
     }
 
