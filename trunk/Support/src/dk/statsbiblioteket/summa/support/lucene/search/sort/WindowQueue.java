733b5f991138db50d6dd073466d407cbc74458cc
toke-sb <toke-sb@6c0f5c6f-88b2-4050-bb98-6ed3ea504abe>
2009-12-06 20:59:42 +0000

    Added WindowQueue implementation of PriorityQueue

---
+++ trunk/Support/src/dk/statsbiblioteket/summa/support/lucene/search/sort/WindowQueue.java
@@ -0,0 +1,115 @@
+/* $Id$
+ * $Revision$
+ * $Date$
+ * $Author$
+ *
+ * The Summa project.
+ * Copyright (C) 2005-2007  The State and University Library
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+/*
+ * The State and University Library of Denmark
+ * CVS:  $Id$
+ */
+package dk.statsbiblioteket.summa.support.lucene.search.sort;
+
+import dk.statsbiblioteket.util.qa.QAInfo;
+import dk.statsbiblioteket.summa.common.util.PriorityQueue;
+import dk.statsbiblioteket.summa.common.util.ResourceTracker;
+
+import java.util.Comparator;
+
+/**
+ * Holds a limited amount of elements in sorted order, meant for sliding-window
+ * usage with lower and upper bounds for insertion acceptance.
+ * </p><p>
+ * The window-size is controlled by a
+ * {@link dk.statsbiblioteket.summa.common.util.ResourceTracker}.
+ */
+@QAInfo(level = QAInfo.Level.NORMAL,
+        state = QAInfo.State.IN_DEVELOPMENT,
+        author = "te")
+public class WindowQueue<T extends Comparable<? super T>> 
+                                                      extends PriorityQueue<T> {
+    // If defined, only elements higher than this will be accepted
+    private T lowerBound = null;
+    // If defined, only elements lower than this will be accepted
+    private T upperBound = null;
+
+    /**
+     * If no comparator is given, a default comparator is created, which will
+     * use the elements natural order.
+     */
+    private Comparator<T> comparator;
+
+    private ResourceTracker<T> resourceTracker;
+
+    /**
+     * Create a queue with the given constraints.
+     * @param comparator      determines the order. If null, natural ordering
+     *                        is used.
+     * @param lowerBound      elements lower than or equal to this won't be
+     *                        inserted in the queue. Ignored if null.
+     * @param upperBound      elements higher than or equal to this won't be
+     *                        inserted in the queue. Ignored if null.
+     * @param resourceTracker determines the queue size.
+     */
+    public WindowQueue(
+            Comparator<T> comparator,
+            T lowerBound, T upperBound, ResourceTracker<T> resourceTracker) {
+        super(comparator, 10, Integer.MAX_VALUE); // resourceTracker controls
+        this.lowerBound = lowerBound;
+        this.upperBound = upperBound;
+        this.resourceTracker = resourceTracker;
+        this.comparator = comparator;
+        if (comparator == null) {
+            this.comparator = new Comparator<T>() {
+                public int compare(T o1, T o2) {
+                    return o1.compareTo(o2);
+                }
+            };
+        }
+    }
+
+    @Override
+    public T insert(T value) {
+        if (lowerBound != null && comparator.compare(lowerBound, value) >= 0) {
+            return null;
+        }
+        if (upperBound != null && comparator.compare(upperBound, value) <= 0) {
+            return null;
+        }
+        resourceTracker.add(value);
+        T bumped = super.insert(value);
+        if (bumped != null) {
+            resourceTracker.remove(bumped);
+        }
+        // Trim
+        while (getSize() > 0 && resourceTracker.isOverflowing()) {
+            removeMin(); // Lost without a trace
+        }
+        return bumped;
+    }
+
+    @Override
+    public T removeMin() {
+        T min = super.removeMin();
+        if (min != null) {
+            resourceTracker.remove(min);
+        }
+        return min;
+    }
+}
---
+++ trunk/Support/test/dk/statsbiblioteket/summa/support/lucene/search/sort/WindowQueueTest.java
@@ -0,0 +1,102 @@
+package dk.statsbiblioteket.summa.support.lucene.search.sort;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+import junit.framework.TestCase;
+import dk.statsbiblioteket.summa.common.util.StringTracker;
+import dk.statsbiblioteket.util.Strings;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class WindowQueueTest extends TestCase {
+    public WindowQueueTest(String name) {
+        super(name);
+    }
+
+    @Override
+    public void setUp() throws Exception {
+        super.setUp();
+    }
+
+    @Override
+    public void tearDown() throws Exception {
+        super.tearDown();
+    }
+
+    public static Test suite() {
+        return new TestSuite(WindowQueueTest.class);
+    }
+
+    public void testBounds() throws Exception {
+        WindowQueue<String> queue = new WindowQueue<String>(
+                null, "b", "k", new StringTracker(200, 500));
+
+        queue.insert("a");
+        assertEquals("a should be outside of bounds", 0, queue.getSize());
+        queue.insert("b");
+        assertEquals("b should be outside of bounds", 0, queue.getSize());
+        queue.insert("c");
+        assertEquals("c should be inside bounds", 1, queue.getSize());
+        queue.insert("j");
+        assertEquals("j should be inside bounds", 2, queue.getSize());
+        queue.insert("k");
+        assertEquals("k should be outside bounds", 2, queue.getSize());
+    }
+    
+    public void testLimits() throws Exception {
+        WindowQueue<String> queue = new WindowQueue<String>(
+                null, "b", "k", new StringTracker(2, 500));
+        String[] ENTRIES = new String[]{
+                "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l"};
+        for (String entry: ENTRIES) {
+            queue.insert(entry);
+        }
+        assertEquals("There should be the right number of elements",
+                     2, queue.getSize());
+        String big = "c123456789123456";
+        for (int i = 0 ; i < 5 ; i++) {
+            big += big;
+        }
+        queue.insert(big);
+        assertEquals("Big element should push other elements out",
+                     1, queue.getSize());
+    }
+
+    public void testBasicOrder() throws Exception {
+        WindowQueue<String> queue = new WindowQueue<String>(
+                null, null, null, new StringTracker(200, 5000));
+        String[] ENTRIES = new String[]{
+                "b", "a", "c", "d"};
+        for (String entry: ENTRIES) {
+            queue.insert(entry);
+        }
+        String[] EXPECTED = new String[] {"d", "c", "b", "a"};
+        assertEquals("The sorted result should be correct",
+                     Strings.join(EXPECTED, ", "),
+                     Strings.join(getElements(queue), ", "));
+    }
+
+    public void testBoundsOrder() throws Exception {
+        WindowQueue<String> queue = new WindowQueue<String>(
+                null, "b", "k", new StringTracker(2, 500));
+        String[] ENTRIES = new String[]{
+                "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l"};
+        for (String entry: ENTRIES) {
+            queue.insert(entry);
+        }
+        String[] EXPECTED = new String[] {"d", "c"};
+        assertEquals("The sorted result should be correct",
+                     Strings.join(EXPECTED, ", "),
+                     Strings.join(getElements(queue), ", "));
+    }
+
+    private List<String> getElements(WindowQueue<String> queue) {
+        List<String> elements = new ArrayList<String>(queue.getSize());
+        while (queue.getSize() > 0) {
+            elements.add(queue.removeMin());
+        }
+        return elements;
+    }
+
+}
