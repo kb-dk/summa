62214daa98d132eb6cab838dc294a369bdd40095
Mikkel Kamstrup Erlandsen (defunct, work profile) <mkamstrup@users.noreply.github.com>
2010-01-11 10:53:05 +0000

    Add comment headers to all .js batch jobs

--- Storage/test/collect_ids.job.js
+++ Storage/test/collect_ids.job.js
@@ -1,2 +1,6 @@
+/*
+ * This batch job collects all record ids and returns them in a |-separated
+ * list
+ */
 out.print(record.getId())
 out.print("|");
--- Storage/test/delete.job.js
+++ Storage/test/delete.job.js
@@ -1,3 +1,7 @@
+/*
+ * This batch job marks all records as deleted and returns a |-separated
+ * list of the record ids of the affected records
+ */
 commit=true;
 record.setDeleted(true);
 out.print(record.getId());
