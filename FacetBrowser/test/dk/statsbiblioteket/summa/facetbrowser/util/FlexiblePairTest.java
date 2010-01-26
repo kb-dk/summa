8ce8ab0b322873b70ad9f01287cc2c3ba18ea8c7
Mikkel Kamstrup Erlandsen (defunct, work profile) <mkamstrup@users.noreply.github.com>
2010-01-26 14:17:34 +0000

    NOTICE: Convert entire Summa source tree to Apache 2 license

--- FacetBrowser/test/dk/statsbiblioteket/summa/facetbrowser/util/FlexiblePairTest.java
+++ FacetBrowser/test/dk/statsbiblioteket/summa/facetbrowser/util/FlexiblePairTest.java
@@ -1,24 +1,16 @@
-/* $Id: FlexiblePairTest.java,v 1.4 2007/10/05 10:20:23 te Exp $
- * $Revision: 1.4 $
- * $Date: 2007/10/05 10:20:23 $
- * $Author: te $
+/*
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
  *
- * The Summa project.
- * Copyright (C) 2005-2007  The State and University Library
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
  *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
  */
 package dk.statsbiblioteket.summa.facetbrowser.util;
 
@@ -79,6 +71,7 @@ public class FlexiblePairTest extends TestCase {
         return new TestSuite(FlexiblePairTest.class);
     }
 }
+
 
 
 
