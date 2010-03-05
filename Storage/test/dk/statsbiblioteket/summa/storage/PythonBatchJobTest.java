8af25170e07ce55e93e77d944603f9d33eaa6897
Henrik Kirk <hkirk@users.noreply.github.com>
2010-03-05 14:18:10 +0000

    Bugfix: fixed test, as long as no one is creating a python scripting extension to summa, this test shouln't fail.

--- Storage/test/dk/statsbiblioteket/summa/storage/PythonBatchJobTest.java
+++ Storage/test/dk/statsbiblioteket/summa/storage/PythonBatchJobTest.java
@@ -17,6 +17,8 @@ package dk.statsbiblioteket.summa.storage;
 import dk.statsbiblioteket.summa.common.Record;
 import dk.statsbiblioteket.util.qa.QAInfo;
 
+import java.io.*;
+
 /**
  * Test running batch jobs with Jython. Note that Summa requires
  * Jython 2.5.1 or later on the class path. Jython <= 2.5 does not implement
@@ -27,15 +29,21 @@ import dk.statsbiblioteket.util.qa.QAInfo;
  */
 @QAInfo(level = QAInfo.Level.NORMAL,
         state = QAInfo.State.IN_DEVELOPMENT,
-        author = "mke")
+        author = "mke, hbk")
 public class PythonBatchJobTest extends StorageTestBase {
 
     public void testPythonEngine() throws Exception {
         storage.flush(new Record(testId1, testBase1, testContent1));
-        String hello = storage.batchJob(
+        try {
+            String hello = storage.batchJob(
                 "test.job.py", null, 0, Long.MAX_VALUE, null
-        );
-        assertEquals("Hello world!", hello);
+            );
+            assertEquals("Hello world!", hello);
+            fail("Python isn't a know scripting language yet.");
+        } catch(IOException e) {
+            // expected;
+        }
+
     }
 
 }
