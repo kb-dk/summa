923f5a6b8d2c8bd2ce68d068103f245ba13e09c0
Henrik Kirk <hkirk@users.noreply.github.com>
2010-07-21 09:19:33 +0000

    Doc: Updated to Apache License 2.0

--- Ingest/src/dk/statsbiblioteket/summa/ingest/split/ARCRecordStream.java
+++ Ingest/src/dk/statsbiblioteket/summa/ingest/split/ARCRecordStream.java
@@ -1,6 +1,16 @@
-/**
- * Created: te 19-08-2009 23:29:25
- * CVS:     $Id$
+/*
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
  */
 package dk.statsbiblioteket.summa.ingest.split;
 
@@ -18,7 +28,7 @@ import java.io.InputStream;
  * The ARC File Format 1.0 is supported, with the exception of GZIPped content.
  * The user of this stream is responsible for GUNZIPping the ARC file
  * beforehand.
- * @see {@url http://www.archive.org/web/researcher/ArcFileFormat.php}
+ * @see <a hre="http://www.archive.org/web/researcher/ArcFileFormat.php">ArcFileFormat</a>
  */
 // TODO: Implement this class
 @QAInfo(level = QAInfo.Level.NORMAL,
