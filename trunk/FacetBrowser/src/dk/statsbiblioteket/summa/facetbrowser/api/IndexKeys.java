901d14d87e76db473f97088a72764c5fa2df0242
hkirk <hkirk@6c0f5c6f-88b2-4050-bb98-6ed3ea504abe>
2010-07-21 08:36:50 +0000

    Doc: Updated to Apache License 2.0

--- trunk/FacetBrowser/src/dk/statsbiblioteket/summa/facetbrowser/api/IndexKeys.java
+++ trunk/FacetBrowser/src/dk/statsbiblioteket/summa/facetbrowser/api/IndexKeys.java
@@ -1,9 +1,16 @@
-/* $Id$
- * $Revision$
- * $Date$
- * $Author$
+/*
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
  *
- * Copyright 2007 Statsbiblioteket, Denmark
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
  */
 package dk.statsbiblioteket.summa.facetbrowser.api;
 
@@ -34,7 +41,7 @@ import dk.statsbiblioteket.util.qa.QAInfo;
  * </p><p>
  * Example: field=author, term=åse, delta=-2, length=5
  * Result: lars, melanie, åse
- * @see {@link IndexResponse} for the returned result.
+ * @see IndexResponse for the returned result.
  */
 @QAInfo(level = QAInfo.Level.NORMAL,
         state = QAInfo.State.IN_DEVELOPMENT,
@@ -42,7 +49,7 @@ import dk.statsbiblioteket.util.qa.QAInfo;
 public interface IndexKeys {
 
     /**
-     * The conseptual field to perform a lookup on. As this is mapped directly
+     * The conceptual field to perform a lookup on. As this is mapped directly
      * to a facet, the facet must be specified in the IndexDescriptor.
      * </p><p>
      * Mandatory: If no field is specified, no index-lookup is performed.
