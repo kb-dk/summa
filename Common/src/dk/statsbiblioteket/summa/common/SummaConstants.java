b5ec96751a52a7c3b3c6fd8be1ef9aa49e626979
Henrik Kirk <hkirk@users.noreply.github.com>
2010-07-26 07:42:49 +0000

    Prior to release of 1.6.2

--- Common/src/dk/statsbiblioteket/summa/common/SummaConstants.java
+++ Common/src/dk/statsbiblioteket/summa/common/SummaConstants.java
@@ -29,13 +29,13 @@ public class SummaConstants {
      * Current Summa version,
      * Note: Auto generated.
      */
-    public static final String SUMMAVERSION = "1.6.1";
+    public static final String SUMMAVERSION = "1.6.2";
 
     /**
      * Last revision for Summa release,
      * Note: Auto generated.
      */
-    public static final int SUMMAVERSIONREVISION = 2374;
+    public static final int SUMMAVERSIONREVISION = 2434;
 
     /**
      * Get version string, used to present the Summa version and the revision
