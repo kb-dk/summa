ffc01633ce25b4f90164d09e71f4a785c534db5e
Henrik Kirk <hkirk@users.noreply.github.com>
2010-07-29 13:07:34 +0000

    Bugfix: Updated JavaDoc. Changed word 'storages' to 'storage' as 'storages' isn't a english word

--- Common/src/dk/statsbiblioteket/summa/common/configuration/ConfigurationStorage.java
+++ Common/src/dk/statsbiblioteket/summa/common/configuration/ConfigurationStorage.java
@@ -14,13 +14,13 @@
  */
 package dk.statsbiblioteket.summa.common.configuration;
 
+import dk.statsbiblioteket.util.qa.QAInfo;
+
 import java.io.IOException;
 import java.io.Serializable;
-import java.util.Map;
 import java.util.Iterator;
 import java.util.List;
-
-import dk.statsbiblioteket.util.qa.QAInfo;
+import java.util.Map;
 
 /**
  * An abstraction of a storage backend for a {@link Configuration}.
@@ -35,92 +35,95 @@ public interface ConfigurationStorage extends Configurable, Serializable {
 
     /**
      * Store a key-value pair.
-     * @param key the name used to access the stored object
-     * @param value the actual value to store
-     * @throws IOException if there is an error performing the operation
+     * @param key The name used to access the stored object.
+     * @param value The actual value to store.
+     * @throws IOException if there is an error performing the operation.
      */
     public void put (String key, Serializable value) throws IOException;
 
     /**
      * Retrieve a stored item.
-     * @param key name of object to look up
-     * @return the object or null if the object is unknown
-     * @throws IOException if there was an error performing the operation
+     * @param key Name of object to look up.
+     * @return The object or null if the object is unknown.
+     * @throws IOException if there was an error performing the operation.
      */
     public Serializable get (String key) throws IOException;
 
     /**
-     * Get an {@code Iterator} over all key-value pairs
-     * @return an Iterator over all stored key-value pairs
-     * @throws IOException if there was an error performing the operation
+     * Get an {@code Iterator} over all key-value pairs.
+     * @return An Iterator over all stored key-value pairs.
+     * @throws IOException if there was an error performing the operation.
      */
-    public Iterator<Map.Entry<String,Serializable>> iterator () throws IOException;
+    public Iterator<Map.Entry<String,Serializable>> iterator ()
+                                                             throws IOException;
 
     /**
-     * Remove a property from the storage
-     * @param key the name of the value to remove from the storage
-     * @throws IOException if there was an error performing the operation
+     * Remove a property from the storage.
+     * @param key The name of the value to remove from the storage.
+     * @throws IOException if there was an error performing the operation.
      */
     public void purge (String key) throws IOException;
 
     /**
-     * Get the number of properties in the configuration
-     * @return number of properties stored
-     * @throws IOException if there was an error performing the operation
+     * Get the number of properties in the configuration.
+     * @return Number of properties stored.
+     * @throws IOException if there was an error performing the operation.
      */
     public int size () throws IOException;
 
     /**
-     * @return true if the storage supports a hierarchical structure. I false,
+     * Tells if this storage supports multiple sub storage.
+     *
+     * @return True if the storage supports a hierarchical structure. I false,
      * the methods {@link #getSubStorage} and {@link #createSubStorage} should
      * throw an UnsupportedOperationException.
-     * @throws IOException if there was an error performing the operation
+     * @throws IOException if there was an error performing the operation.
      */
     public boolean supportsSubStorage() throws IOException;
 
     public static final String NOT_SUBSTORAGE_CAPABLE =
-            "Not capable of handling sub storages";
+            "Not capable of handling sub storage";
     /**
      * Creates a sub storage, adds it to the current storage and returns it.
-     * Sub storages are fully capable storages and are typically used to order
+     * Sub storage are fully capable storage and are typically used to order
      * settings in a hierarchy.
      * </p><p>
-     * Note: This might not be supported by all storages. Check for availability
+     * Note: This might not be supported by all storage. Check for availability
      * with {@link #supportsSubStorage} in case of doubt.
-     * @param key the name of the sub storage.
-     * @return the storage that was created.
-     * @throws IOException if there was an error performing the operation
+     * @param key The name of the sub storage.
+     * @return The storage that was created.
+     * @throws IOException if there was an error performing the operation.
      */
     public ConfigurationStorage createSubStorage(String key) throws IOException;
 
     /**
-     * Note: This might not be supported by all storages. Check for availability
+     * Note: This might not be supported by all storage. Check for availability
      * with {@link #supportsSubStorage} in case of doubt.
-     * @param key the name of the sub storage.
-     * @return a sub storage.
-     * @throws IOException if there was an error performing the operation
+     * @param key The name of the sub storage.
+     * @return A sub storage.
+     * @throws IOException if there was an error performing the operation.
      */
     public ConfigurationStorage getSubStorage(String key) throws IOException;
 
     /**
-     * Creates a list of sub storages, adds it to the current storage and
-     * returns it. Sub storages are fully capable storages.
+     * Creates a list of sub storage, adds it to the current storage and
+     * returns it. Sub storage are fully capable storage.
      * </p><p>
-     * Note: This might not be supported by all storages. Check for availability
+     * Note: This might not be supported by all storage. Check for availability
      * with {@link #supportsSubStorage} in case of doubt.
-     * @param key   the key for the list of storages.
-     * @param count the number of storages to create.
-     * @return a list of count sub storages.
-     * @throws IOException if the storages could not be created.
+     * @param key The key for the list of storage.
+     * @param count The number of storage to create.
+     * @return A list of count sub storage.
+     * @throws IOException if the storage could not be created.
      */
     public List<ConfigurationStorage> createSubStorages(String key, int count)
                                                              throws IOException;
 
     /**
-     * Note: This might not be supported by all storages. Check for availability
+     * Note: This might not be supported by all storage. Check for availability
      * with {@link #supportsSubStorage} in case of doubt.
-     * @param key the key for the list of storages.
-     * @return a list of storages.
+     * @param key the key for the list of storage.
+     * @return A list of storage.
      * @throws IOException if the list could not be extracted.
      */
     public List<ConfigurationStorage> getSubStorages(String key) throws
