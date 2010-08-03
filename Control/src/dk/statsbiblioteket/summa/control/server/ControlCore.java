003b4401c70c6ddb7a2ca91d956ece960e3e9eb4
Henrik Kirk <hkirk@users.noreply.github.com>
2010-08-03 06:30:44 +0000

    Bugfix: Added some JavaDoc

--- Control/src/dk/statsbiblioteket/summa/control/server/ControlCore.java
+++ Control/src/dk/statsbiblioteket/summa/control/server/ControlCore.java
@@ -14,74 +14,89 @@
  */
 package dk.statsbiblioteket.summa.control.server;
 
-import dk.statsbiblioteket.util.qa.QAInfo;
-import dk.statsbiblioteket.util.rpc.ConnectionContext;
-import dk.statsbiblioteket.summa.common.configuration.Configuration;
-import dk.statsbiblioteket.summa.common.configuration.Configurable;
-import dk.statsbiblioteket.summa.common.rpc.RemoteHelper;
 import dk.statsbiblioteket.summa.common.Logging;
-import dk.statsbiblioteket.summa.control.api.*;
+import dk.statsbiblioteket.summa.common.configuration.Configurable;
+import dk.statsbiblioteket.summa.common.configuration.Configuration;
+import dk.statsbiblioteket.summa.common.rpc.RemoteHelper;
+import dk.statsbiblioteket.summa.control.api.BadConfigurationException;
+import dk.statsbiblioteket.summa.control.api.ClientConnection;
+import dk.statsbiblioteket.summa.control.api.ClientDeployer;
+import dk.statsbiblioteket.summa.control.api.ClientDeploymentException;
+import dk.statsbiblioteket.summa.control.api.DeployerFactory;
+import dk.statsbiblioteket.summa.control.api.InvalidClientStateException;
+import dk.statsbiblioteket.summa.control.api.NoSuchClientException;
+import dk.statsbiblioteket.summa.control.api.Status;
+import dk.statsbiblioteket.summa.control.api.StatusMonitor;
 import dk.statsbiblioteket.summa.control.api.feedback.Feedback;
 import dk.statsbiblioteket.summa.control.api.feedback.FeedbackFactory;
 import dk.statsbiblioteket.summa.control.api.rmi.ControlRMIConnection;
-import dk.statsbiblioteket.summa.control.bundle.BundleUtils;
-import dk.statsbiblioteket.summa.control.bundle.BundleSpecBuilder;
 import dk.statsbiblioteket.summa.control.bundle.Bundle;
-
-import java.io.IOException;
-import java.io.File;
-import java.io.ByteArrayInputStream;
-import java.io.Serializable;
-import java.util.List;
-import java.util.Map;
-import java.rmi.server.UnicastRemoteObject;
-import java.rmi.RemoteException;
-
+import dk.statsbiblioteket.summa.control.bundle.BundleSpecBuilder;
+import dk.statsbiblioteket.summa.control.bundle.BundleUtils;
+import dk.statsbiblioteket.util.qa.QAInfo;
+import dk.statsbiblioteket.util.rpc.ConnectionContext;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.IOException;
+import java.io.Serializable;
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+import java.util.List;
+import java.util.Map;
+
 /**
- * 
+ * The core class for the Control server.
  */
 @QAInfo(level = QAInfo.Level.NORMAL,
         state = QAInfo.State.IN_DEVELOPMENT,
         author = "mke",
-        comment="Unfinished")
+        comment="Unfinished, needs JavaDoc")
 public class ControlCore extends UnicastRemoteObject
                implements ControlRMIConnection, ControlCoreMBean, Configurable {
     private static final long serialVersionUID = 4861381684L;
+    private static final Log log = LogFactory.getLog (ControlCore.class);
 
     /**
      * Configuration property defining which port the
-     * {@link ControlCore} should communicate. Default is 27001.
+     * {@link ControlCore} should communicate. Default is
+     * {@link #DEFAULT_CONTROL_CORE_PORT}.
      */
-    public static final String CONF_CONTROL_CORE_PORT = "summa.control.core.port";
+    public static final String CONF_CONTROL_CORE_PORT =
+                                                      "summa.control.core.port";
+    /** Default value for {@link #CONF_CONTROL_CORE_PORT}. */
+    public static final int DEFAULT_CONTROL_CORE_PORT = 27001;
 
     /**
      * Configuration property defining which port the
-     * {@link ControlCore}s registry should run. Default is 27000.
+     * {@link ControlCore}s registry should run. Default is
+     * {@link #DEFAULT_CONTROL_REGISTRY_PORT}.
      */
-    public static final String CONF_CONTROL_REGISTRY_PORT = "summa.control.core.registryport";
+    public static final String CONF_CONTROL_REGISTRY_PORT =
+                                              "summa.control.core.registryport";
+    /** Default value of {@link #CONF_CONTROL_REGISTRY_PORT}. */
+    public static final int DEFAULT_CONTROL_REGISTRY_PORT = 2700;
 
     /**
      * Configuration property defining the base directory for the ControlCore.
      * Default is <code>${user.home}/summa-control</code>.
      */
-    public static final String CONF_CONTROL_BASE_DIR = "summa.control.core.dir";
+    public static final String CONF_CONTROL_BASE_DIR =
+                                                       "summa.control.core.dir";
 
     /**
      * Configuration property defining the default base directory in which
      * to install clients if the deployment configuration doesn't specify it.
      * <p/>
-     * Note that system properties enclosed in <code>${}</code> <i>will be
-     * escaped on the client side</i> of the connection.
+     * Note that system properties enclosed in <code>${users.home}</code>
+     * <i>will be escaped on the client side</i> of the connection.
      * </p>
      * The default value is <code>${user.home}/summa-control</code>.  
      */
     public static final String CONF_CLIENT_BASE_DIR =
-                                          ClientConnection.CONF_CLIENT_BASEPATH;
-
-    private static final Log log = LogFactory.getLog (ControlCore.class);
+                                       ClientConnection.CONF_CLIENT_BASEPATH;
 
     private File baseDir;
     private String defaultClientBasePath;
@@ -90,8 +105,13 @@ public class ControlCore extends UnicastRemoteObject
     private RepositoryManager repoManager;
     private ConfigurationManager confManager;
 
+    /**
+     * Constructs a ControlCore object.
+     * @param conf The configuration which should be used.
+     * @throws IOException If error occur while exporting object.
+     */
     public ControlCore(Configuration conf) throws IOException {
-        super (getServicePort(conf));
+        super(getServicePort(conf));
 
         setStatus(Status.CODE.not_instantiated,
                   "Setting up", Logging.LogLevel.DEBUG);
@@ -102,7 +122,7 @@ public class ControlCore extends UnicastRemoteObject
 
         baseDir = ControlUtils.getControlBaseDir(conf);
         defaultClientBasePath = ControlUtils.getClientBasePath(conf);
-        log.debug ("Using base dir '" + baseDir + "'");
+        log.debug("Using base dir '" + baseDir + "'");
 
         BundleUtils.prepareCodeBase(conf, repoManager.getRepository(),
                                     "summa-common", "summa-control-api");
@@ -110,7 +130,8 @@ public class ControlCore extends UnicastRemoteObject
                   "Exporting remote interfaces", Logging.LogLevel.DEBUG);
         RemoteHelper.exportRemoteInterface(
                                  this,
-                                 conf.getInt(CONF_CONTROL_REGISTRY_PORT, 27000),
+                                 conf.getInt(CONF_CONTROL_REGISTRY_PORT,
+                                           DEFAULT_CONTROL_REGISTRY_PORT),
                                  "summa-control");
 
         try {
@@ -119,20 +140,23 @@ public class ControlCore extends UnicastRemoteObject
             String msg = "Failed to register MBean, going on without it. "
                          + "Error was: " + e.getMessage();
             if (log.isTraceEnabled()) {
-                log.warn (msg, e);
+                log.warn(msg, e);
             } else {
                 log.warn(msg);
             }
         }
 
-        runAutoStartClients ();
+        runAutoStartClients();
 
         setStatus(Status.CODE.idle,
                   "Set up complete. Remote interfaces up",
                   Logging.LogLevel.DEBUG);
     }
 
-    private void runAutoStartClients () {
+    /**
+     * Private helper, to start auto clients. 
+     */
+    private void runAutoStartClients() {
         setStatusRunning("Starting auto-start clients");
 
         for (String instanceId : clientManager) {
@@ -159,14 +183,20 @@ public class ControlCore extends UnicastRemoteObject
                           "auto-start");
             }
         }
-
         setStatusIdle();
     }
 
+    /**
+     * Private helper, that returns the service port.
+     * @param conf The configuration.
+     * @return The service port.
+     */
     private static int getServicePort(Configuration conf) {
-        return conf.getInt(CONF_CONTROL_CORE_PORT, 27001);
+        return conf.getInt(CONF_CONTROL_CORE_PORT,
+                           DEFAULT_CONTROL_CORE_PORT);
     }
 
+    @Override
     public ClientConnection getClient(String instanceId) {
         setStatus(Status.CODE.running,
                   "Looking up client '" + instanceId + "'",
@@ -178,11 +208,11 @@ public class ControlCore extends UnicastRemoteObject
         }
 
         ConnectionContext<ClientConnection> conn =
-                                                 clientManager.get (instanceId);
+                                                  clientManager.get(instanceId);
 
         if (conn == null) {
             setStatusIdle();
-            throw new InvalidClientStateException (instanceId, "Not running");
+            throw new InvalidClientStateException(instanceId, "Not running");
         }
 
         ClientConnection client = conn.getConnection();
@@ -192,8 +222,8 @@ public class ControlCore extends UnicastRemoteObject
             String lookupId = client.getId();
 
             if (!instanceId.equals(lookupId)) {
-                log.warn ("Client reports illegal id '" + lookupId
-                          + "'. Expected '" + instanceId + "'");
+                log.warn("Client reports illegal id '" + lookupId
+                         + "'. Expected '" + instanceId + "'");
                 throw new InvalidClientStateException(instanceId,
                                                       "Reports illegal id '"
                                                       + lookupId + "'");
@@ -203,7 +233,7 @@ public class ControlCore extends UnicastRemoteObject
             throw new InvalidClientStateException(instanceId,
                                                   "Broken connection", e);
         } finally {
-            clientManager.release (conn);
+            clientManager.release(conn);
         }
 
         setStatusIdle();
@@ -211,8 +241,9 @@ public class ControlCore extends UnicastRemoteObject
         return client;
     }
 
+    @Override
     public void deployClient(Configuration conf) {
-        setStatusRunning ("Deploying client");
+        setStatusRunning("Deploying client");
 
         validateClientConf(conf);
 
@@ -223,11 +254,11 @@ public class ControlCore extends UnicastRemoteObject
                      defaultClientBasePath);
         } else {
             log.debug("Using client base path override: "
-                      + conf.getString(ClientConnection.CONF_CLIENT_BASEPATH));
+                    + conf.getString(ClientConnection.CONF_CLIENT_BASEPATH));
         }
 
-        log.info ("Preparing to deploy client with deployer config: \n"
-                   + conf.dumpString());
+        log.info("Preparing to deploy client with deployer config: \n"
+                 + conf.dumpString());
 
         ClientDeployer deployer = DeployerFactory.createClientDeployer(conf);
         Feedback feedback = FeedbackFactory.createFeedback(conf);
@@ -235,14 +266,14 @@ public class ControlCore extends UnicastRemoteObject
         String instanceId = conf.getString(ClientDeployer.CONF_INSTANCE_ID);
 
         /* Make sure that a client with the given id isn't already deployed */
-        if (clientManager.knowsClient (instanceId)) {
+        if (clientManager.knowsClient(instanceId)) {
             throw new ClientDeploymentException ("A client with instance id '"
                                                  +instanceId
                                                  + "' already exists");
         }
 
-        setStatusRunning ("Deploying client '" + instanceId + "'. "
-                          + "Waiting for deployer");
+        setStatusRunning("Deploying client '" + instanceId + "'. "
+                         + "Waiting for deployer");
         try {
             deployer.deploy(feedback);
         } catch (Exception e) {
@@ -256,13 +287,14 @@ public class ControlCore extends UnicastRemoteObject
          * deployment */
 
         // First extract the bundle spec
-        String bundleId = conf.getString(ClientDeployer.CONF_DEPLOYER_BUNDLE);
+        String bundleId =
+                        conf.getString(ClientDeployer.CONF_DEPLOYER_BUNDLE);
         byte[] bundleSpecContent = repoManager.getBundleSpec(bundleId);
         BundleSpecBuilder spec = BundleSpecBuilder.open(
                                    new ByteArrayInputStream(bundleSpecContent));
 
 	// Push all properties from the client spec into the control file
-	for (Map.Entry<String,Serializable> prop : spec.getProperties()){
+	for(Map.Entry<String,Serializable> prop : spec.getProperties()){
 	    conf.set(prop.getKey(), prop.getValue());
 	}
 
@@ -272,10 +304,11 @@ public class ControlCore extends UnicastRemoteObject
                                conf,
                                spec);
 
-        log.info ("Client '" + instanceId + "' deployed");
+        log.info("Client '" + instanceId + "' deployed");
         setStatusIdle();
     }
 
+    @Override
     public void startClient(Configuration conf) {
         setStatusRunning("Starting client");
 
@@ -283,7 +316,7 @@ public class ControlCore extends UnicastRemoteObject
         Configuration deployConf =
                                clientManager.getDeployConfiguration(instanceId);
         String bundleId = deployConf.getString(
-                                           ClientDeployer.CONF_DEPLOYER_BUNDLE);
+                                       ClientDeployer.CONF_DEPLOYER_BUNDLE);
         log.trace("startClient: got bundleId '" + bundleId + "'");
 
         if (bundleId == null) {
@@ -356,6 +389,7 @@ public class ControlCore extends UnicastRemoteObject
         log.info ("Client '" + instanceId + "' deployed");
     }
 
+    @Override
     public void stopClient(String instanceId) {
         setStatusRunning("Stopping '" + instanceId + "'");
 
@@ -401,7 +435,8 @@ public class ControlCore extends UnicastRemoteObject
      * required properties listed in {@link ClientDeployer}.</p>
      *
      * <p>If the {@link ClientDeployer#CONF_CLIENT_CONF} is not set,
-     * it will be set to point at the configuration of this Control instance.</p>
+     * it will be set to point at the configuration of this Control instance.
+     * </p>
      *
      * <p>If the {@link ClientDeployer#CONF_DEPLOYER_BUNDLE_FILE} is not
      * set it will be calculated from the
@@ -457,18 +492,19 @@ public class ControlCore extends UnicastRemoteObject
         try { conf.getString(ClientDeployer.CONF_DEPLOYER_CLASS); }
         catch (NullPointerException e) {
             throw new BadConfigurationException("Required property: "
-                                                + ClientDeployer.CONF_DEPLOYER_CLASS
-                                                + " not set ");
+                                        + ClientDeployer.CONF_DEPLOYER_CLASS
+                                        + " not set ");
         }
 
         try { conf.getString(ClientDeployer.CONF_DEPLOYER_TARGET); }
         catch (NullPointerException e) {
             throw new BadConfigurationException("Required property: "
-                                                + ClientDeployer.CONF_DEPLOYER_TARGET
-                                                + " not set ");
+                                      + ClientDeployer.CONF_DEPLOYER_TARGET
+                                      + " not set ");
         }        
     }
 
+    @Override
     public List<String> getClients() {
         setStatus(Status.CODE.running, "Getting client list",
                   Logging.LogLevel.TRACE);
@@ -477,6 +513,7 @@ public class ControlCore extends UnicastRemoteObject
         return list;
     }
 
+    @Override
     public List<String> getBundles() {
         setStatus(Status.CODE.running, "Getting bundle list",
                   Logging.LogLevel.TRACE);
@@ -486,17 +523,19 @@ public class ControlCore extends UnicastRemoteObject
 
     }
 
+    @Override
     public Configuration getDeployConfiguration(String instanceId)
             throws RemoteException {
         return clientManager.getDeployConfiguration(instanceId);
     }
 
+    @Override
     public Status getStatus() throws RemoteException {
         return status;
     }
 
-    private void setStatus (Status.CODE code, String msg,
-                            Logging.LogLevel level) {
+    private void setStatus(Status.CODE code, String msg,
+                                                       Logging.LogLevel level) {
         status = new Status(code, msg);
         Logging.log("Status: "+ status, log, level);
     }
@@ -509,6 +548,11 @@ public class ControlCore extends UnicastRemoteObject
         setStatus(Status.CODE.running, msg, Logging.LogLevel.INFO);
     }
 
+    /**
+     * Main class, starts an instance of the {@link ControlCore} class.
+     *
+     * @param args Arguments given on the commandline, not used.
+     */
     public static void main(String[] args) {
         Configuration conf = Configuration.getSystemConfiguration();
         try {
@@ -519,7 +563,3 @@ public class ControlCore extends UnicastRemoteObject
         }
     }
 }
-
-
-
-
