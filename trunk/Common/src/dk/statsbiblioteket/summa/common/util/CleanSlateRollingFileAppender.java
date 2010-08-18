6f5ea62f7da9b361a7046feab07755858eddcd9d
hkirk <hkirk@6c0f5c6f-88b2-4050-bb98-6ed3ea504abe>
2010-08-18 08:57:11 +0000

    Bugfix: added JavaDoc and QAInfo

--- trunk/Common/src/dk/statsbiblioteket/summa/common/util/CleanSlateRollingFileAppender.java
+++ trunk/Common/src/dk/statsbiblioteket/summa/common/util/CleanSlateRollingFileAppender.java
@@ -14,6 +14,7 @@
  */
 package dk.statsbiblioteket.summa.common.util;
 
+import dk.statsbiblioteket.util.qa.QAInfo;
 import org.apache.log4j.Layout;
 import org.apache.log4j.RollingFileAppender;
 
@@ -23,27 +24,50 @@ import java.io.IOException;
  * Custom rolling file appender implementation for Log4J that rolls
  * the log files each time the JVM starts
  */
+@QAInfo(author = "mkamstrup",
+        state = QAInfo.State.QA_OK,
+        level = QAInfo.Level.NORMAL)
 public class CleanSlateRollingFileAppender extends RollingFileAppender {
-
+    /**
+     * Creates a clean slate rolling file appender, with not layout and no
+     * filename.
+     */
     public CleanSlateRollingFileAppender () {
         super();
     }
 
-
+    /**
+     * Creates a clean slate rolling file appender, with a specific layout and
+     * to a specific filename.
+     * @param layout The layout.
+     * @param filename They filename.
+     * @throws IOException If error opening file specified.
+     */
     public CleanSlateRollingFileAppender(Layout layout, String filename)
                                                             throws IOException {
         super(layout, filename);
     }
 
+    /**
+     * Creates a clean slate rolling file appender, with a specific layout and
+     * to a specific filename. This appender appends to existing files.
+     * @param layout The Layout.
+     * @param filename The filename.
+     * @param append True iff the appender should append to existing files.
+     * @throws IOException If error opening file specified.
+     */
     public CleanSlateRollingFileAppender(
             Layout layout, String filename, boolean append) throws IOException {
         super(layout, filename, append);
     }
 
+    /**
+     * This calls {@link super#activateOptions()} and den do a
+     * {@link super#rollOver}.
+     */
     @Override
     public void activateOptions() {
         super.activateOptions();
         rollOver();
     }
 }
-
