bb8ccbf598467268bf61b1ac746b2fc55555a9b1
hkirk <hkirk@6c0f5c6f-88b2-4050-bb98-6ed3ea504abe>
2010-08-25 07:00:34 +0000

    Bugfix: added JavaDoc

--- trunk/Control/src/dk/statsbiblioteket/summa/control/client/shell/RepositoryCommand.java
+++ trunk/Control/src/dk/statsbiblioteket/summa/control/client/shell/RepositoryCommand.java
@@ -14,12 +14,13 @@
  */
 package dk.statsbiblioteket.summa.control.client.shell;
 
-import dk.statsbiblioteket.summa.control.api.ClientConnection;
+import dk.statsbiblioteket.summa.common.shell.Command;
 import dk.statsbiblioteket.summa.common.shell.RemoteCommand;
 import dk.statsbiblioteket.summa.common.shell.ShellContext;
-import dk.statsbiblioteket.summa.common.shell.Command;
-import dk.statsbiblioteket.util.rpc.ConnectionManager;
+import dk.statsbiblioteket.summa.control.api.ClientConnection;
 import dk.statsbiblioteket.util.Strings;
+import dk.statsbiblioteket.util.qa.QAInfo;
+import dk.statsbiblioteket.util.rpc.ConnectionManager;
 
 import java.util.Arrays;
 import java.util.List;
@@ -30,25 +31,41 @@ import java.util.TreeSet;
  * A {@link Command} for the {@link ClientShell} used to list the contents
  * of the configured repository.
  */
+@QAInfo(author = "mke",
+        level = QAInfo.Level.NORMAL,
+        state = QAInfo.State.QA_NEEDED)
 public class RepositoryCommand extends RemoteCommand<ClientConnection> {
-
-    private ConnectionManager<ClientConnection> cm;
     private String clientAddress;
 
+    /**
+     * Creates a repository command object.
+     *
+     * @param cm the connection manager, controlling the connection the client.
+     * @param clientAddress Address to the client instance.
+     */
     public RepositoryCommand(ConnectionManager<ClientConnection> cm,
-                        String clientAddress) {
+                                                         String clientAddress) {
         super("repo", "Inspect the contents of the service repository", cm);
         this.clientAddress = clientAddress;
 
         setUsage("repo [filter_regex]");
     }
 
+    /**
+     * Invoke the repository command.
+     * Side-effect: Prints a sorted list of all bundles to the client instance
+     * invoking this command. 
+     *
+     * @param ctx Context used to print messages and retrieve user feedback
+     * @throws Exception If error occur while getting connection to the client.
+     */
+    @Override
     public void invoke(ShellContext ctx) throws Exception {
         ClientConnection client = getConnection(clientAddress);
 
-        String filter = Strings.join (Arrays.asList(getArguments()), " ").trim();
+        String filter = Strings.join(Arrays.asList(getArguments()), " ").trim();
 
-        if (filter.equals("")) {
+        if(filter.equals("")) {
             filter = ".*";
         }
 
@@ -57,22 +74,18 @@ public class RepositoryCommand extends RemoteCommand<ClientConnection> {
             List<String> bundles = client.getRepository().list(filter);
 
             if (filter.equals(".*")) {
-                ctx.info ("Bundles in repository:");
+                ctx.info("Bundles in repository:");
             } else {
-                ctx.info ("Bundles in repository, matching '" + filter + "':");
+                ctx.info("Bundles in repository, matching '" + filter + "':");
             }
 
             /* Sort the bundles before displaying them  */
             SortedSet<String> sortedBundles = new TreeSet<String>(bundles);
-            for (String bdlId : sortedBundles) {
+            for(String bdlId : sortedBundles) {
                 ctx.info("\t" + bdlId);
             }
         } finally {
             releaseConnection();
         }
     }
-
 }
-
-
-
