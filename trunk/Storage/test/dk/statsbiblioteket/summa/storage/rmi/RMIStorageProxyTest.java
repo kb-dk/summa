f81c5248e05d3bb81c58edba314c7156a751d45c
hkirk <hkirk@6c0f5c6f-88b2-4050-bb98-6ed3ea504abe>
2010-09-23 07:08:30 +0000

    Unittest update.

--- trunk/Storage/test/dk/statsbiblioteket/summa/storage/rmi/RMIStorageProxyTest.java
+++ trunk/Storage/test/dk/statsbiblioteket/summa/storage/rmi/RMIStorageProxyTest.java
@@ -14,78 +14,120 @@
  */
 package dk.statsbiblioteket.summa.storage.rmi;
 
-import junit.framework.Test;
-import junit.framework.TestSuite;
-import junit.framework.TestCase;
+import dk.statsbiblioteket.summa.common.Record;
+import dk.statsbiblioteket.summa.common.configuration.Configuration;
+import dk.statsbiblioteket.summa.storage.api.Storage;
+import dk.statsbiblioteket.summa.storage.api.StorageFactory;
+import dk.statsbiblioteket.summa.storage.database.DatabaseStorage;
 
 import java.io.File;
 import java.io.IOException;
 
-import dk.statsbiblioteket.summa.storage.api.Storage;
-import dk.statsbiblioteket.summa.storage.api.StorageFactory;
-import dk.statsbiblioteket.summa.storage.api.WritableStorage;
-import dk.statsbiblioteket.summa.storage.database.DatabaseStorage;
-import dk.statsbiblioteket.summa.storage.database.h2.H2Storage;
-import dk.statsbiblioteket.summa.common.configuration.Configuration;
-import dk.statsbiblioteket.summa.common.Record;
-import dk.statsbiblioteket.util.Files;
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
 
+/**
+ * Tests {@link RMIStorageProxy}.
+ */
 @SuppressWarnings({"DuplicateStringLiteralInspection"})
 public class RMIStorageProxyTest extends TestCase {
+    /**
+     * Constructs a RMI Storage proxy.
+     * @param name The name.
+     */
     public RMIStorageProxyTest(String name) {
         super(name);
     }
 
     @Override
-    public void setUp() throws Exception {
+    public final void setUp() throws Exception {
         super.setUp();
     }
 
     @Override
-    public void tearDown() throws Exception {
+    public final void tearDown() throws Exception {
         super.tearDown();
     }
 
+    /**
+     * @return A test suite.
+     */
     public static Test suite() {
         return new TestSuite(RMIStorageProxyTest.class);
     }
 
+    /** Test root. */
     private File testRoot = new File(System.getProperty(
-            "java.io.tmpdir", "storagetest"));
-    
+                                     "java.io.tmpdir", "storagetest"));
+
+    /**
+     * Returns a RMI storage.
+     * @return A RMI Storage.
+     * @throws Exception If error.
+     */
     private Storage getRMIStorage() throws Exception {
         Configuration conf = Configuration.newMemoryBased(
                 DatabaseStorage.CONF_LOCATION, testRoot.toString());
         return new RMIStorageProxy(conf);
     }
 
-    public void testGetRecord() throws Exception {
-        Storage storage = getRMIStorage();
-        storage.flush(new Record("Dummy1", "SomeBase", new byte[0]));
-        assertFalse("The added record should exist",
-                    storage.getRecord("Dummy1", null).isDeleted());
+    /**
+     * Test get record.
+     */
+    public final void testGetRecord() {
+        try {
+            Storage storage = getRMIStorage();
+            storage.flush(new Record("Dummy1", "SomeBase", new byte[0]));
+            assertFalse("The added record should exist",
+                        storage.getRecord("Dummy1", null).isDeleted());
+        } catch (Exception e) {
+            fail("Not expected here");
+        }
     }
 
-    public void testDirectClear() throws Exception {
-        Configuration conf = Configuration.newMemoryBased(
-                DatabaseStorage.CONF_LOCATION,
-                testRoot.toString());
-        Storage storage = StorageFactory.createStorage(conf);
-        testClearBase(storage);
+    /**
+     * Test direct clear.
+     */
+    public final void testDirectClear() {
+        try {
+            Configuration conf = Configuration.newMemoryBased(
+                    DatabaseStorage.CONF_LOCATION,
+                    testRoot.toString());
+            Storage storage = StorageFactory.createStorage(conf);
+            testClearBase(storage);
+        } catch (IOException e) {
+            fail("Not expected here");
+        }
     }
 
-    public void testClearBase() throws Exception {
-        Storage storage = getRMIStorage();
-        testClearBase(storage);
+    /**
+     * Test clear base.
+     */
+    public final void testClearBase() {
+        try {
+            Storage storage = getRMIStorage();
+            testClearBase(storage);
+        } catch (Exception e) {
+            fail("Not expected here");
+        }
     }
 
-    public static void testClearBase(Storage storage) throws Exception {
-        storage.flush(new Record("Dummy1", "SomeBase", new byte[0]));
-        assertFalse("The added record should exist",
-                   storage.getRecord("Dummy1", null).isDeleted());
-        storage.clearBase("SomeBase");
-        assertTrue("The added record should not exist anymore",
-                   storage.getRecord("Dummy1", null).isDeleted());
+    /**
+     * Test clear base.
+     * @param storage The storage.
+     * @throws Exception
+     */
+    public static void testClearBase(Storage storage) {
+        try {
+            storage.flush(new Record("Dummy1", "SomeBase", new byte[0]));
+            assertFalse("The added record should exist",
+                       storage.getRecord("Dummy1", null).isDeleted());
+            storage.clearBase("SomeBase");
+            assertTrue("The added record should not exist anymore",
+                       storage.getRecord("Dummy1", null).isDeleted());
+        } catch (IOException e) {
+            fail("Not expected here");
+        }
     }
 }
-
