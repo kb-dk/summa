542b6618d0930e62c4634a75c7b6f3be47c4dc3e
hkirk <hkirk@6c0f5c6f-88b2-4050-bb98-6ed3ea504abe>
2010-09-23 06:58:42 +0000

    Updated JavaDoc and unittest for aggregating storage.

--- trunk/Storage/test/dk/statsbiblioteket/summa/storage/AggregatingStorageTest.java
+++ trunk/Storage/test/dk/statsbiblioteket/summa/storage/AggregatingStorageTest.java
@@ -24,9 +24,6 @@ import dk.statsbiblioteket.summa.storage.api.StorageIterator;
 import dk.statsbiblioteket.summa.storage.database.DatabaseStorage;
 import dk.statsbiblioteket.summa.storage.rmi.RMIStorageProxy;
 import dk.statsbiblioteket.util.Files;
-import junit.framework.TestCase;
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 
 import java.io.File;
 import java.io.Serializable;
@@ -34,11 +31,20 @@ import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
 
+import junit.framework.TestCase;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
 /**
- *
+ * Test {@link AggregatingStorage}.
  */
 public class AggregatingStorageTest extends TestCase {
-    private static Log log = LogFactory.getLog(AggregatingStorageTest.class);
+    /** Local log class. */
+    private Log log = LogFactory.getLog(AggregatingStorageTest.class);
+    /** Storage communication port. */
+    private static final int PORT = 29000;
+    /** Test database counter. */
     static int test_db_counter = 0;
     String test_db_1 = "test_db_1_";
     String test_db_2 = "test_db_2_";
@@ -56,68 +62,70 @@ public class AggregatingStorageTest extends TestCase {
     List<Configuration> storageConfs;
     Storage storage;
 
-    public void setUp () throws Exception {
+    @Override
+    public void setUp() throws Exception {
         conf = new Configuration(new XStorage());
-        conf.set (Storage.CONF_CLASS, AggregatingStorage.class);
+        conf.set(Storage.CONF_CLASS, AggregatingStorage.class);
 
         storageConfs =
           conf.createSubConfigurations(AggregatingStorage.CONF_SUB_STORAGES, 2);
 
         /* Clean up old databases */
-        String db1 = test_db_1+test_db_counter;
+        String db1 = test_db_1 + test_db_counter;
         if (new File(db1).exists()) {
-            Files.delete (db1);
+            Files.delete(db1);
         }
 
-        String db2 = test_db_2+test_db_counter;
+        String db2 = test_db_2 + test_db_counter;
         if (new File(db2).exists()) {
-            Files.delete (db2);
+            Files.delete(db2);
         }
 
         /* New db names */
         test_db_counter++;
-        db1 = test_db_1+test_db_counter;
-        db2 = test_db_2+test_db_counter;
+        db1 = test_db_1 + test_db_counter;
+        db2 = test_db_2 + test_db_counter;
 
         /* SUB 1 */
         subConf1 = storageConfs.get(0);
         subConf1.set(
                 AggregatingStorage.CONF_SUB_STORAGE_BASES,
-                (Serializable)Arrays.asList(base1, base2));
+                (Serializable) Arrays.asList(base1, base2));
         subConf1.set(ConnectionConsumer.CONF_RPC_TARGET,
                      "//localhost:29000/summa-storage-sub1");
         storageConf1 =
             subConf1.createSubConfiguration(
                                     AggregatingStorage.CONF_SUB_STORAGE_CONFIG);
-        storageConf1.set (Storage.CONF_CLASS, RMIStorageProxy.class);
-        storageConf1.set (DatabaseStorage.CONF_LOCATION, db1);
-        storageConf1.set (RMIStorageProxy.CONF_REGISTRY_PORT, 29000);
-        storageConf1.set (RMIStorageProxy.CONF_SERVICE_NAME,
+        storageConf1.set(Storage.CONF_CLASS, RMIStorageProxy.class);
+        storageConf1.set(DatabaseStorage.CONF_LOCATION, db1);
+        storageConf1.set(RMIStorageProxy.CONF_REGISTRY_PORT, PORT);
+        storageConf1.set(RMIStorageProxy.CONF_SERVICE_NAME,
                           "summa-storage-sub1");
-        storageConf1.set (DatabaseStorage.CONF_FORCENEW, true);
+        storageConf1.set(DatabaseStorage.CONF_FORCENEW, true);
 
         /* SUB 2 */
         subConf2 = storageConfs.get(1);
         subConf2.set(
                 AggregatingStorage.CONF_SUB_STORAGE_BASES,
-                (Serializable)Arrays.asList(base3));
+                (Serializable) Arrays.asList(base3));
         subConf2.set(ConnectionConsumer.CONF_RPC_TARGET,
                      "//localhost:29000/summa-storage-sub2");
         storageConf2 =
             subConf2.createSubConfiguration(
                                     AggregatingStorage.CONF_SUB_STORAGE_CONFIG);
-        storageConf2.set (Storage.CONF_CLASS, RMIStorageProxy.class);
-        storageConf2.set (DatabaseStorage.CONF_LOCATION, db2);
-        storageConf2.set (RMIStorageProxy.CONF_REGISTRY_PORT, 29000);
-        storageConf2.set (RMIStorageProxy.CONF_SERVICE_NAME,
+        storageConf2.set(Storage.CONF_CLASS, RMIStorageProxy.class);
+        storageConf2.set(DatabaseStorage.CONF_LOCATION, db2);
+        storageConf2.set(RMIStorageProxy.CONF_REGISTRY_PORT, PORT);
+        storageConf2.set(RMIStorageProxy.CONF_SERVICE_NAME,
                           "summa-storage-sub2");
-        storageConf2.set (DatabaseStorage.CONF_FORCENEW, true);
+        storageConf2.set(DatabaseStorage.CONF_FORCENEW, true);
 
         /* Create the aggregating storage and child storages */
         storage = StorageFactory.createStorage(conf);
     }
 
-    public void tearDown () throws Exception {
+    @Override
+    public void tearDown() throws Exception {
         storage.close();
     }
 
@@ -127,7 +135,7 @@ public class AggregatingStorageTest extends TestCase {
         assertNull(r);
     }
 
-    public void testAddOne () throws Exception {
+    public void testAddOne() throws Exception {
         Record r = new Record(testId1, base1, testContent1);
         storage.flush(r);
 
@@ -136,7 +144,7 @@ public class AggregatingStorageTest extends TestCase {
     }
 
     public void testIteration() throws Exception {
-        storage.clearBase (base1);
+        storage.clearBase(base1);
         assertBaseCount(base1, 0);
 
         storage.flush(new Record(testId1, base1, testContent1));
@@ -145,10 +153,10 @@ public class AggregatingStorageTest extends TestCase {
     }
 
     public void testIterationAllBases() throws Exception {
-        storage.clearBase (base1);
-        storage.clearBase (base2);
-        storage.clearBase (base3);
-        
+        storage.clearBase(base1);
+        storage.clearBase(base2);
+        storage.clearBase(base3);
+
         assertBaseCount(base1, 0);
         assertBaseCount(base2, 0);
         assertBaseCount(base3, 0);
@@ -157,7 +165,7 @@ public class AggregatingStorageTest extends TestCase {
         storage.flush(new Record(testId2, base2, testContent1));
         storage.flush(new Record(testId3, base3, testContent1));
         storage.flush(new Record(testId4, base3, testContent1));
-        
+
         assertBaseCount(base1, 1);
         assertBaseCount(base2, 1);
         assertBaseCount(base3, 2);
@@ -182,7 +190,7 @@ public class AggregatingStorageTest extends TestCase {
         assertEquals(globalMtime, mtime3);
     }
 
-    public void assertBaseCount (String base, long expected) throws Exception {
+    public void assertBaseCount(String base, long expected) throws Exception {
         log.info("basecount: " + base + ", expected " + expected);
         long iterKey = storage.getRecordsModifiedAfter(0, base, null);
         log.info("Iteration key: " + iterKey);
