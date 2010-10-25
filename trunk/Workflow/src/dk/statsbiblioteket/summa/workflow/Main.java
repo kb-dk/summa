b419f1937047315bbed91da894e8e51a8d5debf7
hkirk <hkirk@6c0f5c6f-88b2-4050-bb98-6ed3ea504abe>
2010-10-25 08:25:13 +0000

    added java doc

--- trunk/Workflow/src/dk/statsbiblioteket/summa/workflow/Main.java
+++ trunk/Workflow/src/dk/statsbiblioteket/summa/workflow/Main.java
@@ -15,33 +15,38 @@
 package dk.statsbiblioteket.summa.workflow;
 
 import dk.statsbiblioteket.summa.common.configuration.Configuration;
-import dk.statsbiblioteket.summa.common.configuration.storage.XStorage;
-
-import java.io.IOException;
 
 /**
  * Main class for running workflows. It will instantiate a
  * {@link WorkflowManager} based on a configuration obtained from:
  * <ul>
- *   <li>The system property {@link Configuration#CONF_CONFIGURATION_PROPERTY}, or</li>
+ *   <li>The system property {@link Configuration#CONF_CONFIGURATION_PROPERTY},
+ *       or</li>
  *   <li>The first command line argument (only one argument allowed). A workflow
  *       defined this way may be specified as described in
  *       {@link Configuration#load}</li>
  * </ul>
  * If a workflow is specified both ways the program will exit with an error.
  */
-public class Main {
-
+public final class Main {
+    /** Configuration property. */
     public static final String CONF_CONFIG =
                                       Configuration.CONF_CONFIGURATION_PROPERTY;
+    /**
+     * Private constructor to make sure we don't have any objects of this type.
+     */
+    private Main() { }
 
-    public static void main (String[] args) {
+    /**
+     * Main method for starting a {@link WorkflowManager}.
+     * @param args Command line arguments.
+     */
+    public static void main(String[] args) {
         if (args.length > 1) {
             System.err.println("Too many arguments. Only one or zero arguments "
                                + "allowed. Bailing out");
             System.exit(1);
         }
-
         Configuration conf = null;
 
         if (args.length == 1) {
@@ -57,7 +62,7 @@ public class Main {
                 conf = Configuration.load(args[0]);
             } catch (Exception e) {
                 System.err.println("Error reading workflow '" + args[0] + "': "
-                                   +e.getMessage() +". Bailing out");
+                                   + e.getMessage() + ". Bailing out");
                 System.exit(2);
             }
         } else {
@@ -70,10 +75,7 @@ public class Main {
 
             conf  = Configuration.getSystemConfiguration();
         }
-
         WorkflowStep core = new WorkflowManager(conf);
         core.run();
     }
-
 }
-
