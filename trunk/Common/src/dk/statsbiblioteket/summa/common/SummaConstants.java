090112676dea1cb3d049f812c248cad9c2224823
hkirk <hkirk@6c0f5c6f-88b2-4050-bb98-6ed3ea504abe>
2010-11-10 11:33:17 +0000

    Updated for Summa 1.6.6_1 release

--- trunk/Common/src/dk/statsbiblioteket/summa/common/SummaConstants.java
+++ trunk/Common/src/dk/statsbiblioteket/summa/common/SummaConstants.java
@@ -30,13 +30,13 @@ public class SummaConstants {
      * Current Summa version,
      * Note: Auto generated.
      */
-    public static final String SUMMAVERSION = "1.6.6";
+    public static final String SUMMAVERSION = "1.6.6_1";
 
     /**
      * Last revision for Summa release,
      * Note: Auto generated.
      */
-    public static final int SUMMAVERSIONREVISION = 2681;
+    public static final int SUMMAVERSIONREVISION = 2688;
 
     /**
      * Get version string, used to present the Summa version and the revision
