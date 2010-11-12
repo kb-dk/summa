7cd08664257d255774d14aaf813a9f9ce78442bb
Henrik Kirk <hkirk@users.noreply.github.com>
2010-11-12 10:47:30 +0000

    Bugfix: Added JavaDoc

--- Common/src/dk/statsbiblioteket/summa/common/filter/object/PayloadException.java
+++ Common/src/dk/statsbiblioteket/summa/common/filter/object/PayloadException.java
@@ -24,45 +24,88 @@ import dk.statsbiblioteket.util.qa.QAInfo;
         state = QAInfo.State.IN_DEVELOPMENT,
         author = "te")
 public class PayloadException extends Exception {
+    /** The serial version UID. */
     private static final long serialVersionUID = 2348343L;
+    /** The payload which was the reason for this exception. */
     private Payload payload = null;
 
+    /**
+     * Constructs an empty payload exception.
+     */
     public PayloadException() {
         super();
     }
 
+    /**
+     * Constructs a payload exception with a message.
+     * @param message The message.
+     */
     public PayloadException(String message) {
         super(message);
     }
 
+    /**
+     * Constructs a payload exception with a message and the cause.
+     * @param message The message for this exception.
+     * @param cause The cause for this exception.
+     */
     public PayloadException(String message, Throwable cause) {
         super(message, cause);
     }
 
+    /**
+     * Constructs a payload exception with a cause.
+     * @param cause The cause of this exceptopn.
+     */
     public PayloadException(Throwable cause) {
         super(cause);
     }
 
+    /**
+     * Constructs a payload exception with a payload.
+     * @param payload The paylod responsible.
+     */
     public PayloadException(Payload payload) {
         super();
         this.payload = payload;
     }
 
+    /**
+     * Constructs a payload
+     * @param message The message for this exception.
+     * @param payload The paylod responsible.
+     */
     public PayloadException(String message, Payload payload) {
         super(message);
         this.payload = payload;
     }
 
+    /**
+     * Constructs a payload exception with message, the cause and the
+     * responsible payload.
+     * @param message The message for this exception.
+     * @param cause The cause of this exception.
+     * @param payload The paylod responsible.
+     */
     public PayloadException(String message, Throwable cause, Payload payload) {
         super(message, cause);
         this.payload = payload;
     }
 
+    /**
+     * Constructs a payload exception with a cause and a payload.
+     * @param cause The cause of this exception.
+     * @param payload The paylod responsible.
+     */
     public PayloadException(Throwable cause, Payload payload) {
         super(cause);
         this.payload = payload;
     }
 
+    /**
+     * Return the payload responsible for this exception.
+     * @return The payload responsible for this exception.
+     */
     public Payload getPayload() {
         return payload;
     }
