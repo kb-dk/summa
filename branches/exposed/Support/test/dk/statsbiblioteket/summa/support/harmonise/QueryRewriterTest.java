2d36f959f93ee305751c4224ef9790da16ccd961
toke-sb <toke-sb@6c0f5c6f-88b2-4050-bb98-6ed3ea504abe>
2011-10-13 11:48:18 +0000

    Testing: Added test for query parser that demonstrates problems with -

---
+++ branches/exposed/Support/test/dk/statsbiblioteket/summa/support/harmonise/QueryRewriterTest.java
@@ -0,0 +1,86 @@
+package dk.statsbiblioteket.summa.support.harmonise;
+
+import dk.statsbiblioteket.summa.common.lucene.index.IndexChangeEvent;
+import junit.framework.Test;
+import junit.framework.TestSuite;
+import junit.framework.TestCase;
+import org.apache.lucene.queryparser.classic.ParseException;
+import org.apache.lucene.queryparser.classic.QueryParser;
+import org.apache.lucene.search.Query;
+import org.apache.lucene.search.TermQuery;
+
+/**
+ * QueryRewriter Tester.
+ *
+ * @author <Authors name>
+ * @since <pre>10/13/2011</pre>
+ * @version 1.0
+ */
+public class QueryRewriterTest extends TestCase {
+    public QueryRewriterTest(String name) {
+        super(name);
+    }
+
+    @Override
+    public void setUp() throws Exception {
+        super.setUp();
+    }
+
+    @Override
+    public void tearDown() throws Exception {
+        super.tearDown();
+    }
+
+    public static Test suite() {
+        return new TestSuite(QueryRewriterTest.class);
+    }
+
+    public void testNoAdjustment() throws ParseException {
+        assertIdentity("foo", "foo");
+        assertIdentity("(+foo +- +bar)", "foo - bar");
+        assertIdentity("(+foo +bar)", "foo AND bar");
+    }
+
+    public void testScoreAdjustmentPlain() throws ParseException {
+        assertIdentity("foo^1.2", "foo^1.2");
+    }
+
+    public void testScoreAssignmentAndAdjustmentPlain() throws ParseException {
+        final String USER_INPUT = "foo bar";
+        String rewritten = assignWeight(USER_INPUT, 1.2f);
+        // So far so good. Now to eat our own dog food
+        assertIdentity("(+foo^1.2 +bar^1.2)", rewritten);
+        // Hint: Maybe quoting of - works, but does that change it into a phrase-query?
+    }
+
+    public void testScoreAssignmentAndAdjustmentDivider() throws ParseException {
+        final String USER_INPUT = "foo - bar";
+        String rewritten = assignWeight(USER_INPUT, 1.2f);
+        // So far so good. Now to eat our own dog food
+        assertIdentity("(+foo^1.2 +-^1.2 +bar^1.2)", rewritten);
+    }
+
+    private String assignWeight(String query, final float weight) throws ParseException {
+        QueryRewriter.Event event = new QueryRewriter.Event() {
+            @Override
+            public Query onQuery(TermQuery query) {
+                query.setBoost(weight);
+                return query;
+            }
+        };
+        return new QueryRewriter(event).rewrite(query);
+    }
+
+    public void testScoreAdjustmentDivider() throws ParseException {
+        assertIdentity("(+foo +-^1.2 +bar)", "foo -^1.2 bar");
+    }
+
+    public void testScoreAdjustmentDividerQuoted() throws ParseException {
+        assertIdentity("(+foo +-^1.2 +bar)", "foo \"-\"^1.2 bar"); // Shouldn't - be in quotes
+    }
+
+    private void assertIdentity(String expected, String input) throws ParseException {
+        assertEquals("Rewrite should be correct",
+                     expected, new QueryRewriter(new QueryRewriter.Event()).rewrite(input));
+    }
+}
