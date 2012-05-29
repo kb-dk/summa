96545d6299457a4010b68f8599cc824dc3f58076
Toke Eskildsen <te@ekot.dk>
2012-05-29 14:14:45 +0000

    Work in progress: Added problem description to SolrSearchConfig

--- SBSolr/src/test/java/dk/statsbiblioteket/solr/SolrSearchConfigTest.java
+++ SBSolr/src/test/java/dk/statsbiblioteket/solr/SolrSearchConfigTest.java
@@ -3,6 +3,7 @@ package dk.statsbiblioteket.solr;
 import static org.junit.Assert.*;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
@@ -28,83 +29,84 @@ import org.mortbay.jetty.Server;
 import org.mortbay.jetty.testing.HttpTester;
 import org.mortbay.jetty.testing.ServletTester;
 import org.mortbay.jetty.webapp.WebAppContext;
- 
+
 public class SolrSearchConfigTest {
-	
-	String solrWarPath="target/summa-sbsolr-1.8.0-20120529-trunk-SNAPSHOT.war";
-	
-	
-	String solrHome= "src/test/tomcat/solr"; //data-dir (index) will be created here.
-	String context="/solr"; 
-	int port = 8983;
-    
-	 @Before    
-	    public void setUp() throws Exception {             
-	   System.out.println("before called");
-		 System.setProperty("basedir", ".");          		   	          
-	    }
-	
-          
+
+    String solrWarPath = "summa-sbsolr-1.8.0-20120529-trunk-SNAPSHOT.war";
+    //String solrWarPath="/home/te/projects/summa-trunk/SBSolr/target/summa-sbsolr-1.8.0-20120529-trunk-SNAPSHOT.war";
+
+
+    //String solrHome = "src/test/tomcat/solr"; //data-dir (index) will be created here.
+    String solrHome = "/home/te/projects/summa-trunk/SBSolr/src/test/tomcat/solr"; //data-dir (index) will be created here.
+    String context = "/solr";
+    int port = 8983;
+
+    @Before
+    public void setUp() throws Exception {
+        System.setProperty("basedir", ".");
+    }
+
+
     @Test
     public void testEmbeddedJettyWithSolr() throws Exception {
-    
-    	
-    	//Start up webserver
-        EmbeddedJettyWithSolrServer server=  new EmbeddedJettyWithSolrServer(solrHome,solrWarPath,context,port);           
+        System.out.println("The problem is that IDEA has 'target/test-classes' in the class path and not 'target' "
+                           + "itself where the war resides. We need to generate the war and put it in the classpath "
+                           + "(i.e. target/test-classes or whereever resources are normally copied)");
+        System.out.println(Thread.currentThread().getContextClassLoader().getResource(solrWarPath));
+        assertTrue("The Solr war at " + solrWarPath + " should exist", new File(solrWarPath).exists());
+        //Start up webserver
+        EmbeddedJettyWithSolrServer server=  new EmbeddedJettyWithSolrServer(solrHome, solrWarPath, context, port);
         server.run();
-          //Thread.sleep(10000000L);      
+        Thread.sleep(10000000L);
         //Wrap the server with HTTPSolrServer
         //I think this is the prefered way we should do it.
-         String url = server.getServerUrl();    	
-    	 SolrServer server1 = new HttpSolrServer( url );
-  	    // Thread.sleep(500000);
-    	 //This is not found
-    	 SolrParams params = new SolrQuery("eeerere");
-         QueryResponse response = server1.query(params);
-         assertEquals(0L, response.getResults().getNumFound());
-    	 //"video" is found 3 times in my SOLR example index.
-         params = new SolrQuery("*:*");
-         
-         response = server1.query(params);
-         assertEquals(3L, response.getResults().getNumFound());
-    
+        String url = server.getServerUrl();
+        SolrServer server1 = new HttpSolrServer(url);
+        // Thread.sleep(500000);
+        //This is not found
+        SolrParams params = new SolrQuery("eeerere");
+        QueryResponse response = server1.query(params);
+        assertEquals(0L, response.getResults().getNumFound());
+        //"video" is found 3 times in my SOLR example index.
+        params = new SolrQuery("*:*");
+
+        response = server1.query(params);
+        assertEquals(3L, response.getResults().getNumFound());
+
         //  Direct REST call
-        String httpResponse= callURL(url+"/select/?indent=on&q=video&fl=name,id");
-        System.out.print(httpResponse);        
+        String httpResponse = callURL(url+"/select/?indent=on&q=video&fl=name,id");
+        System.out.print(httpResponse);
     }
-    
-       
-    
+
+
+
     /*
-     * Just a simple way to call a HTTP Rest service without Jersey
-     * 
-     */
+    * Just a simple way to call a HTTP Rest service without Jersey
+    *
+    */
     private String callURL(String urlPath){
-        StringBuffer response = new StringBuffer();    
-        BufferedReader reader = null;
-        URL url = null;  
-        try {         
-            url = new URL(urlPath);
+        StringBuilder response = new StringBuilder();
+        try {
+            URL url = new URL(urlPath);
             HttpURLConnection connection = (HttpURLConnection) url.openConnection();
             connection.setRequestMethod("GET");
             connection.setReadTimeout(10000); //10 secs, but only called once.
             connection.setConnectTimeout(10000); //10 secs, but only called once.
             connection.connect();
-            reader = new BufferedReader(new InputStreamReader(connection.getInputStream(), "UTF-8"));
+            BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream(), "UTF-8"));
 
-            String line = null;
-            while ((line = reader.readLine()) != null)
-            {        
-            response.append(line);
+            String line;
+            while ((line = reader.readLine()) != null) {
+                response.append(line);
             }
             reader.close();
-     
+
         } catch (Exception e) {
-           e.printStackTrace();
+            e.printStackTrace();
         }
         return response.toString();
-        
+
     }
-    
-    
+
+
 }
