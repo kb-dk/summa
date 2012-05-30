1df5b44299799c8104078ef8d198e9657cc25632
Toke Eskildsen <te@ekot.dk>
2012-05-30 13:52:15 +0000

    Testing: Tweaked embedded Solr test framework to work independently of current directory

--- Core/src/test/java/dk/statsbiblioteket/summa/support/embeddedsolr/EmbeddedJettyWithSolrServer.java
+++ Core/src/test/java/dk/statsbiblioteket/summa/support/embeddedsolr/EmbeddedJettyWithSolrServer.java
@@ -1,21 +1,25 @@
 package dk.statsbiblioteket.summa.support.embeddedsolr;
 
 import java.io.File;
+
+import dk.statsbiblioteket.summa.common.configuration.Resolver;
 import org.mortbay.jetty.Server;
 import org.mortbay.jetty.webapp.WebAppContext;
 
 public class EmbeddedJettyWithSolrServer extends Thread {
 	private Server server;
 	private String serverUrl;
-	private static String default_solrWarPath = "target/test-classes/support/solr.war";
-	
+	//private static String default_solrWarPath = "Core/target/test-classes/support/solr.war";
+	private static String default_solrWarPath = "support/solr.war";
+
 	/*
 	 *Starts up a Jetty server running the SOLR instance. 
 	 * 
 	 */
 	public EmbeddedJettyWithSolrServer (String solr_solr_home_dir, String solr_war_path , String context, int port) throws Exception{
 	    this.setDaemon(true);   		  
-	    
+
+        solr_war_path = Resolver.getFile(solr_war_path).toString();
 	    if (!new File(solr_war_path ).exists()){
 	    	throw new Exception("The Solr war at " + solr_war_path  + " should exist. Run 'mvn clean install -DskipTests' in the core-module");	
 	    }
@@ -27,7 +31,8 @@ public class EmbeddedJettyWithSolrServer extends Thread {
 						
 		//Hvis solr fejler under opstart med "mockedint classcast exception" er det fordi denne jar mangler på jetty's classpath.
 		//Det er uklart hvorfor dette bliver aktiveret når man kører unittest     	
-    	 webapp.setExtraClasspath("target/test-classes/support/lucene-test-framework-4.0.jar");
+    	 webapp.setExtraClasspath(Thread.currentThread().getContextClassLoader().getResource(
+             "support/lucene-test-framework-4.0.jar").getFile().toString());
 		
 		server.setHandler(webapp);			             
 		serverUrl="http://localhost:"+port+context;
