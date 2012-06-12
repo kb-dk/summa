4e9f31435684fb3dd2f73800619da493d372d8e8
Zoozie <Zoozie@6c0f5c6f-88b2-4050-bb98-6ed3ea504abe>
2012-06-12 13:01:28 +0000

    Unittest

--- trunk/Core/src/test/java/dk/statsbiblioteket/summa/support/embeddedsolr/SolrServerUnitTestUtil.java
+++ trunk/Core/src/test/java/dk/statsbiblioteket/summa/support/embeddedsolr/SolrServerUnitTestUtil.java
@@ -7,10 +7,20 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.apache.commons.io.FileUtils;
 
+import dk.statsbiblioteket.summa.common.Record;
+import dk.statsbiblioteket.summa.common.configuration.Configuration;
 import dk.statsbiblioteket.summa.common.configuration.Resolver;
+import dk.statsbiblioteket.summa.common.filter.Payload;
+import dk.statsbiblioteket.summa.common.filter.object.ObjectFilter;
+import dk.statsbiblioteket.summa.common.unittest.PayloadFeederHelper;
+import dk.statsbiblioteket.summa.index.IndexController;
+import dk.statsbiblioteket.summa.index.IndexControllerImpl;
+import dk.statsbiblioteket.summa.support.solr.SolrManipulator;
 
 public class SolrServerUnitTestUtil {
 
@@ -69,12 +79,50 @@ public class SolrServerUnitTestUtil {
 	 * so the solrhome files are not replaced. 
 	 * 
 	 */
-	public static void  populateSolrHome(File solr_home, File solr_home_default) throws IOException{			
+	public static void populateSolrHome(File solr_home, File solr_home_default) throws IOException{			
 		   String solrHome_merged=solr_home.getAbsolutePath()+SOLR_HOME_SUFFIX;
 		    File  solrHome_merged_dir = new File(solrHome_merged);			
 			FileUtils.deleteDirectory(solrHome_merged_dir); //Clean up first
 		    FileUtils.copyDirectory(solr_home_default, solrHome_merged_dir);		
 		    FileUtils.copyDirectory(solr_home, solrHome_merged_dir);					
-		}
+	}
+	
+	/**
+	 *  Ingest all document specified in the String[].   
+	 * 
+	 */
+	public static void ingestFiles(String[] files) throws Exception{
+		  ObjectFilter data = getDataProvider(files);
+	      ObjectFilter indexer = getIndexer();
+	      indexer.setSource(data);
+	      for (int i = 0 ; i < files.length ; i++) {
+	    	  indexer.next();
+	      }
+	      indexer.close(true);       	 
+		 
+	 }
+    
+    private static ObjectFilter getDataProvider(String[] files) throws IOException {
+        List<Payload> samples = new ArrayList<Payload>(files.length);
+        for (int i = 0 ; i < files.length ; i++) {
+        	Record record = new Record(
+                    "doc" + i, "test_base", Resolver.getUTF8Content(//Doc1, doc 2. etc.. Actually in test this will be the real recordId in the index
+                    files[i]).getBytes("utf-8"));
+        	Payload payload = new Payload(record);
+        	payload.getRecord().setDeleted(false);
+            samples.add(payload);
+        }
+        return new PayloadFeederHelper(samples);
+    }
 
+    private static IndexController getIndexer() throws IOException {
+        Configuration controllerConf = Configuration.newMemoryBased(IndexController.CONF_FILTER_NAME, "testcontroller");
+        Configuration manipulatorConf = controllerConf.createSubConfigurations(IndexControllerImpl.CONF_MANIPULATORS, 1).get(0);
+        manipulatorConf.set(IndexControllerImpl.CONF_MANIPULATOR_CLASS, SolrManipulator.class.getCanonicalName());
+        manipulatorConf.set(SolrManipulator.CONF_ID_FIELD, "recordId"); // 'id' is the default ID field for Solr
+        return new IndexControllerImpl(controllerConf);
+    }
+
+    	
+	
 }
