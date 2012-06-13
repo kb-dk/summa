c8358e7073bcf63d14d98bef0d052b1fe0e59b1a
Zoozie <Zoozie@6c0f5c6f-88b2-4050-bb98-6ed3ea504abe>
2012-06-13 11:42:04 +0000

    SOLR Unitests

--- trunk/Core/src/test/java/dk/statsbiblioteket/summa/support/embeddedsolr/SolrDidYouMeanTest.java
+++ trunk/Core/src/test/java/dk/statsbiblioteket/summa/support/embeddedsolr/SolrDidYouMeanTest.java
@@ -30,6 +30,7 @@ import org.apache.solr.client.solrj.SolrServer;
 import org.apache.solr.client.solrj.impl.HttpSolrServer;
 import org.apache.solr.client.solrj.response.FacetField;
 import org.apache.solr.client.solrj.response.QueryResponse;
+import org.apache.solr.client.solrj.response.SpellCheckResponse;
 import org.apache.solr.client.solrj.response.SpellCheckResponse.Suggestion;
 import org.apache.solr.common.params.SolrParams;
 import org.junit.Before;
@@ -51,32 +52,76 @@ public class SolrDidYouMeanTest {
     }
 
     @Test
-    public void testFacets() throws Exception {
+    public void testDidYouMean() throws Exception {
 
     	//Data:
     	//3 books by Thomas Egense, 1 located at Matematik, 2 at Statsbiblioteket. 2 from 2011, 1 from 2012
     	//2 books by Toke Eskildsen, 2 located at Statsbiblioteket, 1 from 2012, 1 from 2010
         //Facets defined for year,location_normalised,author_normalised
     	String[] files = new String[]{
-    			"support/solr_test_documents/didyoumeantest.xml",
-    		
+    			"support/solr_test_documents/didyoumeantest.xml",    		
     	};
         SolrServerUnitTestUtil.indexFiles(files);
              	    	                
+        
+        //Test of a single word. Missing prefix
+        // gense -> egense 
         SolrQuery query = new SolrQuery("gense");
         query.setParam("spellcheck","true");
         query.setParam("spellcheck.dictionary","summa_spell");        
         query.setParam("spellcheck.count","5");
-        
-        
+                
         QueryResponse response = solrServer.query(query);
         Map<String, Suggestion> suggestionMap = response.getSpellCheckResponse().getSuggestionMap();
+        assertTrue(suggestionMap.size() == 1);                
+                        
+        Suggestion suggestion = response.getSpellCheckResponse().getSuggestions().get(0);
+        assertEquals("gense", suggestion.getToken());
+        assertTrue(suggestion.getAlternatives().contains("egense"));
         
-        
+                                
+        //Single word, midde part wrong
+        // hwllo -> hello 
+        query = new SolrQuery("hwllo");
+        query.setParam("spellcheck","true");
+        query.setParam("spellcheck.dictionary","summa_spell"); //Todo summa spell        
+        query.setParam("spellcheck.count","5");
                 
+        response = solrServer.query(query);
+        suggestionMap = response.getSpellCheckResponse().getSuggestionMap();
+        assertTrue(suggestionMap.size() == 1);                
+                        
+        suggestion = response.getSpellCheckResponse().getSuggestions().get(0);
+        assertEquals("hwllo", suggestion.getToken());
+        assertTrue(suggestion.getAlternatives().contains("hello"));
+        
+
+        //Test of a setentence
+        // thomas exense -> thomas egense 
+        query = new SolrQuery("thomas exense"); 
+        query.setParam("spellcheck","true");
+        query.setParam("spellcheck.dictionary","summa_spell");        
+        query.setParam("spellcheck.extendedResults","true");
+        query.setParam("spellcheck.collateExtendedResults","true");
+        query.setParam("spellcheck.maxCollationTries","5");
+        query.setParam("spellcheck.collate","true");
+        query.setParam("spellcheck.count","5");
+           
+        //String httpResponse = SolrServerUnitTestUtil.callURL(server.getServerUrl()+"/select/?spellcheck=true&spellcheck.dictionary=summa_spell&spellcheck.extendedResults=true&spellcheck.maxCollationsTries=5&spellcheck.collate=true&spellcheck.collateExtendedResults=true&spellcheck.count=5&q=Thomas%20Exense");
+        //System.out.println(httpResponse);
+        
+        response = solrServer.query(query);
+        suggestionMap = response.getSpellCheckResponse().getSuggestionMap();
+        //This method gives the modified query. Very usefull
+        String collatedResult = response.getSpellCheckResponse().getCollatedResult();                
+        assertEquals("thomas egense",collatedResult);
+
+        
+        
+        
         //  Direct REST call. /edismax is defined to search in all summa-fields with boosts        
-        String httpResponse = SolrServerUnitTestUtil.callURL(server.getServerUrl()+"/select/?spellcheck=true&spellcheck.dictionary=summa_spell&spellcheck.extendedResults=true&spellcheck.maxCollationsTries=5&spellcheck.collate=true&spellcheck.collateExtendedResults=true&spellcheck.count=5&q=gense");
-     System.out.println(httpResponse);
+        //  String httpResponse = SolrServerUnitTestUtil.callURL(server.getServerUrl()+"/select/?spellcheck=true&spellcheck.dictionary=summa_spell&spellcheck.extendedResults=true&spellcheck.maxCollationsTries=5&spellcheck.collate=true&spellcheck.collateExtendedResults=true&spellcheck.count=5&q=gense");
+        //  System.out.println(httpResponse);
         
     }
 
