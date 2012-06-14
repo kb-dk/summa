f04757e90245cb30a36fea5ca43e774651620c26
Zoozie <Zoozie@6c0f5c6f-88b2-4050-bb98-6ed3ea504abe>
2012-06-14 11:47:00 +0000

    SOLR Unitests:transliteration

---
+++ trunk/Core/src/test/java/dk/statsbiblioteket/summa/support/embeddedsolr/RelaxedReplaceTransliterationTest.java
@@ -0,0 +1,49 @@
+package dk.statsbiblioteket.summa.support.embeddedsolr;
+
+import static org.junit.Assert.*;
+
+import org.apache.solr.client.solrj.SolrQuery;
+import org.apache.solr.client.solrj.SolrServer;
+import org.apache.solr.client.solrj.impl.HttpSolrServer;
+import org.apache.solr.client.solrj.response.QueryResponse;
+import org.junit.Before;
+import org.junit.Test;
+
+import dk.statsbiblioteket.summa.common.configuration.Resolver;
+
+public class RelaxedReplaceTransliterationTest {
+
+	String solrHome = "support/solr_home1"; //data-dir (index) will be created here.  
+	EmbeddedJettyWithSolrServer server;
+	SolrServer solrServer;
+
+
+	@Before
+	public void setUp() throws Exception {
+		System.setProperty("basedir", "."); //for logback
+		server =  new EmbeddedJettyWithSolrServer(Resolver.getFile(solrHome).toString());
+		server.run();
+		solrServer = new HttpSolrServer(server.getServerUrl());		
+	}
+
+	@Test
+	public void testVoidTransliteration() throws Exception {
+
+		//the document contains "Thomas-Egense ¼" which is mapped to "Thomas Egense 1 4" 
+		String[] files = new String[]{
+				"support/solr_test_documents/relaxed_replace_transliteration_doc.txt"
+		
+		};
+		SolrServerUnitTestUtil.indexFiles(files);
+		SolrQuery query = new SolrQuery("\"Thomas og Toke\"");
+		QueryResponse response = solrServer.query(query);
+		assertEquals(1L, response.getResults().getNumFound());
+
+	    query = new SolrQuery("\"Thomas & Toke\"");
+		response = solrServer.query(query);
+		assertEquals(1L, response.getResults().getNumFound());
+		
+		
+	}
+
+}
