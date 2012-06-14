fe8ebe00d46bffdc4f3b12a0cb5378c771706069
sb-brj <sb-brj@6c0f5c6f-88b2-4050-bb98-6ed3ea504abe>
2012-06-14 07:49:22 +0000

    added a context listener to get SOLR_HOME from jndi if not defined

---
+++ trunk/SBSolr/src/main/java/dk/statsbiblioteket/summa/summasbsolr/listener/JNDIContextListener.java
@@ -0,0 +1,30 @@
+//$Id$
+package dk.statsbiblioteket.summa.summasbsolr.listener;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+public class JNDIContextListener implements ServletContextListener {
+    public void contextInitialized(ServletContextEvent event) {
+        if(System.getProperty("solr.home") == null) {
+
+            try {
+                Context initContext = new InitialContext();
+                Context envContext = (Context) initContext.lookup("java:/comp/env");
+
+                String home = (String) envContext.lookup("solr/home");
+
+                if(home != null) {
+                    System.setProperty("solr.home",home);
+                }
+            } catch (Exception e) {
+                //ignore
+            }
+        }
+    }
+
+    public void contextDestroyed(ServletContextEvent event) {
+    }
+}
