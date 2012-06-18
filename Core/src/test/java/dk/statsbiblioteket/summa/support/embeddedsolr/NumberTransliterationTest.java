806d7e495e6b05eb66bb136066eaf5605746fca4
Thomas Egense <thomas.egense@gmail.com>
2012-06-18 11:57:27 +0000

    SOLR:transliteration

---
+++ Core/src/test/java/dk/statsbiblioteket/summa/support/embeddedsolr/NumberTransliterationTest.java
@@ -0,0 +1,48 @@
+package dk.statsbiblioteket.summa.support.embeddedsolr;
+
+import static org.junit.Assert.*;
+
+import org.apache.solr.client.solrj.SolrQuery;
+import org.apache.solr.client.solrj.SolrServer;
+import org.apache.solr.client.solrj.impl.HttpSolrServer;
+import org.apache.solr.client.solrj.response.QueryResponse;
+import org.junit.Before;
+import org.junit.Test;
+
+import dk.statsbiblioteket.summa.common.configuration.Resolver;
+
+public class NumberTransliterationTest {
+
+	String solrHome = "support/solr_home1"; //data-dir (index) will be created here.  
+	EmbeddedJettyWithSolrServer server;
+	SolrServer solrServer;
+
+
+	@Before
+	public void setUp() throws Exception {
+		System.setProperty("basedir", "."); //for logback
+		server =  new EmbeddedJettyWithSolrServer(Resolver.getFile(solrHome).toString());
+		server.run();
+		solrServer = new HttpSolrServer(server.getServerUrl());		
+	}
+
+	@Test
+	public void testNumberTransliteration() throws Exception {
+
+		//the document contains isbn "X978-3-16-148410-0" which is mapped to "x9783161484100"" 
+		String[] files = new String[]{
+				"support/solr_test_documents/number_transliteration_doc.txt",
+		
+		};
+		SolrServerUnitTestUtil.indexFiles(files);
+		SolrQuery query = new SolrQuery("x9783161484100");
+		QueryResponse response = solrServer.query(query);
+		assertEquals(1L, response.getResults().getNumFound());
+
+		query = new SolrQuery("X978-3-16-148410-0");
+		response = solrServer.query(query);
+		assertEquals(1L, response.getResults().getNumFound());				
+	
+	}
+
+}
