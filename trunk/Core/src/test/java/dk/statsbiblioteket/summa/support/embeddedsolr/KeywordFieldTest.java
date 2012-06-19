9895271f26ec9c33158c0e40f1e245308856e4cd
Zoozie <Zoozie@6c0f5c6f-88b2-4050-bb98-6ed3ea504abe>
2012-06-19 11:20:22 +0000

    SOLR:config

---
+++ trunk/Core/src/test/java/dk/statsbiblioteket/summa/support/embeddedsolr/KeywordFieldTest.java
@@ -0,0 +1,53 @@
+package dk.statsbiblioteket.summa.support.embeddedsolr;
+
+import static org.junit.Assert.*;
+
+import org.apache.solr.client.solrj.SolrQuery;
+import org.apache.solr.client.solrj.SolrServer;
+import org.apache.solr.client.solrj.impl.HttpSolrServer;
+import org.apache.solr.client.solrj.response.QueryResponse;
+import org.junit.Before;
+import org.junit.Test;
+
+import dk.statsbiblioteket.summa.common.configuration.Resolver;
+
+public class KeywordFieldTest {
+
+	String solrHome = "support/solr_home1"; //data-dir (index) will be created here.  
+	EmbeddedJettyWithSolrServer server;
+	SolrServer solrServer;
+
+
+	@Before
+	public void setUp() throws Exception {
+		System.setProperty("basedir", "."); //for logback
+		server =  new EmbeddedJettyWithSolrServer(Resolver.getFile(solrHome).toString());
+		server.run();
+		solrServer = new HttpSolrServer(server.getServerUrl());		
+	}
+
+	@Test
+	public void testNumberTransliteration() throws Exception {
+
+		//field lma_long is fieldtype:keyword
+		//"ABC   DEF_GI J" is mapped to abc def gi j
+		String[] files = new String[]{
+				"support/solr_test_documents/keywordfieldtest_doc.txt",
+		
+		};
+		SolrServerUnitTestUtil.indexFiles(files);
+		SolrQuery query = new SolrQuery("ABC   DEF_GI J");
+		QueryResponse response = solrServer.query(query);
+	//	assertEquals(1L, response.getResults().getNumFound());
+
+		query = new SolrQuery("abc def gi j");
+		response = solrServer.query(query);
+		assertEquals(1L, response.getResults().getNumFound());				
+	
+		query = new SolrQuery("abc\\   def\\ gi\\ j");
+		response = solrServer.query(query);
+		assertEquals(1L, response.getResults().getNumFound());
+		
+	}
+
+}
