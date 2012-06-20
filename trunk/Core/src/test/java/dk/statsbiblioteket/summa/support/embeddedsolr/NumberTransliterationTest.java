9a46ba4eb94df0a55410301a1122b37f1451ee58
Zoozie <Zoozie@6c0f5c6f-88b2-4050-bb98-6ed3ea504abe>
2012-06-20 06:15:39 +0000

    SOLR:config

--- trunk/Core/src/test/java/dk/statsbiblioteket/summa/support/embeddedsolr/NumberTransliterationTest.java
+++ trunk/Core/src/test/java/dk/statsbiblioteket/summa/support/embeddedsolr/NumberTransliterationTest.java
@@ -30,16 +30,17 @@ public class NumberTransliterationTest {
 	public void testNumberTransliteration() throws Exception {
 
 		//the document contains isbn "X978-3-16-148410-0" which is mapped to "x9783161484100"" 
+		//also numbers-field are mapped to isbn (and other fields)
 		String[] files = new String[]{
 				"support/solr_test_documents/number_transliteration_doc.txt",
 		
 		};
 		SolrServerUnitTestUtil.indexFiles(files);
-		SolrQuery query = new SolrQuery("x9783161484100");
+		SolrQuery query = new SolrQuery("numbers:x9783161484100");
 		QueryResponse response = solrServer.query(query);
 		assertEquals(1L, response.getResults().getNumFound());
 
-		query = new SolrQuery("X978-3-16-148410-0");
+		query = new SolrQuery("numbers:X978-3-16-148410-0");
 		response = solrServer.query(query);
 		assertEquals(1L, response.getResults().getNumFound());				
 	
