5b23dcc81e6382b097f6f5671797845d29f6a0d7
zoozie <zoozie@6c0f5c6f-88b2-4050-bb98-6ed3ea504abe>
2012-07-02 07:45:28 +0000

    TEST:Solr

---
+++ trunk/Core/src/test/java/dk/statsbiblioteket/summa/support/embeddedsolr/WildCardTest.java
@@ -0,0 +1,82 @@
+package dk.statsbiblioteket.summa.support.embeddedsolr;
+
+import static org.junit.Assert.*;
+
+import org.apache.solr.client.solrj.SolrQuery;
+import org.apache.solr.client.solrj.SolrServer;
+import org.apache.solr.client.solrj.impl.HttpSolrServer;
+import org.apache.solr.client.solrj.response.QueryResponse;
+import org.junit.Before;
+import org.junit.Test;
+
+import dk.statsbiblioteket.summa.common.configuration.Resolver;
+
+public class WildCardTest {
+
+	String solrHome = "support/solr_home1"; //data-dir (index) will be created here.  
+	EmbeddedJettyWithSolrServer server;
+	SolrServer solrServer;
+
+
+	@Before
+	public void setUp() throws Exception {
+		System.setProperty("basedir", "."); //for logback
+		server =  new EmbeddedJettyWithSolrServer(Resolver.getFile(solrHome).toString());
+		server.run();
+		solrServer = new HttpSolrServer(server.getServerUrl());		
+	}
+
+	@Test
+	public void testKeywordField() throws Exception {
+
+		//recordbase:
+		//title: ABCDEFG 12345678
+		
+		String[] files = new String[]{
+				"support/solr_test_documents/wildcard_doc.txt",		
+		};
+		//test title first						
+		SolrServerUnitTestUtil.indexFiles(files);
+		SolrQuery query = new SolrQuery("title:*");
+		QueryResponse response = solrServer.query(query);
+	  	assertEquals(1L, response.getResults().getNumFound());
+	
+    	SolrServerUnitTestUtil.indexFiles(files);
+		query = new SolrQuery("title:abc*");
+	    response = solrServer.query(query);
+	  	assertEquals(1L, response.getResults().getNumFound());
+
+	  	SolrServerUnitTestUtil.indexFiles(files);
+		query = new SolrQuery("title:abc*efg");
+	    response = solrServer.query(query);
+	  	assertEquals(1L, response.getResults().getNumFound());
+	  	
+	  	SolrServerUnitTestUtil.indexFiles(files);
+		query = new SolrQuery("title:ab*fg");
+	    response = solrServer.query(query);
+	  	assertEquals(1L, response.getResults().getNumFound());
+	  	
+    	SolrServerUnitTestUtil.indexFiles(files);
+		query = new SolrQuery("title:abc*def");//Not found. both * and d
+	    response = solrServer.query(query);
+	  	assertEquals(0L, response.getResults().getNumFound());
+	  	
+	 
+	  	SolrServerUnitTestUtil.indexFiles(files);
+		query = new SolrQuery("title:*ef");//not found missing g
+	    response = solrServer.query(query);
+	  	assertEquals(0L, response.getResults().getNumFound());
+	  	
+
+	  	SolrServerUnitTestUtil.indexFiles(files);
+		query = new SolrQuery("title:*efg");
+	    response = solrServer.query(query);
+	  	assertEquals(1L, response.getResults().getNumFound());
+	  	
+	  	//test recordbase
+	  	
+	  	
+		
+	}
+
+}
