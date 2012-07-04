0138602eec7b6726929543259d4fdb3d1cfad2a6
Thomas Egense <thomas.egense@gmail.com>
2012-07-04 09:43:36 +0000

    SOLR:Tests

---
+++ Core/src/test/java/dk/statsbiblioteket/summa/support/embeddedsolr/SpecialCharacterTest.java
@@ -0,0 +1,53 @@
+package dk.statsbiblioteket.summa.support.embeddedsolr;
+
+import static org.junit.Assert.*;
+
+import org.apache.solr.client.solrj.SolrQuery;
+import org.apache.solr.client.solrj.SolrServer;
+import org.apache.solr.client.solrj.impl.HttpSolrServer;
+import org.apache.solr.client.solrj.response.QueryResponse;
+import org.junit.Before;
+import org.junit.Test;
+
+import dk.statsbiblioteket.summa.common.configuration.Resolver;
+
+public class SpecialCharacterTest {
+
+	String solrHome = "support/solr_home1"; //data-dir (index) will be created here.  
+	EmbeddedJettyWithSolrServer server;
+	SolrServer solrServer;
+	
+	@Before
+	public void setUp() throws Exception {
+		System.setProperty("basedir", "."); //for logback
+		server =  new EmbeddedJettyWithSolrServer(Resolver.getFile(solrHome).toString());
+		server.run();
+		solrServer = new HttpSolrServer(server.getServerUrl());		
+	}
+
+	@Test
+	public void testChineseCharacters() throws Exception {
+
+		/*
+	      doc 1: title:"ABCD-EFGH" (notice analyzer will make it abcd efgh)
+	      doc 2: title:"ABCD"
+	    */
+		String[] files = new String[]{
+				"support/solr_test_documents/special_character_test_doc1.txt",
+				"support/solr_test_documents/special_character_test_doc2.txt",
+		};
+		SolrServerUnitTestUtil.indexFiles(files);
+		
+		
+		//TODO: FIX. Seems summa handles the '-' character if there is no whitespace before
+		//We only want to find doc1
+		SolrQuery query = new SolrQuery("abcd-efgh");
+ 		
+		QueryResponse response = solrServer.query(query);
+	  	assertEquals(1L, response.getResults().getNumFound());
+	  
+	  	
+	  	
+	}
+
+}
