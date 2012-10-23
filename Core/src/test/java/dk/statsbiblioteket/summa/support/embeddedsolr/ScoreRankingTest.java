18a0754696004b68769f6beca7cde8b6f9d50398
Toke Eskildsen <te@ekot.dk>
2012-10-23 08:05:14 +0000

    Testing: Further tweaks to ScoreRankingTest in order to simulate production

--- Core/src/test/java/dk/statsbiblioteket/summa/support/embeddedsolr/ScoreRankingTest.java
+++ Core/src/test/java/dk/statsbiblioteket/summa/support/embeddedsolr/ScoreRankingTest.java
@@ -14,40 +14,48 @@ import dk.statsbiblioteket.summa.common.configuration.Resolver;
 
 public class ScoreRankingTest {
 
-	String solrHome = "support/solr_home1"; //data-dir (index) will be created here.  
-	EmbeddedJettyWithSolrServer server;
-	SolrServer solrServer;
+    String solrHome = "support/solr_home1"; //data-dir (index) will be created here.
+    EmbeddedJettyWithSolrServer server;
+    SolrServer solrServer;
 
 
-	@Before
-	public void setUp() throws Exception {
-		System.setProperty("basedir", "."); //for logback
-		server =  new EmbeddedJettyWithSolrServer(Resolver.getFile(solrHome).toString());
-		server.run();
-		solrServer = new HttpSolrServer(server.getServerUrl());		
-	}
+    @Before
+    public void setUp() throws Exception {
+        System.setProperty("basedir", "."); //for logback
+        server =  new EmbeddedJettyWithSolrServer(Resolver.getFile(solrHome).toString());
+        server.run();
+        solrServer = new HttpSolrServer(server.getServerUrl());
+    }
 
-	@Test
-	public void testScoreRanking() throws Exception {
-		
-		String[] files = new String[]{
-				"support/solr_test_documents/score_ranking_doc.txt"
-		
-		};
-		SolrServerUnitTestUtil.indexFiles(files);
+    @Test
+    public void testScoreRanking() throws Exception {
 
-	    SolrQuery query = new SolrQuery("\"Thomas og Toke\"");
-		QueryResponse response = solrServer.query(query);
-		assertEquals(1L, response.getResults().getNumFound());
-		SolrDocument solrDocument = response.getResults().get(0);
-		Float score= (Float) solrDocument.getFieldValue("score");
-		assertTrue("Insane boost bug not fixed, score="+score, score < 10000000);
+        String[] files = new String[]{
+            "support/solr_test_documents/score_ranking_doc.txt",
+            "support/solr_test_documents/doc1.xml",
+            "support/solr_test_documents/doc1_sort.xml",
+            "support/solr_test_documents/doc2.xml",
+            "support/solr_test_documents/doc2_sort.xml",
+            "support/solr_test_documents/doc3.xml",
+            "support/solr_test_documents/doc3_sort.xml",
+            "support/solr_test_documents/doc4.xml",
+            "support/solr_test_documents/doc5.xml",
+            "support/solr_test_documents/doc6_sort.xml"
+        };
+        SolrServerUnitTestUtil.indexFiles(files);
+
+        SolrQuery query = new SolrQuery("\"Thomas og Toke\"");
+        QueryResponse response = solrServer.query(query);
+        assertEquals(1L, response.getResults().getNumFound());
+        SolrDocument solrDocument = response.getResults().get(0);
+        Float score= (Float) solrDocument.getFieldValue("score");
+        assertTrue("Insane boost bug not fixed, score="+score, score < 10000000);
         System.out.println("Got score: " + score);
-		
-		
-		
-	//Thread.sleep(1000000000000000L);	
-		
-	}
+
+
+
+        //Thread.sleep(1000000000000000L);
+
+    }
 
 }
