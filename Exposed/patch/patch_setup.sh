5380206c8ae79457b59b573fac3d905e849b02df
Toke Eskildsen <te@ekot.dk>
2012-12-03 15:46:29 +0000

    Updated Exposed patch scripts to generate a working Lucene/Solr 4 patch

--- Exposed/patch/patch_setup.sh
+++ Exposed/patch/patch_setup.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-# Checks out Lucene 4 BETA in preparation for patch creation
+# Checks out Lucene 4 in preparation for patch creation
 rm -rf lucene_4
 
-svn checkout https://svn.apache.org/repos/asf/lucene/dev/tags/lucene_solr_4_0_0_BETA/ lucene_4
+svn checkout https://svn.apache.org/repos/asf/lucene/dev/tags/lucene_solr_4_0_0/ lucene_4
 
-# Enable this when Lucene/Solr 4.0 goes gold
-#svn checkout http://svn.apache.org/repos/asf/lucene/dev/branches/lucene_solr_4_0/ lucene_4
+#svn checkout https://svn.apache.org/repos/asf/lucene/dev/tags/lucene_solr_4_0_0_BETA/ lucene_4
+
