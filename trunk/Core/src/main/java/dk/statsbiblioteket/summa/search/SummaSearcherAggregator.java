a9c616d9bbd1cced4a54f29146bf9fa05cb3616c
toke-sb <toke-sb@6c0f5c6f-88b2-4050-bb98-6ed3ea504abe>
2012-12-19 13:53:35 +0000

    Bugfix: AdjustingSearcherAggregator did not check for null in getTransient()

--- trunk/Core/src/main/java/dk/statsbiblioteket/summa/search/SummaSearcherAggregator.java
+++ trunk/Core/src/main/java/dk/statsbiblioteket/summa/search/SummaSearcherAggregator.java
@@ -213,7 +213,7 @@ public class SummaSearcherAggregator implements SummaSearcher {
             success = true;
             return merged;
         } finally {
-            if (merged.getTransient().containsKey(DocumentSearcher.DOCIDS)) {
+            if (merged.getTransient() != null && merged.getTransient().containsKey(DocumentSearcher.DOCIDS)) {
                 Object o = merged.getTransient().get(DocumentSearcher.DOCIDS);
                 if (o instanceof DocIDCollector) {
                     ((DocIDCollector)o).close();
