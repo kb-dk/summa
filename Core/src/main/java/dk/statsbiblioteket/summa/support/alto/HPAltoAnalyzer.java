1417664e544db19c2e4e40689cd72296cfcfadaa
Toke Eskildsen <te@ekot.dk>
2013-01-18 10:48:30 +0000

    Enhancement: Better segmentation order in the HPAltoAnalyzer

--- Core/src/main/java/dk/statsbiblioteket/summa/support/alto/HPAltoAnalyzer.java
+++ Core/src/main/java/dk/statsbiblioteket/summa/support/alto/HPAltoAnalyzer.java
@@ -9,10 +9,11 @@
 package dk.statsbiblioteket.summa.support.alto;
 
 import dk.statsbiblioteket.summa.common.configuration.Configuration;
-import org.apache.commons.logging.LogFactory;
 import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -54,69 +55,138 @@ public class HPAltoAnalyzer {
     private Pattern programPattern = Pattern.compile("(PROGRAM.+)", Pattern.CASE_INSENSITIVE);
     private Pattern fromTimePattern = Pattern.compile("([0-9]{1,2}).([0-9]{2})(.*)");
     private Pattern fullTimePattern = Pattern.compile("([0-9]{1,2}).([0-9]{2})[^0-9]{1,2}([0-9]{1,2}).([0-9]{2})(.*)");
+
+    /**
+     * The heart of the analyzer tries to extract Segments from the given alto. Special segments are "Program x" that
+     * are used to mark subsequent segments with the right program.
+     * @param alto an object representation of alto XML.
+     * @return the Segments for the page in the alto (note: Currently only the first page is processed).
+     */
     public List<Segment> getSegments(Alto alto) {
-        List<Segment> segments = new ArrayList<Segment>();
+        // We'll do a lot of random access extraction so linked lists seems the obvious choice (ignoring caching)
+        final List<Alto.TextBlock> blocks = new LinkedList<Alto.TextBlock>(alto.getLayout().get(0).getPrintSpace());
+        final List<Segment> segments = new ArrayList<Segment>();
         String lastProgram = null; // Last encountered program
-        Alto.TextBlock textBlock = null;
         int hPos = 0;
-        int vPos = 0;
-        int counter = 0;
-        while (true) {
-            // TODO: Add support for multiple pages
-            textBlock = getNextTextBlock(alto.getLayout().get(0), textBlock);
-            if (textBlock == null) {
-                break;
+        int vPos = -1;
+        int maxHPos = Integer.MAX_VALUE;
+
+        while (!blocks.isEmpty()) {
+            Alto.TextBlock best = null;
+
+            // Find the first valid candidate with the given parameters
+            for (Alto.TextBlock candidate: blocks) {
+                if (candidate.getHpos() >= hPos && candidate.getVpos() > vPos && candidate.getHpos() <= maxHPos) {
+                    best = candidate;
+                    break;
+                }
             }
 
-            Matcher programMatcher = programPattern.matcher(textBlock.getAllText());
-            if (programMatcher.matches()) {
-                lastProgram = programMatcher.group(1);
-                log.trace("Found program " + lastProgram);
+            // Endless loop detection
+            if (best == null && maxHPos == Integer.MAX_VALUE) {
+                log.warn(String.format(
+                        "getSegments found %d segments with %d remaining TextBlocks, where there should be 0 remaining",
+                        segments.size(), blocks.size()));
+                return segments;
+            }
+
+            // If there are no candidate, adjust search parameters for next column
+            if (best == null) {
+                hPos = maxHPos;
+                vPos = -1;
+                maxHPos = Integer.MAX_VALUE;
                 continue;
             }
 
-            Segment segment = new Segment();
-            segment.filename = alto.getFilename();
-            segment.date = getDateFromFilename(segment.filename);
-            segment.id = "alto_" + segment.date + "_" + counter;
-            segment.program = lastProgram;
-            segment.hpos = textBlock.getHpos();
-            segment.vpos = textBlock.getVpos();
-
-            List<Alto.TextLine> lines = textBlock.getLines();
-
-            boolean gotTime = false;
-            Matcher fullTimeMatcher = fullTimePattern.matcher(lines.get(0).getAllText()); // First line
-            if (fullTimeMatcher.matches()) {
-                segment.startTime = fullTimeMatcher.group(1) + ":" + fullTimeMatcher.group(2);
-                segment.endTime = fullTimeMatcher.group(3) + ":" + fullTimeMatcher.group(4);
-                segment.title = clean(fullTimeMatcher.group(5));
-                log.trace("Found start time " + segment.startTime + " and end time " + segment.endTime + " with title "
-                          + segment.title);
-                gotTime = true;
-            } else {
-                Matcher fromTimeMatcher = fromTimePattern.matcher(lines.get(0).getAllText()); // First line
-                if (fromTimeMatcher.matches()) {
-                    segment.startTime = fromTimeMatcher.group(1) + ":" + fromTimeMatcher.group(2);
-                    segment.title = clean(fromTimeMatcher.group(3));
-                    log.trace("Found start time " + segment.startTime + " with title " + segment.title);
-                    gotTime = true;
-                }
-            }
-            if (gotTime) {
-                if (lines.size() > 1) {
-                    for (Alto.TextLine line: lines.subList(1, lines.size() - 1)) {
-                        segment.paragraphs.add(clean(line.getAllText()));
+            // See if there are a better candidate
+            for (Alto.TextBlock candidate: blocks) {
+                if (candidate.getHpos() >= hPos && candidate.getVpos() > vPos && candidate.getHpos() <= maxHPos) {
+                    // Valid. Check is the distance is better
+                    if (getDistance(hPos, vPos, candidate) < getDistance(hPos, vPos, best)) {
+                        best = candidate;
                     }
                 }
-            } else {
-                for (Alto.TextLine line: lines) {
-                    segment.paragraphs.add(clean(line.getAllText()));
-                }
             }
+
+            // We got the best block. Remove it from the pool
+            blocks.remove(best);
+            maxHPos = best.getHpos() + best.getWidth(); // Only skip to next column when the current one is exhausted
+            vPos = best.getVpos();
+
+            // Is the block a program?
+            String program = getProgram(best);
+            if (program != null) {
+                lastProgram = program;
+                continue;
+            }
+
+            // Create the segment
+            Segment segment = blockToSegment(alto, best, lastProgram);
+
+            // See if there are blocks below that belongs to the current segment
+            // TODO: Implement this
             segments.add(segment);
         }
         return segments;
+    }
+
+    // TODO: Consider weights that prefers closer vDistance over hDistance
+    private double getDistance(int hPos, int vPos, Alto.TextBlock candidate) {
+        return Math.sqrt(Math.pow(hPos-candidate.getHpos(), 2) + Math.pow(vPos-candidate.getVpos(), 2));
+    }
+
+    private String getProgram(Alto.TextBlock textBlock) {
+        Matcher programMatcher = programPattern.matcher(textBlock.getAllText());
+        if (programMatcher.matches()) {
+            String program = programMatcher.group(1);
+            log.trace("Found program " + program);
+            return program;
+        }
+        return null;
+    }
+
+    private int counter = 0;
+    private Segment blockToSegment(Alto alto, Alto.TextBlock textBlock, String program) {
+        Segment segment = new Segment();
+        segment.filename = alto.getFilename();
+        segment.date = getDateFromFilename(segment.filename);
+        segment.id = "alto_" + segment.date + "_" + counter++;
+        segment.program = program;
+        segment.hpos = textBlock.getHpos();
+        segment.vpos = textBlock.getVpos();
+
+        List<Alto.TextLine> lines = textBlock.getLines();
+
+        boolean gotTime = false;
+        Matcher fullTimeMatcher = fullTimePattern.matcher(lines.get(0).getAllText()); // First line
+        if (fullTimeMatcher.matches()) {
+            segment.startTime = fullTimeMatcher.group(1) + ":" + fullTimeMatcher.group(2);
+            segment.endTime = fullTimeMatcher.group(3) + ":" + fullTimeMatcher.group(4);
+            segment.title = clean(fullTimeMatcher.group(5));
+            log.trace("Found start time " + segment.startTime + " and end time " + segment.endTime + " with title "
+                      + segment.title);
+            gotTime = true;
+        } else {
+            Matcher fromTimeMatcher = fromTimePattern.matcher(lines.get(0).getAllText()); // First line
+            if (fromTimeMatcher.matches()) {
+                segment.startTime = fromTimeMatcher.group(1) + ":" + fromTimeMatcher.group(2);
+                segment.title = clean(fromTimeMatcher.group(3));
+                log.trace("Found start time " + segment.startTime + " with title " + segment.title);
+                gotTime = true;
+            }
+        }
+        if (gotTime) {
+            if (lines.size() > 1) {
+                for (Alto.TextLine line: lines.subList(1, lines.size() - 1)) {
+                    segment.paragraphs.add(clean(line.getAllText()));
+                }
+            }
+        } else {
+            for (Alto.TextLine line: lines) {
+                segment.paragraphs.add(clean(line.getAllText()));
+            }
+        }
+        return segment;
     }
 
     // TODO: Improve cleanup by collapsing multiple spaces and removing "unrealistic" chars
