ee2cb8e18432f87c3aed65425d980c2c55a34952
toke-sb <toke-sb@6c0f5c6f-88b2-4050-bb98-6ed3ea504abe>
2013-09-02 14:27:51 +0000

    Cleanup: Removed dependency on old Summa merge classes

--- trunk/Core/src/main/java/dk/statsbiblioteket/summa/support/harmonise/hub/HubTagAdjuster.java
+++ trunk/Core/src/main/java/dk/statsbiblioteket/summa/support/harmonise/hub/HubTagAdjuster.java
@@ -22,9 +22,6 @@ package dk.statsbiblioteket.summa.support.harmonise.hub;
 import dk.statsbiblioteket.summa.common.configuration.Configurable;
 import dk.statsbiblioteket.summa.common.configuration.Configuration;
 import dk.statsbiblioteket.summa.common.util.ManyToManyMapper;
-import dk.statsbiblioteket.summa.facetbrowser.api.FacetResult.Reliability;
-import dk.statsbiblioteket.summa.facetbrowser.api.FacetResultExternal;
-import dk.statsbiblioteket.summa.facetbrowser.api.FacetResultImpl;
 import dk.statsbiblioteket.util.Strings;
 import dk.statsbiblioteket.util.qa.QAInfo;
 import org.apache.commons.logging.Log;
@@ -128,21 +125,15 @@ public class HubTagAdjuster implements Configurable {
         return facetNames;
     }
 
+    @SuppressWarnings("UnusedParameters")
     public void adjust(SolrParams request, QueryResponse response) {
         List<FacetField> facetFields = response.getFacetFields();
         if (facetFields == null) {
             return;
         }
-        List<FacetField> adjustedFacetFields = new ArrayList<FacetField>(facetFields.size());
         for (FacetField facetField: facetFields) {
             expandTags(facetField);
         }
-    }
-
-    private void mergeAssign(List<FacetField> facetFields, List<FacetField> adjustedFacetFields) {
-        facetFields.clear();
-        // TODO: Add with merging in case of same name. Remember sorting
-        facetFields.addAll(adjustedFacetFields);
     }
 
     private void expandTags(FacetField facetField) {
@@ -151,78 +142,47 @@ public class HubTagAdjuster implements Configurable {
             return;
         }
 
-        List<FacetField.Count> tags = expandTags(facetField.getValues());
+        Map<String, Long> tags = expandTags(facetField.getValues());
         facetField.getValues().clear();
-        for (FacetField.Count tag: tags) {
-            facetField.add(tag.getName(), tag.getCount());
+        for (Map.Entry<String, Long> tag: tags.entrySet()) {
+            facetField.add(tag.getKey(), tag.getValue());
         }
     }
 
-    private List<FacetField.Count> expandTags(List<FacetField.Count> tags) {
-        List<FacetField.Count> expandedTags = new ArrayList<FacetField.Count>((int) (tags.size() * 1.5));
+    private Map<String, Long> expandTags(List<FacetField.Count> tags) {
+        Map<String, Long> expandedTags = new LinkedHashMap<String, Long>((int) (tags.size() * 1.5));
         for (FacetField.Count tag: tags) {
             Set<String> ets = map.getReverseSet(tag.getName());
             if (ets == null) {
-                expandedTags.add(tag);
+                mergeTag(expandedTags, tag.getName(), tag.getCount());
             } else {
                 for (String et: ets) {
-                    expandedTags.add(new FacetField.Count(null, et, tag.getCount()));
+                    mergeTag(expandedTags, et, tag.getCount());
                 }
             }
         }
-        return merge(expandedTags);
+        return expandedTags;
     }
 
-    private List<FacetField.Count> merge(List<FacetField.Count> tags) {
-        // TODO: Implement merging
-        return tags;
-    }
-
-    //TODO. The following method can be improve by using Reliability more instead of just using merge mode.
-    // ie MORE 4 and LESS 2 -> 4 MORE  (no matter of merge mode)
-    private void mergePut(Map<String, FacetResultImpl.Tag<String>> tags, String newName,
-                          FacetResultImpl.Tag<String> oldTag) {
-        if (!tags.containsKey(newName)) {
-            FacetResultImpl.Tag<String> newTag = new FacetResultImpl.Tag<String>(
-                    newName, oldTag.getCount(), oldTag.getReliability());
-            tags.put(newName, newTag);
+    // TODO: Consider how and if to handle reliability
+    private void mergeTag(Map<String, Long> tags, String name, long count) {
+        if (!tags.containsKey(name)) {
+            tags.put(name, count);
             return;
         }
-
-        FacetResultImpl.Tag<String> tag = tags.get(newName);
-        Reliability newR = tag.getReliability();
-        Reliability oldR = oldTag.getReliability();
-
+        Long oldCount = tags.get(name);
         switch (mergeMode) {
             case min:
-                tag.setCount(Math.min(tag.getCount(), oldTag.getCount()));
-                if (oldR == Reliability.IMPRECISE || newR == Reliability.IMPRECISE) {
-                    tag.setReliability(Reliability.IMPRECISE);
-                } else {
-                    tag.setReliability(Reliability.MORE);
-                }
+                tags.put(name, Math.min(oldCount, count));
                 break;
             case max:
-                if (oldR == Reliability.IMPRECISE || newR == Reliability.IMPRECISE) {
-                    tag.setReliability(Reliability.IMPRECISE);
-                } else {
-                    tag.setReliability(Reliability.MORE);
-                }
-
-                tag.setCount(Math.max(tag.getCount(), oldTag.getCount()));
+                tags.put(name, Math.max(oldCount, count));
                 break;
             case sum:
-                if (oldR == Reliability.IMPRECISE || newR == Reliability.IMPRECISE) {
-                    tag.setReliability(Reliability.IMPRECISE);
-                } else {
-                    tag.setReliability(Reliability.LESS);
-                }
-
-                tag.setCount(tag.getCount() + oldTag.getCount());
+                tags.put(name, oldCount + count);
                 break;
             default:
-                throw new UnsupportedOperationException(
-                        "Merge mode '" + mergeMode + "' is unknown, unsuspected and unsupported");
+                throw new IllegalArgumentException("The mergeMode '" + mergeMode + "' is unknown");
         }
     }
 
