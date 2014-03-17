747db39f273f81fbb8f37017bdd1811278bb726c
Toke Eskildsen <te@ekot.dk>
2014-03-17 12:55:56 +0000

    Bugfix: Changes to the Exposed system required tweaks to Core

--- Core/src/main/java/dk/statsbiblioteket/summa/facetbrowser/PoolFactoryGate.java
+++ Core/src/main/java/dk/statsbiblioteket/summa/facetbrowser/PoolFactoryGate.java
@@ -22,7 +22,7 @@ import org.apache.commons.logging.LogFactory;
 import org.apache.lucene.index.IndexReader;
 import org.apache.lucene.search.exposed.facet.CollectorPool;
 import org.apache.lucene.search.exposed.facet.CollectorPoolFactory;
-import org.apache.lucene.search.exposed.facet.TagCollectorMulti;
+import org.apache.lucene.search.exposed.facet.TagCollector;
 import org.apache.lucene.search.exposed.facet.request.FacetRequest;
 
 import java.io.IOException;
@@ -41,9 +41,9 @@ public class PoolFactoryGate {
 
   private static AtomicLong allocateCounter = new AtomicLong(0);
   /**
-   * Acquire a CollectorPool and TagCollectorMulti based on the given reader and request.
+   * Acquire a CollectorPool and TagCollector based on the given reader and request.
    * </p><p>
-   * Important: The TagCollectorMulti _must_ be released after usage with the code
+   * Important: The TagCollector _must_ be released after usage with the code
    * {@code collectorPool.release(query, tagCollector)}, where query can be null.
    * Failure to do so will result in a blocked CollectorPool.
    * @param factory used for generating the CollectorPool.
@@ -51,36 +51,32 @@ public class PoolFactoryGate {
    * @param key   the facet or index lookup request.
    * @param request the query to use for finding filled TagCollectors. This can be null.
    * @param caller  human readable designation of the calling code (normally "facet" or "index lookup").
-   * @return a CollectorPool and a TagCollectorMulti ready for use.
+   * @return a CollectorPool and a TagCollector ready for use.
    * @throws IOException if the CollectorPool could not be constructed.
    */
-  public static synchronized SimplePair<CollectorPool, TagCollectorMulti> acquire(
+  public static synchronized SimplePair<CollectorPool, ? extends TagCollector> acquire(
       CollectorPoolFactory factory, IndexReader reader, String key,
       FacetRequest request, String caller) throws IOException {
     CollectorPool collectorPool;
     try {
       boolean hasPool = factory.hasPool(reader, request);
       if (!hasPool) {
-        log.info("The CollectorPoolFactory has no structures for the given " +
-                 "request from " + caller + ". A new structure will be " +
-                 "generated, which can take several minutes. The request was "
-                 + request.getBuildKey());
+        log.info("The CollectorPoolFactory has no structures for the given request from " + caller + ". A new structure"
+                 + " will be generated, which can take several minutes. The request was " + request.getBuildKey());
       }
       long buildTime = -System.currentTimeMillis();
       collectorPool = factory.acquire(reader, request);
       buildTime += System.currentTimeMillis();
       if (!hasPool) {
-        log.info("The PoolFactory was successfully updated in "
-                 + buildTime/1000 + " seconds. "
-                 + "Total allocation is " + factory.toString()
-                 + " with MachineStats(" + MachineStats.stats() + ")");
+        log.info("The PoolFactory was successfully updated in " + buildTime/1000 + " seconds. "
+                 + "Total allocation is " + factory.toString() + " with MachineStats(" + MachineStats.stats() + ")");
       }
     } catch (IOException e) {
       throw new RuntimeException(
           "Unable to acquire a CollectorPool for " + request, e);
     }
 
-    TagCollectorMulti tagCollector;
+    TagCollector tagCollector;
     try {
       CollectorPool.AVAILABILITY availability =
           collectorPool.getAvailability(key);
@@ -92,16 +88,13 @@ public class PoolFactoryGate {
           log.debug("Acquiring filled tagCollector for '" + key + "'");
           break;
         case mustWait:
-          log.debug("All allowed TagCollectors are active for key '" + key
-                    + "' for " + caller);
+          log.debug("All allowed TagCollectors are active for key '" + key + "' for " + caller);
           break;
         case mustCreateNew:
-          log.info("A new TagCollector will be created for key '" + key
-                   + "' for " + caller + " from " + collectorPool);
+          log.info("A new TagCollector will be created for key '" + key + "' for " + caller + " from " + collectorPool);
           break;
         case mightCreateNew:
-          log.info("A new TagCollector might be created for key '" + key
-                   + "' for " + caller);
+          log.info("A new TagCollector might be created for key '" + key + "' for " + caller);
           break;
         default:
           log.warn("Unknown availability state: " + availability);
@@ -110,11 +103,10 @@ public class PoolFactoryGate {
       if (tagCollector.isNewborn()) {
         long a = allocateCounter.incrementAndGet();
         if (a % 100 == 0 || log.isDebugEnabled()) {
-          String message = "Allocated " + tagCollector + " #" + a
-                           + "(availability was " + availability + "). "
-                           + "CollectorPoolFactory memory allocation is now "
-                           + factory.getMem()/1048576 + "MB with MachineStats("
-                           + MachineStats.stats() + ")";
+          String message =
+              "Allocated " + tagCollector + " #" + a + "(availability was " + availability + "). "
+              + "CollectorPoolFactory memory allocation is now " + factory.getMem()/1048576 + "MB with MachineStats("
+              + MachineStats.stats() + ")";
           if (a % 100 == 0) {
             log.info(message);
           } else {
@@ -124,37 +116,33 @@ public class PoolFactoryGate {
                     + factory.toString());
         }
       }
-      return new SimplePair<CollectorPool, TagCollectorMulti>(
-          collectorPool, tagCollector);
+      return new SimplePair<CollectorPool, TagCollector>(collectorPool, tagCollector);
     } catch (OutOfMemoryError e) {
       StringWriter writer = new StringWriter(1000);
       PrintWriter pw = new PrintWriter(writer);
       e.printStackTrace(pw);
       pw.flush();
       throw new OutOfMemoryError(
-          "Encountered OOM when acquiring TagCollector for '" + key
-          + "' with full request " + request + " from pool " + collectorPool
-          + " with full factory " + factory + "\nCaused by: "
-          + writer.toString());
+          "Encountered OOM when acquiring TagCollector for '" + key + "' with full request " + request + " from pool "
+          + collectorPool + " with full factory " + factory + "\nCaused by: " + writer.toString());
     }
 
   }
 
   private static AtomicLong freeCounter = new AtomicLong(0);
   /**
-   * Release a TagCollectorMulti after use.
+   * Release a TagCollector after use.
    * @param pool      the pool for the collector.
    * @param collector a used collector.
    * @param key       the key identifying the collector content. null is valid.
    */
-  public static void release(CollectorPool pool, TagCollectorMulti collector, String key) {
+  public static void release(CollectorPool pool, TagCollector collector, String key) {
     boolean freed = pool.release(key, collector);
     if (freed) {
       long r = freeCounter.incrementAndGet();
       if (log.isDebugEnabled()) {
         log.debug(
-            "The release of a TagCollector with size "
-            + collector.getMemoryUsage()/1048576
+            "The release of a TagCollector with size " + collector.getMemoryUsage()/1048576
             + "MB for query key '" + key + "' resulted in the collector being freed. "
             + "Consider increasing the limits for the CollectorPool " + pool.getKey());
       }
