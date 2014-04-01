25231bb7975f0e2e40d8deed71d456cfd221e89a
Toke Eskildsen <te@ekot.dk>
2014-04-01 10:53:47 +0000

    Upgrade of patch generator and build files for solr 4.6.1

--- Exposed/patch/patch_full.sh
+++ Exposed/patch/patch_full.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
+
+# 
+# No assumptions patch creator: Just run & wait (takes quite a while)
+#
+
 ./patch_setup.sh
 ./patch_copy.sh
+# Build not needed for patch creation
+#./patch_build.sh
 ./patch_create.sh
--- Exposed/patch/patch_setup.sh
+++ Exposed/patch/patch_setup.sh
@@ -3,7 +3,7 @@
 # Checks out Lucene 4 in preparation for patch creation
 rm -rf lucene_4
 
-svn checkout https://svn.apache.org/repos/asf/lucene/dev/tags/lucene_solr_4_5_1/ lucene_4
+svn checkout https://svn.apache.org/repos/asf/lucene/dev/tags/lucene_solr_4_6_1/ lucene_4
 
 #svn checkout https://svn.apache.org/repos/asf/lucene/dev/tags/lucene_solr_4_0_0_BETA/ lucene_4
 
