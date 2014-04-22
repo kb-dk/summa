59415f39339d3246e3748e7e46af7a45ba61a38f
Toke Eskildsen <te@ekot.dk>
2014-04-22 13:23:32 +0000

    Feature: Limit the size of an XML block by removing elements

--- Core/src/test/java/dk/statsbiblioteket/summa/common/xml/XMLStepperTest.java
+++ Core/src/test/java/dk/statsbiblioteket/summa/common/xml/XMLStepperTest.java
@@ -14,6 +14,7 @@
  */
 package dk.statsbiblioteket.summa.common.xml;
 
+import dk.statsbiblioteket.util.Strings;
 import dk.statsbiblioteket.util.qa.QAInfo;
 import dk.statsbiblioteket.util.xml.XMLUtil;
 import junit.framework.TestCase;
@@ -23,8 +24,11 @@ import org.apache.commons.logging.LogFactory;
 import javax.xml.stream.*;
 import java.io.ByteArrayOutputStream;
 import java.io.StringReader;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.regex.Pattern;
 
 @QAInfo(level = QAInfo.Level.NORMAL,
         state = QAInfo.State.IN_DEVELOPMENT,
@@ -52,6 +56,40 @@ public class XMLStepperTest extends TestCase {
     }
     private XMLOutputFactory xmlOutFactory = XMLOutputFactory.newInstance();
 
+
+    private final static String LIMIT_BARS =
+            "<foo><bar zoo=\"true\"></bar><bar zoo=\"true\"></bar><bar zoo=\"false\"></bar><baz></baz></foo>";
+
+    public void testLimitXMLSimple() throws XMLStreamException {
+        assertLimit(LIMIT_BARS, "<foo><baz /></foo>", true,
+                    "/foo/bar", 0);
+        assertLimit(LIMIT_BARS, "<foo><bar zoo=\"true\" /><baz /></foo>", true,
+                    "/foo/bar", 1);
+        assertLimit(LIMIT_BARS, "<foo><bar zoo=\"true\" /><bar zoo=\"true\" /><baz /></foo>", true,
+                    "/foo/bar", 2);
+    }
+
+    public void testLimitXMLAttribute() throws XMLStreamException {
+        assertLimit(LIMIT_BARS, "<foo><bar zoo=\"false\" /><baz /></foo>", false,
+                    "/foo/bar#zoo=true", 0);
+        assertLimit(LIMIT_BARS, "<foo><bar zoo=\"true\" /><bar zoo=\"false\" /><baz /></foo>", false,
+                    "/foo/bar#zoo=true", 1);
+    }
+
+    private void assertLimit(String input, String expected, boolean onlyElementMatch, Object... limits)
+            throws XMLStreamException {
+        Map<Pattern, Integer> lims = new HashMap<Pattern, Integer>();
+        for (int i = 0 ; i < limits.length ; i+=2) {
+            lims.put(Pattern.compile((String) limits[i]), (Integer) limits[i + 1]);
+        }
+        XMLStreamReader in = xmlFactory.createXMLStreamReader(new StringReader(input));
+        ByteArrayOutputStream os = new ByteArrayOutputStream();
+        XMLStreamWriter out = xmlOutFactory.createXMLStreamWriter(os);
+        XMLStepper.limitXML(in, out, lims, false, onlyElementMatch);
+        assertEquals("The input should be reduced properly for limits " + Strings.join(limits),
+                     expected, os.toString());
+    }
+
     // Sanity check for traversal of sub
     public void testIterateTags() throws Exception {
         XMLStreamReader xml = xmlFactory.createXMLStreamReader(new StringReader(SAMPLE));
@@ -71,22 +109,38 @@ public class XMLStepperTest extends TestCase {
         assertTrue("The second 'bar' should be findable", XMLStepper.findTagStart(xml, "bar"));
     }
 
-    public void testPipePosition() throws XMLStreamException {
+    public void testPipePositionOnIgnoredFail() throws XMLStreamException {
         ByteArrayOutputStream os = new ByteArrayOutputStream();
         XMLStreamWriter out = xmlOutFactory.createXMLStreamWriter(os);
         XMLStreamReader in = xmlFactory.createXMLStreamReader(new StringReader(SAMPLE));
         assertTrue("The first 'bar' should be findable", XMLStepper.findTagStart(in, "bar"));
-        XMLStepper.pipeXML(in, out, false); // until </bar>
+        XMLStepper.pipeXML(in, out, false); // until first </bar>
         assertEquals("The reader should be positioned at a character tag (newline) but was positioned at "
                      + XMLUtil.eventID2String(in.getEventType()),
                      XMLStreamConstants.CHARACTERS, in.getEventType());
+    }
+
+    public void testPipe() throws XMLStreamException {
+        ByteArrayOutputStream os = new ByteArrayOutputStream();
+        XMLStreamWriter out = xmlOutFactory.createXMLStreamWriter(os);
+        XMLStreamReader in = xmlFactory.createXMLStreamReader(new StringReader(SAMPLE));
+        XMLStepper.pipeXML(in, out, false);
+        assertEquals("Piped stream should match input stream",
+                     SAMPLE, os.toString());
     }
 
     public void testGetSubXML_NoOuter() throws XMLStreamException {
         XMLStreamReader in = xmlFactory.createXMLStreamReader(new StringReader(OUTER_FULL));
         assertTrue("The first 'foo' should be findable", XMLStepper.findTagStart(in, "foo"));
         String piped = XMLStepper.getSubXML(in, false, true);
-        assertEquals("The output should contain only the inner XML", OUTER_SNIPPET, piped);
+        assertEquals("The output should contain the inner XML", "<bar>bar1</bar>", piped);
+    }
+
+    public void testGetSubXML_Outer() throws XMLStreamException {
+        XMLStreamReader in = xmlFactory.createXMLStreamReader(new StringReader(OUTER_FULL));
+        assertTrue("The first 'foo' should be findable", XMLStepper.findTagStart(in, "foo"));
+        String piped = XMLStepper.getSubXML(in, false, false);
+        assertEquals("The output should contain the inner XML", "<foo><bar>bar1</bar><bar>bar2</bar></foo>", piped);
     }
 
     public void testPipeComments() throws XMLStreamException {
@@ -118,11 +172,9 @@ public class XMLStepperTest extends TestCase {
     }
 
     private void assertPipe(String expected, XMLStreamReader xml) throws XMLStreamException {
-        ByteArrayOutputStream os = new ByteArrayOutputStream();
-        XMLStreamWriter out = xmlOutFactory.createXMLStreamWriter(os);
-        XMLStepper.pipeXML(xml, out, false);
-        log.info("Sub-XML: " + os.toString());
+        String result = XMLStepper.getSubXML(xml, false);
+        log.info("Sub-XML: " + result);
         assertEquals("The piper should reproduce the desired sub section of the XML",
-                     expected, os.toString());
+                     expected, result);
     }
 }
