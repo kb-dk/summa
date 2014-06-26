ab22eb8d4305f6b70348f7b14fb2fbd7b26cea23
toke-sb <toke-sb@6c0f5c6f-88b2-4050-bb98-6ed3ea504abe>
2014-06-26 13:31:17 +0000

    Feature: ALTO duplicate finder as standalone util (not tested yet)

--- trunk/Core/src/test/java/dk/statsbiblioteket/summa/support/alto/AltoDuplicateTest.java
+++ trunk/Core/src/test/java/dk/statsbiblioteket/summa/support/alto/AltoDuplicateTest.java
@@ -35,11 +35,66 @@ public class AltoDuplicateTest extends TestCase {
     private static Log log = LogFactory.getLog(AltoDuplicateTest.class);
 
     // TODO: We do not have license to publish these files. Generate obfuscated test files from them
+/*    public static final File[] sources = new File[]{
+            new File("/home/te/smb/U/B400026951974-RT1")
+//            new File("/home/te/smb/U/B400026952083-RT1/400026952083-10") // Verified 4 duplicate groups
+//            new File("/home/te/smb/U/B400026952121-RT1") // Verified at least 2 duplicate groups
+            //new File("/home/te/smb/U/B400026952083-RT1/400026952083-13") // Alleged 4 groups
+    };*/
     public static final File[] sources = new File[]{
-//            new File("/home/te/smb/U/B400026952083-RT1/400026952083-10")
-            new File("/home/te/smb/U/B400026952121-RT1")
-            //new File("/home/te/smb/U/B400026952083-RT1/400026952083-13")
+//            new File("/home/te/smb/U/B400026951974-RT1"), // None
+//            new File("/home/te/smb/U/B400026951982-RT1"), // None
+//            new File("/home/te/smb/U/B400026951990-RT1"), // None
+//            new File("/home/te/smb/U/B400026952016-RT1"), // None
+//            new File("/home/te/smb/U/B400026952032-RT1"), // None
+//            new File("/home/te/smb/U/B400026952040-RT1"), // 27 groups
+//            new File("/home/te/smb/U/B400026952059-RT1"), // 24 groups
+/*            new File("/home/te/smb/U/B400026952067-RT1"),
+            new File("/home/te/smb/U/B400026952075-RT1"),
+            new File("/home/te/smb/U/B400026952083-RT1"),
+            new File("/home/te/smb/U/B400026952091-RT1"),
+            new File("/home/te/smb/U/B400026952105-RT1"),
+            new File("/home/te/smb/U/B400026952113-RT1"),
+            new File("/home/te/smb/U/B400026952121-RT1"),
+            new File("/home/te/smb/U/B400026952131-RT1"),
+            new File("/home/te/smb/U/B400026952148-RT1"),
+            new File("/home/te/smb/U/B400026952148-RT2"),
+            new File("/home/te/smb/U/B400026952148-RT4"),
+            new File("/home/te/smb/U/B400026952156-RT1"),
+            new File("/home/te/smb/U/B400026955252-RT1"), // None
+            new File("/home/te/smb/U/B400026955260-RT1"), // None
+            new File("/home/te/smb/U/B400026955287-RT1"), // None
+            new File("/home/te/smb/U/B400026955295-RT1"), // None
+
+            new File("/home/te/smb/U/B400026955244-RT1"), // Jun 7*/
+            //new File("/home/te/smb/U/B400026952008-RT1"), // None
+            /*new File("/home/te/smb/U/B400027043877-RT1"), // None
+            */
+            //new File("/home/te/smb/U/B400026952024-RT1")
+            /*new File("/home/te/smb/U/B400027043796-RT1"), // None
+            new File("/home/te/smb/U/B400027043761-RT1"), // None
+            new File("/home/te/smb/U/B400027043818-RT1"), // None
+            new File("/home/te/smb/U/B400027043788-RT1"), // None
+            new File("/home/te/smb/U/B400027043834-RT1"), // None
+            new File("/home/te/smb/U/B400027043915-RT1"), // None
+            new File("/home/te/smb/U/B400026952148-RT4"), // Maybe duplicates
+            new File("/home/te/smb/U/B400026952156-RT1"), // Maybe duplicates
+            new File("/home/te/smb/U/B400027043931-RT1"), // None
+            new File("/home/te/smb/U/B400027043893-RT1"), // None
+            new File("/home/te/smb/U/B400026951771-RT1"), // None
+            new File("/home/te/smb/U/B400027043923-RT1"), // None
+            new File("/home/te/smb/U/B400027043885-RT1"), // None
+            new File("/home/te/smb/U/B400027043907-RT1"), // None
+            new File("/home/te/smb/U/B400026951753-RT1"), // None
+            new File("/home/te/smb/U/B400026951761-RT1"), // None
+            new File("/home/te/smb/U/B400026951796-RT1"), // None
+            new File("/home/te/smb/U/B400026951788-RT1")    // 2 duplicates (false positives)*/
+//            new File("/home/te/smb/U/B400026951801-RT1"), // 9 groups (false positives)
+//            new File("/home/te/smb/U/B400026951818-RT1") , // None
+//            new File("/home/te/smb/U/B400026951826-RT1") // 4 groups (false positives)
     };
+
+
             //"/home/te/projects/summa-trunk/Core/src/test/resources/support/alto/as2/sample_2_2/B400022028241-RT2/";
             //"/home/te/smb/U/B400026952040-RT1/";
     public static final String ALTO_SUFFIX = ".alto.xml";
@@ -49,8 +104,11 @@ public class AltoDuplicateTest extends TestCase {
 
     private static final int SECOND_MIN_CHARS = 100;
     private static final int SECOND_MIN_BLOCK_MATCHES_PER_PAGE = 3;
+    private static final double SECOND_MIN_BLOCK_FRACTION_MATCHES_PER_PAGE = 0.5;
     private static final int SECOND_MIN_DUPLICATES = 2;
 
+    public static final int MAX_OUTPUT_GROUPS = 5;
+    public static final int MAX_OUTPUT_GROUP_SIZE = 5;
 
     public AltoDuplicateTest(String name) {
         super(name);
@@ -73,13 +131,21 @@ public class AltoDuplicateTest extends TestCase {
     @SuppressWarnings("UseOfSystemOutOrSystemErr")
     public void testDuplicates() throws FileNotFoundException, XMLStreamException {
         if (!sources[0].exists()) {
+            System.out.println("The source '" + sources[0] + "' is not accessible");
             return;
         }
         final String DEST = "/media/te/SB_TE/avis/duplicates/sample/";
 
         for (File source: sources) {
+            long startTime = System.nanoTime();
+            System.out.println("# Processing " + source);
             List<List<File>> duplicated = findPotentialDuplicates(new File[]{source}, FIRST_MIN_CHARS);
-            System.out.println("# " + duplicated.size() + " potential duplicate text blocks from source " + source);
+            System.out.println(String.format("\n# Found %d duplicate text blocks from source %s in %s seconds",
+                                             duplicated.size(), source, (System.nanoTime()-startTime)/1000000000L));
+            System.out.println(String.format(
+                    "# Min %d blocks of %d chars, %d blocks of %d chars and %d%% blocks of any chars",
+                    FIRST_MIN_DUPLICATES, FIRST_MIN_CHARS, SECOND_MIN_BLOCK_MATCHES_PER_PAGE, SECOND_MIN_CHARS,
+                    (int)(SECOND_MIN_BLOCK_FRACTION_MATCHES_PER_PAGE*100)));
             if (duplicated.isEmpty()) {
                 continue;
             }
@@ -91,14 +157,23 @@ public class AltoDuplicateTest extends TestCase {
             for (List<File> entries: duplicated) {
                 System.out.println("");
                 String finalDest = String.format("%s/%02d", superGroup, dup);
+                if (dup > MAX_OUTPUT_GROUPS) {
+                    System.out.print("# ");
+                }
                 System.out.println("mkdir -p " + finalDest);
+                int entryCount = 1;
                 for (File entry: entries) {
+                    if (dup > MAX_OUTPUT_GROUPS || entryCount >= MAX_OUTPUT_GROUP_SIZE) {
+                        System.out.print("# ");
+                    }
                     System.out.println(
                             "cp -n "+ entry.getAbsolutePath().replace(ALTO_SUFFIX, "") + ".* " + finalDest);
+                    entryCount++;
                 }
                 dup++;
 //                System.out.println(Strings.join(entries, "\n"));
             }
+            System.out.println("");
         }
     }
 
@@ -107,7 +182,11 @@ public class AltoDuplicateTest extends TestCase {
             throws FileNotFoundException, XMLStreamException {
         Map<Integer, Set<File>> duplicates = new HashMap<>();
         List<File> altoFiles = getAltoFiles(sources);
-        System.out.println("Calculating text block hashes from " + altoFiles.size() + " alto files");
+        System.out.print("# Calculating text block hashes from " + altoFiles.size() + " alto files\n# ");
+        for(int i = 0 ; i < 99 ; i++) {
+            System.out.print(".");
+        }
+        System.out.print("|\n# ");
         int altoCount = 0;
         for (File altoFile: altoFiles) {
             if (++altoCount % (altoFiles.size() / 100) == 0) {
@@ -144,9 +223,10 @@ public class AltoDuplicateTest extends TestCase {
 
     private List<List<File>> prune(Map<Integer, Set<File>> potentialDuplicates)
             throws FileNotFoundException, XMLStreamException {
-        System.out.println("Pruning " + potentialDuplicates.size() + " potential duplicate groups");
+        System.out.println(
+                "# Pruning " + potentialDuplicates.size() + " groups (min group size " + FIRST_MIN_DUPLICATES + ")");
         List<List<File>> minDupReduced = pruneMinDuplicates(potentialDuplicates);
-        System.out.println("Eliminating false positives from " + minDupReduced.size() + " duplicate groups");
+        System.out.println("# Eliminating false positives from " + minDupReduced.size() + " duplicate groups");
         List<List<File>> minBlocksReduced = pruneMinBlocks(minDupReduced);
 
         return minBlocksReduced;
@@ -189,7 +269,7 @@ public class AltoDuplicateTest extends TestCase {
         for (Alto primary: altos) {
             int matchCount = 0;
             for (Alto secondary: altos) {
-                if (primary != secondary && connectedMatches(primary, secondary) >= SECOND_MIN_BLOCK_MATCHES_PER_PAGE) {
+                if (primary != secondary && connectedMatches(primary, secondary)) {
                     matchCount++;
                 }
             }
@@ -201,16 +281,24 @@ public class AltoDuplicateTest extends TestCase {
     }
 
     // Count matching blocks between two Altos
-    private int connectedMatches(Alto primary, Alto secondary) {
-        int matches = 0;
-        for (String pBlock: getTextBlocks(primary, SECOND_MIN_CHARS)) {
-            for (String sBlock: getTextBlocks(secondary, SECOND_MIN_CHARS)) {
+    private boolean connectedMatches(Alto primary, Alto secondary) {
+        int largeMatches = 0;
+        int allMatches = 0;
+        List<String> pBlocks = getTextBlocks(primary, 1);
+        for (String pBlock: pBlocks) {
+            for (String sBlock: getTextBlocks(secondary, 1)) {
                 if (pBlock.equals(sBlock)) {
-                    matches++;
+                    if (pBlock.length() >= SECOND_MIN_CHARS) {
+                        largeMatches++;
+                    }
+                    allMatches++;
                 }
             }
+
         }
-        return matches;
+
+        return largeMatches >= SECOND_MIN_BLOCK_MATCHES_PER_PAGE
+               && allMatches >= pBlocks.size() * SECOND_MIN_BLOCK_FRACTION_MATCHES_PER_PAGE;
     }
 
     public void testGetAltos() throws XMLStreamException, FileNotFoundException {
@@ -246,6 +334,6 @@ public class AltoDuplicateTest extends TestCase {
                 addFiles(altoFiles, sub);
             }
         }
-        System.out.println("Alto count: " + altoFiles.size() + " from " + folder);
+    //    System.out.println("Alto count: " + altoFiles.size() + " from " + folder);
     }
 }
